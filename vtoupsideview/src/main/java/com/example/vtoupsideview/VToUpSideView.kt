292ac7e5e0f43eada17215acbd13c3f512cbc3d7
Anwesh43 <anweshthecool0@gmail.com>
2021-07-11 02:42:32 +0530

    adding Canvas extension methods

--- vtoupsideview/src/main/java/com/example/vtoupsideview/VToUpSideView.kt
+++ vtoupsideview/src/main/java/com/example/vtoupsideview/VToUpSideView.kt
@@ -28,3 +28,30 @@ val deg : Float = -45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawVToUpSide(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        translate((w / 2) * sc4, (h / 2) * (1 - sc2))
+        rotate(deg * (1f - sc3))
+        drawLine(0f, 0f, size * sc1, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawVTUSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawVToUpSide(scale, w, h, paint)
+}
