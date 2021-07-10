b4ac8327a79371e9f0f453325c71afb0a3e2d5b6
Anwesh43 <anweshthecool0@gmail.com>
2021-07-11 03:00:22 +0530

    adding Renderer class

--- vtoupsideview/src/main/java/com/example/vtoupsideview/VToUpSideView.kt
+++ vtoupsideview/src/main/java/com/example/vtoupsideview/VToUpSideView.kt
@@ -182,4 +182,27 @@ class VToUpSideView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : VToUpSideView) {
+
+        private val vtus : VToUpSide = VToUpSide(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            vtus.draw(canvas, paint)
+            animator.animate {
+                vtus.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            vtus.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
