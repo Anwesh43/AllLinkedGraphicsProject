8180d97b62a03ae7b6652c666ebb53f9d48deb32
Anwesh43 <anweshthecool0@gmail.com>
2021-07-15 01:55:26 +0530

    adding LFGNode class

--- linefollowgoview/src/main/java/com/example/linefollowgoview/LineFollowGoView.kt
+++ linefollowgoview/src/main/java/com/example/linefollowgoview/LineFollowGoView.kt
@@ -120,4 +120,45 @@ class LineFollowGoView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LFGNode(var i : Int, val state : State = State()) {
+
+        private var next : LFGNode? = null
+        private var prev : LFGNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LFGNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLFGNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LFGNode {
+            var curr : LFGNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
