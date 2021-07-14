af873d7405166424e2f35c3c811aa91ec45724b3
Anwesh43 <anweshthecool0@gmail.com>
2021-07-15 01:41:48 +0530

    adding Canvas extension methods

--- linefollowgoview/src/main/java/com/example/linefollowgoview/LineFollowGoView.kt
+++ linefollowgoview/src/main/java/com/example/linefollowgoview/LineFollowGoView.kt
@@ -30,3 +30,30 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineFollowGo(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..(lines - 1)) {
+        val sc1j : Float = sc1.divideScale(j, lines)
+        val sc2j : Float = sc2.divideScale(j, lines)
+        save()
+        translate(0f, h / 2)
+        rotate(deg * sc1j * j)
+        drawLine(-size / 2, -size * sc2j, -size / 2, -size * sc1j, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLFGNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineFollowGo(scale, w, h, paint)
+}
