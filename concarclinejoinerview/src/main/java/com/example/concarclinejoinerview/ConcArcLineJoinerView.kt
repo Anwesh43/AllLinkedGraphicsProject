1e2cab5d7c1ce9359176702d0da7711f7b705959
Anwesh43 <anweshthecool0@gmail.com>
2021-07-16 02:00:34 +0530

    adding Int and Float extension methods

--- concarclinejoinerview/src/main/java/com/example/concarclinejoinerview/ConcArcLineJoinerView.kt
+++ concarclinejoinerview/src/main/java/com/example/concarclinejoinerview/ConcArcLineJoinerView.kt
@@ -26,3 +26,7 @@ val r1Factor : Float = 5.2f
 val r2Factor : Float = 3.2f
 val backColor : Int = Color.parseColor("#BDBDBD")
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
