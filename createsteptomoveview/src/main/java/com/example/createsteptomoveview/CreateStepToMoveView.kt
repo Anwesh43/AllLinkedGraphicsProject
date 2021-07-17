841957b9e341206e583b7f09123b3b376f9464e4
Anwesh43 <anweshthecool0@gmail.com>
2021-07-17 20:50:17 +0530

    adding CreateStepToMoveView class

--- createsteptomoveview/src/main/java/com/example/createsteptomoveview/CreateStepToMoveView.kt
+++ createsteptomoveview/src/main/java/com/example/createsteptomoveview/CreateStepToMoveView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawCSTMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawCreateStepToMove(scale, w, h, paint)
 }
+
+class CreateStepToMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
