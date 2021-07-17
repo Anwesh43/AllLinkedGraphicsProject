76d275c5b3073b1943f556e745e4d8a3fbe1479b
Anwesh43 <anweshthecool0@gmail.com>
2021-07-18 02:47:22 +0530

    adding RotateSemiArcMoveView class

--- rotatesemiarcmoveview/src/main/java/com/example/rotatesemiarcmoveview/RotateSemiArcMoveView.kt
+++ rotatesemiarcmoveview/src/main/java/com/example/rotatesemiarcmoveview/RotateSemiArcMoveView.kt
@@ -68,3 +68,19 @@ fun Canvas.drawRSAMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRotateSemiArcMove(scale, w, h, paint)
 }
+
+class RotateSemiArcMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
