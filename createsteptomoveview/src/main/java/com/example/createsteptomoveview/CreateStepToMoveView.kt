d9f3003791d88af0c4719197d206e9fbd85fff4c
Anwesh43 <anweshthecool0@gmail.com>
2021-07-17 17:54:38 +0530

    adding canvas extension methods

--- createsteptomoveview/src/main/java/com/example/createsteptomoveview/CreateStepToMoveView.kt
+++ createsteptomoveview/src/main/java/com/example/createsteptomoveview/CreateStepToMoveView.kt
@@ -29,3 +29,35 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawCreateStepToMove(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val sc6 : Float = scale.divideScale(5, parts)
+    val sc7 : Float = scale.divideScale(6, parts)
+    val xGap : Float = size / lines
+    save()
+    translate(w / 2 + (w / 2 + size / 2) * sc7, h / 2 + (h / 2 - size / 2) * (1 - sc6))
+    rotate(rot * sc7)
+    for (j in 0..(lines - 1)) {
+        val scj : Float = scale.divideScale(j, parts)
+        val scj1 : Float = scj.divideScale(0, 2)
+        val scj2 : Float = scj.divideScale(1, 2)
+        save()
+        translate(-size / 2 + xGap * j, size / 2 - xGap * j)
+        drawLine(0f, 0f, 0f, -xGap * scj1, paint)
+        drawLine(0f, -xGap, xGap * scj2, -xGap, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawCSTMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawCreateStepToMove(scale, w, h, paint)
+}
