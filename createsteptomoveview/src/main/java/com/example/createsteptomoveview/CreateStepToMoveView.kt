1401db7e68f917d6d8d5b393dce9a25f440bd463
Anwesh43 <anweshthecool0@gmail.com>
2021-07-17 23:00:36 +0530

    adding CSTMNode class

--- createsteptomoveview/src/main/java/com/example/createsteptomoveview/CreateStepToMoveView.kt
+++ createsteptomoveview/src/main/java/com/example/createsteptomoveview/CreateStepToMoveView.kt
@@ -124,4 +124,45 @@ class CreateStepToMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CSTMNode(var i : Int, val state : State = State()) {
+
+        private var next : CSTMNode? = null
+        private var prev : CSTMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CSTMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCSTMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CSTMNode {
+            var curr : CSTMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
