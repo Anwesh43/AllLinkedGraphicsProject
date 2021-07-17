e54ac4a49576c44dcc044b11f880b1f484d6c3ec
Anwesh43 <anweshthecool0@gmail.com>
2021-07-18 03:02:10 +0530

    adding RotateSemiArcMove class

--- rotatesemiarcmoveview/src/main/java/com/example/rotatesemiarcmoveview/RotateSemiArcMoveView.kt
+++ rotatesemiarcmoveview/src/main/java/com/example/rotatesemiarcmoveview/RotateSemiArcMoveView.kt
@@ -169,5 +169,28 @@ class RotateSemiArcMoveView(ctx : Context) : View(ctx) {
                 return this
             }
         }
+
+        data class RotateSemiArcMove(var i : Int) {
+
+            private var curr : RSAMNode = RSAMNode(0)
+            private var dir : Int = 1
+
+            fun draw(canvas : Canvas, paint : Paint) {
+                curr.draw(canvas, paint)
+            }
+
+            fun update(cb : (Float) -> Unit) {
+                curr.update {
+                    curr = curr.getNext(dir) {
+                        dir *= -1
+                    }
+                    cb(it)
+                }
+            }
+
+            fun startUpdating(cb : () -> Unit) {
+                curr.startUpdating(cb)
+            }
+        }
     }
 }
