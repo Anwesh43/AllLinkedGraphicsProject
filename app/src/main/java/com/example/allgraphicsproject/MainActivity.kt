a48fe49da757a18e6c8b026f8936123f22e586f2
Anwesh43 <anweshthecool0@gmail.com>
2021-07-19 00:20:37 +0530

    adding module setup

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,15 +3,15 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.concarclinejoinerview.ConcArcLineJoinerView
-import com.example.createsteptomoveview.CreateStepToMoveView
-import com.example.rotatesemiarcmoveview.RotateSemiArcMoveView
+//import com.example.concarclinejoinerview.ConcArcLineJoinerView
+//import com.example.createsteptomoveview.CreateStepToMoveView
+//import com.example.rotatesemiarcmoveview.RotateSemiArcMoveView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         RotateSemiArcMoveView.create(this)
+         //RotateSemiArcMoveView.create(this)
         fullScreen()
     }
 }
