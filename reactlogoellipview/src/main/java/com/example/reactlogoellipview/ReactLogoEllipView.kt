12f6f9dec2d3ff153141498df5d5e2959c7e8e27
Anwesh43 <anweshthecool0@gmail.com>
2021-07-19 01:01:09 +0530

    adding Renderer class

--- reactlogoellipview/src/main/java/com/example/reactlogoellipview/ReactLogoEllipView.kt
+++ reactlogoellipview/src/main/java/com/example/reactlogoellipview/ReactLogoEllipView.kt
@@ -189,4 +189,27 @@ class ReactLogoEllipView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ReactLogoEllipView) {
+
+        private val animator : Animator = Animator(view)
+        private val rle : ReactLogoEllip = ReactLogoEllip(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            rle.draw(canvas, paint)
+            animator.animate {
+                rle.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            rle.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
