dd49445fcca55b347b5e293ede4a799afd8dcd8d
Anwesh43 <anweshthecool0@gmail.com>
2021-07-20 01:37:10 +0530

    adding Canvas extension methods

--- rectchairlineview/src/main/java/com/example/rectchairlineview/RectChairLneView.kt
+++ rectchairlineview/src/main/java/com/example/rectchairlineview/RectChairLneView.kt
@@ -30,3 +30,35 @@ val scGap : Float = 0.02f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawRectChairLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val rw : Float = Math.min(w, h) / rwFactor
+    val rh : Float = Math.min(w, h) / rhFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2 - (rw + rh) / 2) * (1 - sc3) - (h / 2 + (rw + rh)) * sc4)
+    rotate(rot * sc3)
+    save()
+    translate(-rw / 2, (rw + rh) / 2)
+    drawRect(RectF(0f, -(rh) * sc1, rw, 0f), paint)
+    for (j in 0..1) {
+        save()
+        translate(rw * j, 0f)
+        drawLine(0f, 0f, 0f, -rh * sc2, paint)
+        restore()
+    }
+    restore()
+    restore()
+}
+
+fun Canvas.drawRCLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRectChairLine(scale, w, h, paint)
+}
