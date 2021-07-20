e7aae45dc0a0507735968165d2e039100a837cb7
Anwesh43 <anweshthecool0@gmail.com>
2021-07-21 01:20:39 +0530

    adding PieToTMove class

--- pietotmoveview/src/main/java/com/example/pietotmoveview/PieToTMoveView.kt
+++ pietotmoveview/src/main/java/com/example/pietotmoveview/PieToTMoveView.kt
@@ -165,4 +165,27 @@ class PieToTMoveView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class PieToTMove(var i : Int, val state : State = State()) {
+
+        private var curr : PTTMNode = PTTMNode(0)
+        private var dir : Int =1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
