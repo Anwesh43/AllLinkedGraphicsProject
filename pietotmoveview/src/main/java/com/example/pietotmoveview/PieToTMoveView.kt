386c31c6abda796e8dce1cccf6ae62db0d7390fd
Anwesh43 <anweshthecool0@gmail.com>
2021-07-21 01:04:24 +0530

    adding PieToTMoveView class

--- pietotmoveview/src/main/java/com/example/pietotmoveview/PieToTMoveView.kt
+++ pietotmoveview/src/main/java/com/example/pietotmoveview/PieToTMoveView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawPTTMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) /strokeFactor
     drawPieToMove(scale, w, h, paint)
 }
+
+class PieToTMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
