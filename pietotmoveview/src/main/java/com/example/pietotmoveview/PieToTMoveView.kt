f9b4c7a5e02225e68ecd04a7881c3516486e7ba3
Anwesh43 <anweshthecool0@gmail.com>
2021-07-21 00:53:21 +0530

    adding constants and libraries

---
+++ pietotmoveview/src/main/java/com/example/pietotmoveview/PieToTMoveView.kt
@@ -0,0 +1,27 @@
+package com.example.pietotmoveview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 45f
+val deg : Float = 180f
