150d4347f6c0b4113014679c6ddeea0a6ba50c5a
Anwesh43 <anweshthecool0@gmail.com>
2021-07-21 01:11:30 +0530

    adding PTTMNode class

--- pietotmoveview/src/main/java/com/example/pietotmoveview/PieToTMoveView.kt
+++ pietotmoveview/src/main/java/com/example/pietotmoveview/PieToTMoveView.kt
@@ -124,4 +124,45 @@ class PieToTMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PTTMNode(var i : Int, val state : State = State()) {
+
+        private var next : PTTMNode? = null
+        private var prev : PTTMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PTTMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPTTMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PTTMNode {
+            var curr : PTTMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
