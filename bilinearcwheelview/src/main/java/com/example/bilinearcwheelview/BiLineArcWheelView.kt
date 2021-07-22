fbd6f5439472f31d5f18c11d37fffa7f7f2a9b06
Anwesh43 <anweshthecool0@gmail.com>
2021-07-23 01:20:00 +0530

    addin Canvas extension methods

--- bilinearcwheelview/src/main/java/com/example/bilinearcwheelview/BiLineArcWheelView.kt
+++ bilinearcwheelview/src/main/java/com/example/bilinearcwheelview/BiLineArcWheelView.kt
@@ -5,6 +5,7 @@ import android.view.MotionEvent
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.RectF
 import android.content.Context
 import android.app.Activity
 
@@ -31,3 +32,35 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+
+fun Canvas.drawBiLineArcWheel(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        translate(0f, h / 2 - (h / 2 - size) * sc2)
+        save()
+        rotate(sweep * sc3)
+        drawLine(0f, 0f, 0f, -size * sc1, paint)
+        restore()
+        drawArc(RectF(-size, -size, size, size), rot, sweep * sc3, false, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBLAWNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiLineArcWheel(scale, w, h, paint)
+}
