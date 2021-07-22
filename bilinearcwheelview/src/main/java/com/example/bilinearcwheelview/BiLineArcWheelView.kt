5aba777cdaf711bb2540ea01e0911ad2b122dee9
Anwesh43 <anweshthecool0@gmail.com>
2021-07-23 02:13:54 +0530

    addign static create method

--- bilinearcwheelview/src/main/java/com/example/bilinearcwheelview/BiLineArcWheelView.kt
+++ bilinearcwheelview/src/main/java/com/example/bilinearcwheelview/BiLineArcWheelView.kt
@@ -216,4 +216,13 @@ class BiLineArcWheelView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : BiLineArcWheelView {
+            val view : BiLineArcWheelView = BiLineArcWheelView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
