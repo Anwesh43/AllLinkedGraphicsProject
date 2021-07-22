9ac5a01862c922bb2fd45215000dda7a572ff343
Anwesh43 <anweshthecool0@gmail.com>
2021-07-23 01:59:42 +0530

    adding BLAWNode class

--- bilinearcwheelview/src/main/java/com/example/bilinearcwheelview/BiLineArcWheelView.kt
+++ bilinearcwheelview/src/main/java/com/example/bilinearcwheelview/BiLineArcWheelView.kt
@@ -127,4 +127,45 @@ class BiLineArcWheelView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLAWNode(var i : Int, val state : State = State()) {
+
+        private var next : BLAWNode? = null
+        private var prev : BLAWNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLAWNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLAWNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLAWNode {
+            var curr : BLAWNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
