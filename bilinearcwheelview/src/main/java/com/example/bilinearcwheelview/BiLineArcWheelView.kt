2486c7fecf3b68997dfdeb1888eb075a46673638
Anwesh43 <anweshthecool0@gmail.com>
2021-07-23 01:24:20 +0530

    adding BiLineArcWheelView class

--- bilinearcwheelview/src/main/java/com/example/bilinearcwheelview/BiLineArcWheelView.kt
+++ bilinearcwheelview/src/main/java/com/example/bilinearcwheelview/BiLineArcWheelView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawBLAWNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBiLineArcWheel(scale, w, h, paint)
 }
+
+class BiLineArcWheelView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
