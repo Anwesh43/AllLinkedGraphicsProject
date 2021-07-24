4121f541c2b8eb29fbd2521e17b69fe8a787397b
Anwesh43 <anweshthecool0@gmail.com>
2021-07-25 01:44:08 +0530

    adding constants and libraries

---
+++ pauserotlineview/src/main/java/com/example/pauserotlineview/PauseRotLineView.kt
@@ -0,0 +1,27 @@
+package com.example.pauserotlineview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.Paint
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
+val gapFactor : Float = 8.3f
