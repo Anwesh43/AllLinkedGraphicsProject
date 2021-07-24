f21d29da6405a65cd8619c15027b3d27d1189980
Anwesh43 <anweshthecool0@gmail.com>
2021-07-25 02:01:08 +0530

    adding PauseRotLineView class

--- pauserotlineview/src/main/java/com/example/pauserotlineview/PauseRotLineView.kt
+++ pauserotlineview/src/main/java/com/example/pauserotlineview/PauseRotLineView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawPRLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawPauseRotLine(scale, w, h, paint)
 }
+
+class PauseRotLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
