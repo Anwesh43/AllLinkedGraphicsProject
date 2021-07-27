4efa6becf628a98bf1a5c662c8c98c65e9306f7d
Anwesh43 <anweshthecool0@gmail.com>
2021-07-27 09:17:13 +0530

    adding Canvas extension methods

--- blockrotatedownview/src/main/java/com/example/blockrotatedownview/BlockRotateDownView.kt
+++ blockrotatedownview/src/main/java/com/example/blockrotatedownview/BlockRotateDownView.kt
@@ -29,3 +29,34 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBlockRotateDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(
+        w / 2 + (-w / 2 - size) * (1 - sc1),
+        h / 2 + (h / 2 + size / 2) * sc4
+    )
+    rotate(rot * sc3)
+    drawRect(RectF(0f, -size / 2, size, size / 2), paint)
+    for (j in 0..1) {
+        save()
+        translate(0f, -size / 2 + size * j)
+        drawLine(0f, 0f, -size * sc2, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBlockRotateDown(scale, w, h, paint)
+}
