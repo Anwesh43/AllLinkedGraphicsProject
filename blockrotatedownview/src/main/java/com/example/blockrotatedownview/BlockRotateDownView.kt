e17fd8922abc286267b89e6b11596b9d3baa91ac
Anwesh43 <anweshthecool0@gmail.com>
2021-07-27 09:26:52 +0530

    adding BlockRotateDown class

--- blockrotatedownview/src/main/java/com/example/blockrotatedownview/BlockRotateDownView.kt
+++ blockrotatedownview/src/main/java/com/example/blockrotatedownview/BlockRotateDownView.kt
@@ -164,4 +164,27 @@ class BlockRotateDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BlockRotateDown(var i : Int) {
+
+        private var curr : BRDNode = BRDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
