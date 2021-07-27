ce8b8134f1fb1eb5f40dd275d05865eb8921b979
Anwesh43 <anweshthecool0@gmail.com>
2021-07-27 09:25:23 +0530

    adding BRDNode class

--- blockrotatedownview/src/main/java/com/example/blockrotatedownview/BlockRotateDownView.kt
+++ blockrotatedownview/src/main/java/com/example/blockrotatedownview/BlockRotateDownView.kt
@@ -123,4 +123,45 @@ class BlockRotateDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BRDNode(var i : Int, val state : State = State()) {
+
+        private var next : BRDNode? = null
+        private var prev : BRDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BRDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBRDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BRDNode {
+            var curr : BRDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
