12de924558d2eb0c6cdeed5a19e356d198083865
Anwesh43 <anweshthecool0@gmail.com>
2021-07-28 10:29:11 +0530

    adding BFDDUNode class

--- ballfromdiagdownupview/src/main/java/com/example/ballfromdiagdownupview/BallFromDiagDownUpView.kt
+++ ballfromdiagdownupview/src/main/java/com/example/ballfromdiagdownupview/BallFromDiagDownUpView.kt
@@ -114,4 +114,45 @@ class BallFromDiagDownUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BFDDUNode(var i : Int, val state : State = State()) {
+
+        private var next : BFDDUNode? = null
+        private var prev : BFDDUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BFDDUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBFDDUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BFDDUNode {
+            var curr : BFDDUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
