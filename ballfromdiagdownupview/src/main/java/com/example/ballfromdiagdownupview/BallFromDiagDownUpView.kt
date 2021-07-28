ca070f83aa18c41ce4e2b2c22208671122e710c5
Anwesh43 <anweshthecool0@gmail.com>
2021-07-28 10:19:26 +0530

    adding BallFromDiagDownUpView class

--- ballfromdiagdownupview/src/main/java/com/example/ballfromdiagdownupview/BallFromDiagDownUpView.kt
+++ ballfromdiagdownupview/src/main/java/com/example/ballfromdiagdownupview/BallFromDiagDownUpView.kt
@@ -51,3 +51,19 @@ fun Canvas.drawBFDDUNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBallFromDiagDown(scale, w, h, paint)
 }
+
+class BallFromDiagDownUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
