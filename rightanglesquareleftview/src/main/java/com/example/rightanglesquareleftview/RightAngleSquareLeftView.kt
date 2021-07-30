24d6337584d41362ddd523e350b90957648cfc18
Anwesh43 <anweshthecool0@gmail.com>
2021-07-31 02:28:30 +0530

    adding Renderer class

--- rightanglesquareleftview/src/main/java/com/example/rightanglesquareleftview/RightAngleSquareLeftView.kt
+++ rightanglesquareleftview/src/main/java/com/example/rightanglesquareleftview/RightAngleSquareLeftView.kt
@@ -183,4 +183,27 @@ class RightAngleSquareLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : RightAngleSquareLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val rasl : RightAngleSquareLeft = RightAngleSquareLeft(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            rasl.draw(canvas, paint)
+            animator.animate {
+                rasl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            rasl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
