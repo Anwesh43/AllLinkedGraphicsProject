5c65f253d248a921c71603fa9866e47db86a1bcb
Anwesh43 <anweshthecool0@gmail.com>
2021-08-01 01:54:26 +0530

    adding Renderer class

--- squaresrotpushlineview/src/main/java/com/example/squaresrotpushlineview/SquaresRotPushLineView.kt
+++ squaresrotpushlineview/src/main/java/com/example/squaresrotpushlineview/SquaresRotPushLineView.kt
@@ -190,4 +190,27 @@ class SquaresRotPushLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SquaresRotPushLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val srpl : SquaresRotPushLine = SquaresRotPushLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            srpl.draw(canvas, paint)
+            animator.animate {
+                srpl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            srpl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
