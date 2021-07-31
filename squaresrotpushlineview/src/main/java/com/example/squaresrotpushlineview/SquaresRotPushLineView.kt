580eb5072991e462b12642230a52da9054b24142
Anwesh43 <anweshthecool0@gmail.com>
2021-08-01 01:36:38 +0530

    adding Canvas extension methods

--- squaresrotpushlineview/src/main/java/com/example/squaresrotpushlineview/SquaresRotPushLineView.kt
+++ squaresrotpushlineview/src/main/java/com/example/squaresrotpushlineview/SquaresRotPushLineView.kt
@@ -30,3 +30,36 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSquareRotPushLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sqSize : Float = size / sqSizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + sqSize / 2) * sc4, h / 2)
+    save()
+    rotate(rot * sc2)
+    drawLine(-size * 0.5f * sc1, 0f, size * 0.5f * sc1, 0f, paint)
+    restore()
+    for (j in 0..1) {
+        save()
+        translate(-sqSize / 2 - w / 2 + (w / 2) * sc3, 0f)
+        scale(1f, 1f - 2 * j)
+        translate(0f, size / 2 - sqSize / 2)
+        drawRect(RectF(-sqSize / 2, -sqSize / 2, sqSize / 2, sqSize / 2), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSRPLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSquareRotPushLine(scale, w, h, paint)
+}
