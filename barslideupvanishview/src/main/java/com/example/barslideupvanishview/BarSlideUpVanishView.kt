df70706355680daf0f33757762bd6ca187299b29
Anwesh43 <anweshthecool0@gmail.com>
2021-08-03 02:20:10 +0530

    adding BarSlideUpVanish class

--- barslideupvanishview/src/main/java/com/example/barslideupvanishview/BarSlideUpVanishView.kt
+++ barslideupvanishview/src/main/java/com/example/barslideupvanishview/BarSlideUpVanishView.kt
@@ -149,4 +149,27 @@ class BarSlideUpVanishView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BarSlideUpVanish(var i : Int) {
+
+        private var curr : BSUVNode = BSUVNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
