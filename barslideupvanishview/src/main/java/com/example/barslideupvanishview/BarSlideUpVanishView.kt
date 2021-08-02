386733cc2fe9f2a5e7a455dea961dd82f2cd45dc
Anwesh43 <anweshthecool0@gmail.com>
2021-08-03 02:18:25 +0530

    adding BSUVNode class

--- barslideupvanishview/src/main/java/com/example/barslideupvanishview/BarSlideUpVanishView.kt
+++ barslideupvanishview/src/main/java/com/example/barslideupvanishview/BarSlideUpVanishView.kt
@@ -108,4 +108,45 @@ class BarSlideUpVanishView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BSUVNode(var i : Int, val state : State = State()) {
+
+        private var next : BSUVNode? = null
+        private var prev : BSUVNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BSUVNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBSUVNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BSUVNode {
+            var curr : BSUVNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
