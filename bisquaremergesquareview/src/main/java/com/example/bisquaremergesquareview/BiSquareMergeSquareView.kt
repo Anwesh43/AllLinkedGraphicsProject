b6abcfa3ae525c6701eed55eb416d7d8c8b4970b
Anwesh43 <anweshthecool0@gmail.com>
2021-08-04 00:39:14 +0530

    adding constants and libraries

---
+++ bisquaremergesquareview/src/main/java/com/example/bisquaremergesquareview/BiSquareMergeSquareView.kt
@@ -0,0 +1,25 @@
+package com.example.bisquaremergesquareview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.03f / parts
+val sizeFactor : Float = 5.6f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
