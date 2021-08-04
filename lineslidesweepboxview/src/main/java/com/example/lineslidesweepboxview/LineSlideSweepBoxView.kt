8c5e6b2482bc05e26890d42179289e89b562fa22
Anwesh43 <anweshthecool0@gmail.com>
2021-08-05 02:18:06 +0530

    adding LineSlideSweepBox class

--- lineslidesweepboxview/src/main/java/com/example/lineslidesweepboxview/LineSlideSweepBoxView.kt
+++ lineslidesweepboxview/src/main/java/com/example/lineslidesweepboxview/LineSlideSweepBoxView.kt
@@ -136,7 +136,7 @@ class LineSlideSweepBoxView(ctx : Context) : View(ctx) {
             canvas.drawLSSBNode(i, state.scale, paint)
         }
 
-        fun update(cb : () -> Unit) {
+        fun update(cb : (Float) -> Unit) {
             state.update(cb)
         }
 
@@ -154,6 +154,29 @@ class LineSlideSweepBoxView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class LineSlideSweepBox(var i : Int) {
+
+        private var curr : LSSBNode = LSSBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
