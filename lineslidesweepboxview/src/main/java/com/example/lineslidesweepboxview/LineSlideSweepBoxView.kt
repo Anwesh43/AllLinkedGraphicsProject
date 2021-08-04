01bcc3dc44a4f792f1cd353d8da2197e8167fdc6
Anwesh43 <anweshthecool0@gmail.com>
2021-08-05 01:47:15 +0530

    adding constants and libraries

---
+++ lineslidesweepboxview/src/main/java/com/example/lineslidesweepboxview/LineSlideSweepBoxView.kt
@@ -0,0 +1,25 @@
+package com.example.lineslidesweepboxview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.03f / parts
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
