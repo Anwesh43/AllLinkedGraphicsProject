8e8ea6c0ee49a480abf3c6bc10b84a9ac265bffe
Anwesh43 <anweshthecool0@gmail.com>
2021-08-05 02:09:39 +0530

    adding Canvas extension methods

--- lineslidesweepboxview/src/main/java/com/example/lineslidesweepboxview/LineSlideSweepBoxView.kt
+++ lineslidesweepboxview/src/main/java/com/example/lineslidesweepboxview/LineSlideSweepBoxView.kt
@@ -27,3 +27,28 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineSlideSweepBox(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        drawLine(-w * 0.5f * sc3, h / 2, -w * 0.5f * sc3, h * 0.5f * (1f - sc1), paint)
+        drawRect(RectF(-w * 0.5f * sc2,0f, h / 2, -w * 0.5f * sc3), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLSSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineSlideSweepBox(scale, w, h, paint)
+}
