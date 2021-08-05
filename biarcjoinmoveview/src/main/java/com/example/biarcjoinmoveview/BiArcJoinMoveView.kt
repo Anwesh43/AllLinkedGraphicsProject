87d07ad4406eff5c9e76ae20365c92fd10fc0b1a
Anwesh43 <anweshthecool0@gmail.com>
2021-08-06 02:50:46 +0530

    adding Canvas extension methods

--- biarcjoinmoveview/src/main/java/com/example/biarcjoinmoveview/BiArcJoinMoveView.kt
+++ biarcjoinmoveview/src/main/java/com/example/biarcjoinmoveview/BiArcJoinMoveView.kt
@@ -30,3 +30,27 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawBiArcJoinMove(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc3)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        translate(0f, j * (h / 2 - size / 2) * (1 - sc2))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, rot * sc1, true, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBAJMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBAJMNode(i, scale, paint)
+}
