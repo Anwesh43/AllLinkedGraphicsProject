33751ac90f67b264a4089637b0f16911cd577741
Anwesh43 <anweshthecool0@gmail.com>
2021-08-06 03:06:37 +0530

    addig BAJMNode class

--- biarcjoinmoveview/src/main/java/com/example/biarcjoinmoveview/BiArcJoinMoveView.kt
+++ biarcjoinmoveview/src/main/java/com/example/biarcjoinmoveview/BiArcJoinMoveView.kt
@@ -117,4 +117,45 @@ class BiArcJoinMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BAJMNode(var i : Int, val state : State = State()) {
+
+        private var next : BAJMNode? = null
+        private var prev : BAJMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BAJMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBAJMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BAJMNode {
+            var curr : BAJMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
