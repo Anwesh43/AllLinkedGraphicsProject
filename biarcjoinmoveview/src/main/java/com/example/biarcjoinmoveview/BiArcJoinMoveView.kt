24c21e871d2ba42b422006d639c4d1e8a543fe77
Anwesh43 <anweshthecool0@gmail.com>
2021-08-06 02:53:27 +0530

    adding BiArcJoinMoveView class

--- biarcjoinmoveview/src/main/java/com/example/biarcjoinmoveview/BiArcJoinMoveView.kt
+++ biarcjoinmoveview/src/main/java/com/example/biarcjoinmoveview/BiArcJoinMoveView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawBAJMNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBAJMNode(i, scale, paint)
 }
+
+class BiArcJoinMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
