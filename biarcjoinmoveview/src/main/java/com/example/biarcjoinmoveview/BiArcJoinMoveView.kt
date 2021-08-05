8bdc020a84d80e7f8fdb034cc76867612c05bf7b
Anwesh43 <anweshthecool0@gmail.com>
2021-08-06 02:30:32 +0530

    adding constants and libraries

---
+++ biarcjoinmoveview/src/main/java/com/example/biarcjoinmoveview/BiArcJoinMoveView.kt
@@ -0,0 +1,27 @@
+package com.example.biarcjoinmoveview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.03f / parts
+val sizeFactor : Float = 4.3f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val rot : Float = 180f
+val deg : Float = 90f
