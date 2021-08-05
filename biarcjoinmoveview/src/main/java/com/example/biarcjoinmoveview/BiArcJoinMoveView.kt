b8720ed2765614322e3e275b2ae0dd172d5242f7
Anwesh43 <anweshthecool0@gmail.com>
2021-08-06 03:10:56 +0530

    adding BiArcJoiNmove class

--- biarcjoinmoveview/src/main/java/com/example/biarcjoinmoveview/BiArcJoinMoveView.kt
+++ biarcjoinmoveview/src/main/java/com/example/biarcjoinmoveview/BiArcJoinMoveView.kt
@@ -158,4 +158,27 @@ class BiArcJoinMoveView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiArcJoinMove(var i : Int) {
+
+        private var curr : BAJMNode = BAJMNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
