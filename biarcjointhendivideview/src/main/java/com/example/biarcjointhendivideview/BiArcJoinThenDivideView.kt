5de42f2d78a316956a373c7481c8176b49feadd6
Anwesh43 <anweshthecool0@gmail.com>
2021-08-07 05:23:26 +0530

    adding canvas extension methods

--- biarcjointhendivideview/src/main/java/com/example/biarcjointhendivideview/BiArcJoinThenDivideView.kt
+++ biarcjointhendivideview/src/main/java/com/example/biarcjointhendivideview/BiArcJoinThenDivideView.kt
@@ -18,8 +18,8 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor("#BDBDBD")
 }.toTypedArray()
-val parts : Int = 3
-val scGap : Float = 0.03f / parts
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
 val sizeFactor : Float = 4.9f
 val deg : Float = 180f
 val backColor : Int = Color.parseColor("#BDBDBD")
@@ -27,3 +27,29 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBiArcJoinThenDivide(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        translate((w / 2 - size) * (1 - sc2 + sc3), 0f)
+        rotate(deg * sc3)
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -deg / 2, deg * (sc1 - sc4), true, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBAJTDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBiArcJoinThenDivide(scale, w, h, paint)
+}
