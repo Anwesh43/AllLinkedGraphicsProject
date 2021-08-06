75cf511df9d79d338ffd04b3fc10b76b27cc72cc
Anwesh43 <anweshthecool0@gmail.com>
2021-08-07 03:54:34 +0530

    adding Int and Float extension methods

--- biarcjointhendivideview/src/main/java/com/example/biarcjointhendivideview/BiArcJoinThenDivideView.kt
+++ biarcjointhendivideview/src/main/java/com/example/biarcjointhendivideview/BiArcJoinThenDivideView.kt
@@ -23,3 +23,7 @@ val scGap : Float = 0.03f / parts
 val sizeFactor : Float = 4.9f
 val deg : Float = 180f
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
