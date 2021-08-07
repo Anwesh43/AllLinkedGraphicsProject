6d807ecb58adc68b52919f231d05aa4ada69c004
Anwesh43 <anweshthecool0@gmail.com>
2021-08-08 01:47:24 +0530

    adding TPTSNode class

--- tripathtosquareview/src/main/java/com/example/tripathtosquareview/TriPathToSquareView.kt
+++ tripathtosquareview/src/main/java/com/example/tripathtosquareview/TriPathToSquareView.kt
@@ -129,4 +129,45 @@ class TriPathToSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TPTSNode(var i : Int, val state : State = State()) {
+
+        private var prev : TPTSNode? = null
+        private var next : TPTSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TPTSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTPTSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TPTSNode {
+            var curr : TPTSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
