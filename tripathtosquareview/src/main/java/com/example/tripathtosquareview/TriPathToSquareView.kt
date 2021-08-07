d2bdbf766a3660f5988ac6a67f95f5030bbf7584
Anwesh43 <anweshthecool0@gmail.com>
2021-08-08 01:40:50 +0530

    adding TriPathToSquareView class

--- tripathtosquareview/src/main/java/com/example/tripathtosquareview/TriPathToSquareView.kt
+++ tripathtosquareview/src/main/java/com/example/tripathtosquareview/TriPathToSquareView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawTPTSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTriPathToSquare(scale, w, h, paint)
 }
+
+class TriPathToSquareView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
