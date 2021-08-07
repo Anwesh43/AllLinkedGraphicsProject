99981649ce049947f155bc8bd0c6116b4c32e253
Anwesh43 <anweshthecool0@gmail.com>
2021-08-07 05:34:36 +0530

    adding Animator class

--- biarcjointhendivideview/src/main/java/com/example/biarcjointhendivideview/BiArcJoinThenDivideView.kt
+++ biarcjointhendivideview/src/main/java/com/example/biarcjointhendivideview/BiArcJoinThenDivideView.kt
@@ -23,6 +23,7 @@ val scGap : Float = 0.04f / parts
 val sizeFactor : Float = 4.9f
 val deg : Float = 180f
 val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -85,6 +86,34 @@ class BiArcJoinThenDivideView(ctx : Context) : View(ctx) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
