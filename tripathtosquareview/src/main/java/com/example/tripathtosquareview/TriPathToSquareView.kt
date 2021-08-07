c2af4f1c76dc43fcb13960b64ad66dd35f21403e
Anwesh43 <anweshthecool0@gmail.com>
2021-08-08 01:39:28 +0530

    adding Canvas extension methods

--- tripathtosquareview/src/main/java/com/example/tripathtosquareview/TriPathToSquareView.kt
+++ tripathtosquareview/src/main/java/com/example/tripathtosquareview/TriPathToSquareView.kt
@@ -28,3 +28,41 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n))  * n
+
+fun Canvas.drawAt(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTriPathToSquare(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val x : Float = size * 0.5f * sc2
+    drawAt(w / 2 + (w / 2 + size / 2) * sc3, h / 2) {
+        rotate(deg * sc3)
+        drawAt(0f, -size / 2) {
+            drawLine(-size * 0.5f * sc1, 0f, size * 0.5f * sc1, 0f, paint)
+        }
+        drawAt(0f, size / 2) {
+            drawLine(-x, 0f, x, 0f, paint)
+            for (j in 0..1) {
+                drawAt(x * (1f - 2 * j), 0f) {
+                    drawLine(0f, 0f, 0f, -size * sc2, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawTPTSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTriPathToSquare(scale, w, h, paint)
+}
