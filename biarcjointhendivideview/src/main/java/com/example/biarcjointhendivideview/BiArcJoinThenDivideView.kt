78815b199eee61a566edfdf0f1e0329ff9553d16
Anwesh43 <anweshthecool0@gmail.com>
2021-08-07 05:39:38 +0530

    adding BAJTDNode clas

--- biarcjointhendivideview/src/main/java/com/example/biarcjointhendivideview/BiArcJoinThenDivideView.kt
+++ biarcjointhendivideview/src/main/java/com/example/biarcjointhendivideview/BiArcJoinThenDivideView.kt
@@ -117,4 +117,45 @@ class BiArcJoinThenDivideView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BAJTDNode(var i : Int, val state : State = State()) {
+
+        private var next : BAJTDNode? = null
+        private var prev : BAJTDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BAJTDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBAJTDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BAJTDNode {
+            var curr : BAJTDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
