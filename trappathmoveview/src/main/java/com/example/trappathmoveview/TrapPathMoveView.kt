48e41c4fd27eab713e5bc36803a3fd034f0bee23
Anwesh43 <anweshthecool0@gmail.com>
2021-08-09 00:57:51 +0530

    adding TrapPathMoveView class

--- trappathmoveview/src/main/java/com/example/trappathmoveview/TrapPathMoveView.kt
+++ trappathmoveview/src/main/java/com/example/trappathmoveview/TrapPathMoveView.kt
@@ -7,6 +7,8 @@ import android.graphics.Paint
 import android.graphics.Color
 import android.graphics.RectF
 import android.graphics.Canvas
+import android.content.Context
+
 
 val colors : Array<Int> = arrayOf(
     "#1A237E",
@@ -55,4 +57,20 @@ fun Canvas.drawTPMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTrapPath(scale, w, h, paint)
+}
+
+class TrapPathMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
 }
