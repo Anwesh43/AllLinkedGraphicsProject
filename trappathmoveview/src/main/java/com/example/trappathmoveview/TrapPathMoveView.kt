27a3269b7292865fbc07565a00f05f29a152f3f6
Anwesh43 <anweshthecool0@gmail.com>
2021-08-09 00:54:52 +0530

    adding Canvas extension methods

--- trappathmoveview/src/main/java/com/example/trappathmoveview/TrapPathMoveView.kt
+++ trappathmoveview/src/main/java/com/example/trappathmoveview/TrapPathMoveView.kt
@@ -27,3 +27,32 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawTrapPath(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(w * 0.5f * sc3, 0f)
+        drawLine(0f, 0f, 0f, -size * 0.5f * sc1, paint)
+        drawLine(-size / 2, 0f, -size / 2, -size * sc1, paint)
+        drawLine(0f, 0f, -size * 0.5f * sc2, 0f, paint)
+        drawLine(0f, -size / 2, -size * 0.5f * sc3, -size / 2 - size * 0.5f * sc3, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawTPMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTrapPath(scale, w, h, paint)
+}
