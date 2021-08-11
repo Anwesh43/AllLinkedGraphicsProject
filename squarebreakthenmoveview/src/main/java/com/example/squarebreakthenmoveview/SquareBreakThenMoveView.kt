2522d9c74233f33d2499c7344c917e37c66b2ca6
Anwesh43 <anweshthecool0@gmail.com>
2021-08-12 01:07:57 +0530

    adding Canvas extension methods

--- squarebreakthenmoveview/src/main/java/com/example/squarebreakthenmoveview/SquareBreakThenMoveView.kt
+++ squarebreakthenmoveview/src/main/java/com/example/squarebreakthenmoveview/SquareBreakThenMoveView.kt
@@ -29,3 +29,34 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSquareBreakThenMove(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2) * sc4)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f,1f - 2 * j)
+        save()
+        translate(0f, -size * sc2)
+        drawRect(RectF(0f, -size * sc1, size, 0f), paint)
+        restore()
+        drawLine(0f, 0f, 0f, -size * sc2, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSBTMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSquareBreakThenMove(scale, w, h, paint)
+}
