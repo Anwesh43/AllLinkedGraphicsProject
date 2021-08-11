13d50a0d2b6e7963ccda619f245bb7efd5763b5a
Anwesh43 <anweshthecool0@gmail.com>
2021-08-12 01:39:26 +0530

    adding renderer in view

--- squarebreakthenmoveview/src/main/java/com/example/squarebreakthenmoveview/SquareBreakThenMoveView.kt
+++ squarebreakthenmoveview/src/main/java/com/example/squarebreakthenmoveview/SquareBreakThenMoveView.kt
@@ -9,7 +9,7 @@ import android.graphics.Color
 import android.app.Activity
 import android.content.Context
 
-val colors : Array<Int> = arrayOf(
+val colors: Array<Int> = arrayOf(
     "#1A237E",
     "#EF5350",
     "#AA00FF",
@@ -18,30 +18,30 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
-val scGap : Float = 0.04f / parts
-val strokeFactor : Float = 90f
-val sizeFactor : Float = 4.2f
-val delay : Long = 20
-val backColor : Int = Color.parseColor("#BDBDBD")
-val deg : Float = 90f
+val parts: Int = 4
+val scGap: Float = 0.04f / parts
+val strokeFactor: Float = 90f
+val sizeFactor: Float = 4.2f
+val delay: Long = 20
+val backColor: Int = Color.parseColor("#BDBDBD")
+val deg: Float = 90f
 
-fun Int.inverse() : Float = 1f / this
-fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
-fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Int.inverse(): Float = 1f / this
+fun Float.maxScale(i: Int, n: Int): Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i: Int, n: Int): Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
-fun Canvas.drawSquareBreakThenMove(scale : Float, w : Float, h : Float, paint : Paint) {
-    val size : Float = Math.min(w, h) / sizeFactor
-    val sc1 : Float = scale.divideScale(0, parts)
-    val sc2 : Float = scale.divideScale(1, parts)
-    val sc3 : Float = scale.divideScale(2, parts)
-    val sc4 : Float = scale.divideScale(3, parts)
+fun Canvas.drawSquareBreakThenMove(scale: Float, w: Float, h: Float, paint: Paint) {
+    val size: Float = Math.min(w, h) / sizeFactor
+    val sc1: Float = scale.divideScale(0, parts)
+    val sc2: Float = scale.divideScale(1, parts)
+    val sc3: Float = scale.divideScale(2, parts)
+    val sc4: Float = scale.divideScale(3, parts)
     save()
     translate(w / 2, h / 2 + (h / 2) * sc4)
     rotate(deg * sc3)
     for (j in 0..1) {
         save()
-        scale(1f,1f - 2 * j)
+        scale(1f, 1f - 2 * j)
         save()
         translate(0f, -size * sc2)
         drawRect(RectF(0f, -size * sc1, size, 0f), paint)
@@ -52,33 +52,35 @@ fun Canvas.drawSquareBreakThenMove(scale : Float, w : Float, h : Float, paint : 
     restore()
 }
 
-fun Canvas.drawSBTMNode(i : Int, scale : Float, paint : Paint) {
-    val w : Float = width.toFloat()
-    val h : Float = height.toFloat()
+fun Canvas.drawSBTMNode(i: Int, scale: Float, paint: Paint) {
+    val w: Float = width.toFloat()
+    val h: Float = height.toFloat()
     paint.color = colors[i]
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSquareBreakThenMove(scale, w, h, paint)
 }
 
-class SquareBreakThenMoveView(ctx : Context) : View(ctx) {
+class SquareBreakThenMoveView(ctx: Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer: Renderer = Renderer(this)
 
+    override fun onDraw(canvas: Canvas) {
+        renderer.render(canvas)
     }
 
-    override fun onTouchEvent(event : MotionEvent) : Boolean {
+    override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
     }
 
-    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+    data class State(var scale: Float = 0f, var dir: Float = 0f, var prevScale: Float = 0f) {
 
-        fun update(cb : (Float) -> Unit) {
+        fun update(cb: (Float) -> Unit) {
             scale += scGap * dir
             if (Math.abs(scale - prevScale) > 1) {
                 scale = prevScale + dir
@@ -88,125 +90,125 @@ class SquareBreakThenMoveView(ctx : Context) : View(ctx) {
             }
         }
 
-        fun startUpdating(cb : () -> Unit) {
+        fun startUpdating(cb: () -> Unit) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
             }
         }
+    }
 
-        data class Animator(var view : View, var animated : Boolean = false) {
+    data class Animator(var view: View, var animated: Boolean = false) {
 
-            fun animate(cb : () -> Unit) {
-                if (animated) {
-                    cb()
-                    try {
-                        Thread.sleep(delay)
-                        view.invalidate()
-                    } catch(ex : Exception) {
-
-                    }
-                }
-            }
-
-            fun start() {
-                if (!animated) {
-                    animated = true
-                    view.postInvalidate()
-                }
-            }
-
-            fun stop() {
-                if (animated) {
-                    animated = false
-                }
-            }
-        }
-
-        data class SBTMNode(var i : Int, val state : State = State()) {
-
-            private var prev : SBTMNode? = null
-            private var next : SBTMNode? = null
-
-            init {
-                addNeighbor()
-            }
-
-            fun addNeighbor() {
-                if (i < colors.size - 1) {
-                    next = SBTMNode(i + 1)
-                    next?.prev = this
-                }
-            }
-
-            fun draw(canvas : Canvas, paint : Paint) {
-                canvas.drawSBTMNode(i, state.scale, paint)
-            }
-
-            fun update(cb : (Float) -> Unit) {
-                state.update(cb)
-            }
-
-            fun startUpdating(cb : () -> Unit) {
-                state.startUpdating(cb)
-            }
-
-            fun getNext(dir : Int, cb : () -> Unit) : SBTMNode {
-                var curr : SBTMNode? = prev
-                if (dir == 1) {
-                    curr = next
-                }
-                if (curr != null) {
-                    return curr
-                }
+        fun animate(cb: () -> Unit) {
+            if (animated) {
                 cb()
-                return this
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch (ex: Exception) {
+
+                }
             }
         }
 
-        data class SquareBreakThenMove(var i : Int) {
-
-            private var curr : SBTMNode = SBTMNode(0)
-            private var dir : Int = 1
-
-            fun draw(canvas : Canvas, paint : Paint) {
-                curr.draw(canvas, paint)
-            }
-
-            fun update(cb : (Float) -> Unit) {
-                curr.update {
-                    curr = curr.getNext(dir) {
-                        dir *= -1
-                    }
-                    cb(it)
-                }
-            }
-
-            fun startUpdating(cb : () -> Unit) {
-                curr.startUpdating(cb)
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
             }
         }
 
-        data class Renderer(var view : SquareBreakThenMoveView) {
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 
-            private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-            private val sbtm : SquareBreakThenMove = SquareBreakThenMove(0)
-            private val animator : Animator = Animator(view)
+    data class SBTMNode(var i: Int, val state: State = State()) {
 
-            fun render(canvas : Canvas) {
-                canvas.drawColor(backColor)
-                sbtm.draw(canvas, paint)
-                animator.animate {
-                    sbtm.update {
-                        animator.stop()
-                    }
+        private var prev: SBTMNode? = null
+        private var next: SBTMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SBTMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas: Canvas, paint: Paint) {
+            canvas.drawSBTMNode(i, state.scale, paint)
+        }
+
+        fun update(cb: (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb: () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir: Int, cb: () -> Unit): SBTMNode {
+            var curr: SBTMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
+
+    data class SquareBreakThenMove(var i: Int) {
+
+        private var curr: SBTMNode = SBTMNode(0)
+        private var dir: Int = 1
+
+        fun draw(canvas: Canvas, paint: Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb: (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb: () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
+
+    data class Renderer(var view: SquareBreakThenMoveView) {
+
+        private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val sbtm: SquareBreakThenMove = SquareBreakThenMove(0)
+        private val animator: Animator = Animator(view)
+
+        fun render(canvas: Canvas) {
+            canvas.drawColor(backColor)
+            sbtm.draw(canvas, paint)
+            animator.animate {
+                sbtm.update {
+                    animator.stop()
                 }
             }
+        }
 
-            fun handleTap() {
-                sbtm.startUpdating {
-                    animator.start()
-                }
+        fun handleTap() {
+            sbtm.startUpdating {
+                animator.start()
             }
         }
     }
