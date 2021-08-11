f932f3dffef21f356243fbe756f8750eda39e324
Anwesh43 <anweshthecool0@gmail.com>
2021-08-12 01:32:25 +0530

    adding SquareBreakThenMove class

--- squarebreakthenmoveview/src/main/java/com/example/squarebreakthenmoveview/SquareBreakThenMoveView.kt
+++ squarebreakthenmoveview/src/main/java/com/example/squarebreakthenmoveview/SquareBreakThenMoveView.kt
@@ -163,5 +163,28 @@ class SquareBreakThenMoveView(ctx : Context) : View(ctx) {
                 return this
             }
         }
+
+        data class SquareBreakThenMove(var i : Int) {
+
+            private var curr : SBTMNode = SBTMNode(0)
+            private var dir : Int = 1
+
+            fun draw(canvas : Canvas, paint : Paint) {
+                curr.draw(canvas, paint)
+            }
+
+            fun update(cb : (Float) -> Unit) {
+                curr.update {
+                    curr = curr.getNext(dir) {
+                        dir *= -1
+                    }
+                    cb(it)
+                }
+            }
+
+            fun startUpdating(cb : () -> Unit) {
+                curr.startUpdating(cb)
+            }
+        }
     }
 }
