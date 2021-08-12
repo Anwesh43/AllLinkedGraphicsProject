d01d4bca564df09ea865d02ce28a8c3818f25c43
Anwesh43 <anweshthecool0@gmail.com>
2021-08-13 03:11:40 +0530

    adding RSTSNode class

--- rotswitchtosquareview/src/main/java/com/example/rotswitchtosquareview/RotSwitchToSquareView.kt
+++ rotswitchtosquareview/src/main/java/com/example/rotswitchtosquareview/RotSwitchToSquareView.kt
@@ -124,4 +124,45 @@ class RotSwitchToSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RSTSNode(var i : Int, val state : State = State()) {
+
+        private var prev : RSTSNode? = null
+        private var next : RSTSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RSTSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRSTSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RSTSNode {
+            var curr : RSTSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
