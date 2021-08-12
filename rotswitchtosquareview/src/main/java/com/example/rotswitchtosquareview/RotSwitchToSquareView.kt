2ef2626c2145343809a04f754e3d4444cec1e07e
Anwesh43 <anweshthecool0@gmail.com>
2021-08-13 02:58:06 +0530

    adding Canvas extension methods

--- rotswitchtosquareview/src/main/java/com/example/rotswitchtosquareview/RotSwitchToSquareView.kt
+++ rotswitchtosquareview/src/main/java/com/example/rotswitchtosquareview/RotSwitchToSquareView.kt
@@ -28,3 +28,37 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawRotSwitchToSquare(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        translate(0f, size / 4)
+        rotate(rot * sc3)
+        drawLine(-size * 0.5f * sc1, -size / 4, size * 0.5f * sc1, -size / 4, paint)
+        for (k in 0..1) {
+            save()
+            scale(1f - 2 * k, 1f)
+            translate(0f, -size / 4)
+            drawLine(0f, 0f, 0f, -size * 0.5f * sc2, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+fun Canvas.drawRSTSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRotSwitchToSquare(scale, w, h, paint)
+}
