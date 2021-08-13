c87d7ec6d23375a6beea607c326bc8123edad284
Anwesh43 <anweshthecool0@gmail.com>
2021-08-14 04:46:39 +0530

    adding Canvas extension methods

--- crosstouparrowview/src/main/java/com/example/crosstouparrowview/CrossToUpArrowView.kt
+++ crosstouparrowview/src/main/java/com/example/crosstouparrowview/CrossToUpArrowView.kt
@@ -28,3 +28,33 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawCrossToUpArrow(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val y : Float = size * 0.25f
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        translate(0f, -y)
+        rotate(180f * sc2)
+        drawLine(0f, y, -size * 0.5f * sc1, y - size * 0.5f * sc1, paint)
+        drawLine(0f, y, size * 0.5f * sc1, y - size * 0.5f * sc1, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawCTUANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawCrossToUpArrow(scale, w, h, paint)
+}
