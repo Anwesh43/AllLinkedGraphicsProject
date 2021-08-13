e7a17b58dd9193d0d52adc1f1aece919658e5cb0
Anwesh43 <anweshthecool0@gmail.com>
2021-08-14 04:27:52 +0530

    adding constants and libraries

--- crosstouparrowview/src/main/java/com/example/crosstouparrowview/CrossToUpArrowView.kt
+++ crosstouparrowview/src/main/java/com/example/crosstouparrowview/CrossToUpArrowView.kt
@@ -1,2 +1,26 @@
 package com.example.crosstouparrowview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.2f
+val scGap : Float = 0.04f / parts
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 180f
