9a952c327b3c4632c3d240db5fba6a5257b06cf6
Anwesh43 <anweshthecool0@gmail.com>
2021-08-14 05:20:59 +0530

    adding CTUANode class

--- crosstouparrowview/src/main/java/com/example/crosstouparrowview/CrossToUpArrowView.kt
+++ crosstouparrowview/src/main/java/com/example/crosstouparrowview/CrossToUpArrowView.kt
@@ -121,4 +121,45 @@ class CrossToUpArrowView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CTUANode(var i : Int, val state : State = State()) {
+
+        private var next : CTUANode? = null
+        private var prev : CTUANode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CTUANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCTUANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CTUANode {
+            var curr : CTUANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
