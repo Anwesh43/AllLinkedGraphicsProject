441314cbfc25d7b70f8ffcbe9065f323da8c08de
Anwesh43 <anweshthecool0@gmail.com>
2021-08-15 03:19:19 +0530

    adding BFCTLNode class

--- bifillcirctolineview/src/main/java/com/example/bifillcirctolineview/BiFillCircToLineView.kt
+++ bifillcirctolineview/src/main/java/com/example/bifillcirctolineview/BiFillCircToLineView.kt
@@ -125,4 +125,45 @@ class BiFillCircToLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BFCTLNode(var i : Int, val state : State = State()) {
+
+        private var next : BFCTLNode? = null
+        private var prev : BFCTLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BFCTLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBFCTLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BFCTLNode {
+            var curr : BFCTLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
