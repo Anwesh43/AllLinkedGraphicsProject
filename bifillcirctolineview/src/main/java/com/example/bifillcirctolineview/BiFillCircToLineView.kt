70b8a58c05deee33abadd92b08db652b5f7cfb7d
Anwesh43 <anweshthecool0@gmail.com>
2021-08-15 03:07:09 +0530

    adding Canvas extension methods

--- bifillcirctolineview/src/main/java/com/example/bifillcirctolineview/BiFillCircToLineView.kt
+++ bifillcirctolineview/src/main/java/com/example/bifillcirctolineview/BiFillCircToLineView.kt
@@ -30,3 +30,35 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBiFillCircToLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate((w / 2 + size) * sc4, 0f)
+        drawArc(RectF(-size / 2,-size /2, size / 2, size / 2), -deg / 2, deg * sc1, false, paint)
+        if (sc2 > 0f) {
+            drawLine(0f, 0f, size * sc2, 0f, paint)
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBFCTLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiFillCircToLine(scale, w, h, paint)
+}
