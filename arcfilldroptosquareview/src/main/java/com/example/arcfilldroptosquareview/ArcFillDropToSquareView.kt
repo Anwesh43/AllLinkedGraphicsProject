e9c2f53b61068cf8fb7a07f95472b86030bb5933
Anwesh43 <anweshthecool0@gmail.com>
2021-08-17 02:26:54 +0530

    adding constants and libraries

--- arcfilldroptosquareview/src/main/java/com/example/arcfilldroptosquareview/ArcFillDropToSquareView.kt
+++ arcfilldroptosquareview/src/main/java/com/example/arcfilldroptosquareview/ArcFillDropToSquareView.kt
@@ -1,2 +1,26 @@
 package com.example.arcfilldroptosquareview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val delay : Long = 20
+val sizeFactor : Float = 5.6f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
