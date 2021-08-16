68be1e1088db3f07302e50bcf28b35a9c20ea77a
Anwesh43 <anweshthecool0@gmail.com>
2021-08-17 02:47:11 +0530

    adding AFDTSNode class

--- arcfilldroptosquareview/src/main/java/com/example/arcfilldroptosquareview/ArcFillDropToSquareView.kt
+++ arcfilldroptosquareview/src/main/java/com/example/arcfilldroptosquareview/ArcFillDropToSquareView.kt
@@ -121,4 +121,45 @@ class ArcFillDropToSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AFDTSNode(var i : Int, val state : State = State()) {
+
+        private var prev : AFDTSNode? = null
+        private var next : AFDTSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AFDTSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAFDTSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AFDTSNode {
+            var curr : AFDTSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
