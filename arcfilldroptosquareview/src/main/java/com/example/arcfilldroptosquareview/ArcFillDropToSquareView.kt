141a638e17cba5417948c4b5e9a535c7489ccf5c
Anwesh43 <anweshthecool0@gmail.com>
2021-08-17 02:38:13 +0530

    adding ArcFillDropToSquareView class

--- arcfilldroptosquareview/src/main/java/com/example/arcfilldroptosquareview/ArcFillDropToSquareView.kt
+++ arcfilldroptosquareview/src/main/java/com/example/arcfilldroptosquareview/ArcFillDropToSquareView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawAFDTSNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawArcFillDropToSquare(scale, w, h, paint)
 }
+
+class ArcFillDropToSquareView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
