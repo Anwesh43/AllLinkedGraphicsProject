5bb8b86f6ad583b0d95ceb0bf64ef28d088ff64b
Anwesh43 <anweshthecool0@gmail.com>
2021-08-18 00:59:44 +0530

    adding PieStrokeThenFillView class

--- piestrokethenfillview/src/main/java/com/example/piestrokethenfillview/PieStrokeThenFillView.kt
+++ piestrokethenfillview/src/main/java/com/example/piestrokethenfillview/PieStrokeThenFillView.kt
@@ -57,3 +57,18 @@ fun Canvas.drawPSTFNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawPieStrokeThenFill(scale, w, h, paint)
 }
+class PieStrokeThenFillView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
