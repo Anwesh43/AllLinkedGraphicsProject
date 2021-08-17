9f8e6dc6f67d181ff89436cb66d4234cdb4cf668
Anwesh43 <anweshthecool0@gmail.com>
2021-08-18 00:55:20 +0530

    adding Canvas extension methods

--- piestrokethenfillview/src/main/java/com/example/piestrokethenfillview/PieStrokeThenFillView.kt
+++ piestrokethenfillview/src/main/java/com/example/piestrokethenfillview/PieStrokeThenFillView.kt
@@ -29,3 +29,31 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawPieStrokeThenFill(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, size / 2 + (h + size) * sc4)
+    paint.style = Paint.Style.STROKE
+    drawArc(RectF(
+        -size / 2, -size / 2, size / 2, size / 2
+    ), 0f, rot * sc1, false, paint)
+    paint.style = Paint.Style.FILL
+    drawArc(RectF(
+        -size / 2, -size / 2, size / 2, size / 2
+    ), rot * sc3, rot * (sc2 - sc3), true, paint)
+    restore()
+}
+
+fun Canvas.drawPSTFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawPieStrokeThenFill(scale, w, h, paint)
+}
