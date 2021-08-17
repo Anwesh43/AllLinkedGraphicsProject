1b858aadf0d2c11313bac9c9c72fb049950f7a91
Anwesh43 <anweshthecool0@gmail.com>
2021-08-18 00:33:29 +0530

    adding constants and libraries

---
+++ piestrokethenfillview/src/main/java/com/example/piestrokethenfillview/PieStrokeThenFillView.kt
@@ -0,0 +1,27 @@
+package com.example.piestrokethenfillview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val delay : Long = 20
+val sizeFactor : Float = 4.2f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 360f
+val strokeFactor : Float = 90f
