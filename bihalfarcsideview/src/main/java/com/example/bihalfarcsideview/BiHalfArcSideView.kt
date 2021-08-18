39ca67b96aadbff0b3c81adb7c152fe57abb437a
Anwesh43 <anweshthecool0@gmail.com>
2021-08-19 02:25:23 +0530

    adding Canvas extension methods

--- bihalfarcsideview/src/main/java/com/example/bihalfarcsideview/BiHalfArcSideView.kt
+++ bihalfarcsideview/src/main/java/com/example/bihalfarcsideview/BiHalfArcSideView.kt
@@ -29,3 +29,30 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBiHalfArcSide(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+    for (j in 0..1) {
+        save()
+        translate(size * 0.5f * (1 - 2 * j) * (1 - sc3), 0f)
+        drawArc(
+            RectF(-size / 2, -size / 2, size / 2, size / 2),
+            180f * j, 180f * scale.divideScale(j, parts),
+            true,
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBHASNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBiHalfArcSide(scale, w, h, paint)
+}
