aa193bb911d2aad0899164c5dff6f9bea1c56cd4
Anwesh43 <anweshthecool0@gmail.com>
2021-08-20 01:06:39 +0530

    adding Renderer class

--- squarecreatedropview/src/main/java/com/example/squarecreatedropview/SquareCreateDropView.kt
+++ squarecreatedropview/src/main/java/com/example/squarecreatedropview/SquareCreateDropView.kt
@@ -189,4 +189,27 @@ class SquareCreateDropView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SquareCreateDropView) {
+
+        private val animator : Animator = Animator(view)
+        private val scd : SquareCreateDrop = SquareCreateDrop(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            scd.draw(canvas, paint)
+            animator.animate {
+                scd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            scd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
