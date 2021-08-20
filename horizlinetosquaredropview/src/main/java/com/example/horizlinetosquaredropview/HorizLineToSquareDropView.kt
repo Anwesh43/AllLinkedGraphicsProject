4ad16dd5be76d061c125bfab718fcc3150600cac
Anwesh43 <anweshthecool0@gmail.com>
2021-08-21 03:28:24 +0530

    adding Renderer class

--- horizlinetosquaredropview/src/main/java/com/example/horizlinetosquaredropview/HorizLineToSquareDropView.kt
+++ horizlinetosquaredropview/src/main/java/com/example/horizlinetosquaredropview/HorizLineToSquareDropView.kt
@@ -183,4 +183,27 @@ class HorizLineToSquareDropView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : HorizLineToSquareDropView) {
+
+        private val animator : Animator = Animator(view)
+        private val hltsd : HorizLineToSquareDrop = HorizLineToSquareDrop(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            hltsd.draw(canvas, paint)
+            animator.animate {
+                hltsd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            hltsd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
