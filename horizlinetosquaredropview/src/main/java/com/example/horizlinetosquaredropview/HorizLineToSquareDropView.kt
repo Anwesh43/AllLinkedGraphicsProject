134a2d104a61416039947c5493614fa4ffa0cdaf
Anwesh43 <anweshthecool0@gmail.com>
2021-08-21 03:02:14 +0530

    adding Canvas extension methods

--- horizlinetosquaredropview/src/main/java/com/example/horizlinetosquaredropview/HorizLineToSquareDropView.kt
+++ horizlinetosquaredropview/src/main/java/com/example/horizlinetosquaredropview/HorizLineToSquareDropView.kt
@@ -29,3 +29,31 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawHorizLineToSquare(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+    save()
+    rotate(-deg * sc2)
+    translate(-size - w / 2 + (w / 2 + size) * sc1, 0f)
+    drawLine(0f, 0f, size, 0f, paint)
+    restore()
+    drawRect(RectF(0f, -size, size * sc3, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawHLTDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    drawHorizLineToSquare(scale, w, h, paint)
+}
+
