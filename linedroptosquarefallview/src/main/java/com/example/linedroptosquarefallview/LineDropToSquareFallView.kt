fd5e94a959ad3f144fd31752d63924648527fc9e
Anwesh43 <anweshthecool0@gmail.com>
2021-08-22 02:11:37 +0530

    adding LineDropToSquare class

--- linedroptosquarefallview/src/main/java/com/example/linedroptosquarefallview/LineDropToSquareFallView.kt
+++ linedroptosquarefallview/src/main/java/com/example/linedroptosquarefallview/LineDropToSquareFallView.kt
@@ -159,4 +159,27 @@ class LineDropToSquareFallView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineDropToSquareFall(var i : Int) {
+
+        private var curr : LDTSFNode = LDTSFNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun udpate(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
