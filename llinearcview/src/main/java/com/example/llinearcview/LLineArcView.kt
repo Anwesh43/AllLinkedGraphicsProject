4a83aaa761e80c22642d97cc75de95622e0dc0a0
Anwesh43 <anweshthecool0@gmail.com>
2021-08-26 17:28:35 +0530

    adding LLANode class

--- llinearcview/src/main/java/com/example/llinearcview/LLineArcView.kt
+++ llinearcview/src/main/java/com/example/llinearcview/LLineArcView.kt
@@ -127,5 +127,47 @@ class LLineArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LLANode(var i : Int, val state : State = State()) {
+
+        private var next : LLANode? = null
+        private var prev : LLANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LLANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLLANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LLANode {
+            var curr : LLANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+    }
 }
 
