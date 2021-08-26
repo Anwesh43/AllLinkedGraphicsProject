47b7de1cdab7e768b5d2aa86282e25d540103e12
Anwesh43 <anweshthecool0@gmail.com>
2021-08-27 01:25:41 +0530

    adding Canvas extension methods

--- balldownfromdiagview/src/main/java/com/example/balldownfromdiagview/BallDownFromDiagView.kt
+++ balldownfromdiagview/src/main/java/com/example/balldownfromdiagview/BallDownFromDiagView.kt
@@ -26,3 +26,39 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBallDownFromDiag(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2  + (h / 2 + size) * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawCircle(
+            -w * 0.25f * sc2,
+            -h * 0.25f * (1 - sc2),
+            size * sc1,
+            paint
+        )
+        drawLine(
+            -w * 0.25f * sc3,
+            -h * 0.25f + h * 0.25f * sc3,
+            -w * 0.25f * sc2,
+            -h * 0.25f + h * 0.25f * sc2,
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBDFDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBallDownFromDiag(scale, w, h, paint)
+}
