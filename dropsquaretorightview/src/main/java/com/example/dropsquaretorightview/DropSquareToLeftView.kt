9006a58cc53913f63c175d6e42405416248d78eb
Anwesh43 <anweshthecool0@gmail.com>
2021-08-29 02:17:00 +0530

    adding constants and libraries

---
+++ dropsquaretorightview/src/main/java/com/example/dropsquaretorightview/DropSquareToLeftView.kt
@@ -0,0 +1,26 @@
+package com.example.dropsquaretorightview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#304FFE",
+    "#C51162",
+    "#F57F17",
+    "#00C853",
+    "#FFD600"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
