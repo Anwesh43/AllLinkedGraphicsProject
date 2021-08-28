cfc8fae6065b0666a05add5d4bd78066e1dc2b97
Anwesh43 <anweshthecool0@gmail.com>
2021-08-29 02:39:33 +0530

    adding DSTLNode class

--- dropsquaretorightview/src/main/java/com/example/dropsquaretorightview/DropSquareToLeftView.kt
+++ dropsquaretorightview/src/main/java/com/example/dropsquaretorightview/DropSquareToLeftView.kt
@@ -118,4 +118,45 @@ class DropSquareToLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DSTLNode(var i : Int, val state : State = State()) {
+
+        private var next : DSTLNode? = null
+        private var prev : DSTLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DSTLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDSTLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DSTLNode {
+            var curr : DSTLNode? = prev
+            if (dir == 1) {
+                curr = prev
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
