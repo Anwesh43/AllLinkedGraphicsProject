152a7532a51859f3a2f82e62b61aa8484dedb7e6
Anwesh43 <anweshthecool0@gmail.com>
2021-08-29 02:42:59 +0530

    adding Renderer class

--- dropsquaretorightview/src/main/java/com/example/dropsquaretorightview/DropSquareToLeftView.kt
+++ dropsquaretorightview/src/main/java/com/example/dropsquaretorightview/DropSquareToLeftView.kt
@@ -182,4 +182,27 @@ class DropSquareToLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : DropSquareToLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val dstl : DropSquareToLeft = DropSquareToLeft(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            dstl.draw(canvas, paint)
+            animator.animate {
+                dstl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            dstl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
