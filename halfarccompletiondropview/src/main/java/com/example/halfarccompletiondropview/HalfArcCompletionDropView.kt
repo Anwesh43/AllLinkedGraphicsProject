4e69748b3c204d1b3a9234e5cfdcb21e5e235c69
Anwesh43 <anweshthecool0@gmail.com>
2021-08-30 00:25:24 +0530

    adding HalfArcCompletionDropView class

--- halfarccompletiondropview/src/main/java/com/example/halfarccompletiondropview/HalfArcCompletionDropView.kt
+++ halfarccompletiondropview/src/main/java/com/example/halfarccompletiondropview/HalfArcCompletionDropView.kt
@@ -71,3 +71,19 @@ fun Canvas.drawHACDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawHalfArcCompletionDrop(scale, w, h, paint)
 }
+
+class HalfArcCompletionDropView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
