95d08d2aced4a5ba53529b7dd1b50beb8e6e1c86
Anwesh43 <anweshthecool0@gmail.com>
2021-08-30 00:15:38 +0530

    adding constants and libraries

---
+++ halfarccompletiondropview/src/main/java/com/example/halfarccompletiondropview/HalfArcCompletionDropView.kt
@@ -0,0 +1,28 @@
+package com.example.halfarccompletiondropview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#304FFE",
+    "#C51162",
+    "#F57F17",
+    "#00C853",
+    "#FFD600"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 4.9f
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
+val start : Float = -90f
