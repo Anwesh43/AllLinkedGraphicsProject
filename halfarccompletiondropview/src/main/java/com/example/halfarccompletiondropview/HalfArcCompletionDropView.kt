3c5fb131205b74a71274867800cecf9ea778e140
Anwesh43 <anweshthecool0@gmail.com>
2021-08-30 00:24:14 +0530

    adding Canvas extension methods

--- halfarccompletiondropview/src/main/java/com/example/halfarccompletiondropview/HalfArcCompletionDropView.kt
+++ halfarccompletiondropview/src/main/java/com/example/halfarccompletiondropview/HalfArcCompletionDropView.kt
@@ -30,3 +30,44 @@ val start : Float = -90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawHalfArcCompletionDrop(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        save()
+        translate((w / 2 - size / 2) * (1 - sc3), 0f)
+        drawArc(
+            RectF(-size / 2, -size / 2, size / 2, size / 2),
+            start,
+            deg * sc2,
+            false,
+            paint
+        )
+        restore()
+        for (k in 0..1) {
+            save()
+            translate(0f, -size / 2 + size * k)
+            drawLine(0f, 0f, size * (sc2 - sc3), 0f, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawHACDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawHalfArcCompletionDrop(scale, w, h, paint)
+}
