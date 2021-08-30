b6d2d3eaff46807d9b7f70805771a77904e4517d
Anwesh43 <anweshthecool0@gmail.com>
2021-08-31 02:07:34 +0530

    adding Canvas extension methods

--- circularsqjoinerdropview/src/main/java/com/example/circularsqjoinerdropview/CircularSqJoinerDropView.kt
+++ circularsqjoinerdropview/src/main/java/com/example/circularsqjoinerdropview/CircularSqJoinerDropView.kt
@@ -4,10 +4,7 @@ import android.view.View
 import android.view.MotionEvent
 import android.app.Activity
 import android.content.Context
-import android.graphics.Paint
-import android.graphics.Color
-import android.graphics.RectF
-import android.graphics.Canvas
+import android.graphics.*
 
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
@@ -29,3 +26,29 @@ val deg : Float = 270f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawCircularSqJoinerDrop(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+    for (j in 0..1) {
+        save()
+        rotate(90f * j)
+        translate(-size * 0.5f * j, -size * 0.5f)
+        drawLine(0f, 0f, size * 0.5f * scale.divideScale(j, parts), 0f, paint)
+        restore()
+    }
+    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, deg * sc3, false, paint)
+    restore()
+}
+
+fun Canvas.drawCSJDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawCircularSqJoinerDrop(scale, w , h, paint)
+}
