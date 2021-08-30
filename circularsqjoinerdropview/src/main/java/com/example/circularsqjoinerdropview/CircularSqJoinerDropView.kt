0202b686f71d22c91ebafe854f56ae40b13f838f
Anwesh43 <anweshthecool0@gmail.com>
2021-08-31 01:41:09 +0530

    adding constants and libraries

---
+++ circularsqjoinerdropview/src/main/java/com/example/circularsqjoinerdropview/CircularSqJoinerDropView.kt
@@ -0,0 +1,27 @@
+package com.example.circularsqjoinerdropview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val colors : Array<Int> = arrayOf(
+    "#304FFE",
+    "#C51162",
+    "#F57F17",
+    "#00C853",
+    "#FFD600"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 270f
