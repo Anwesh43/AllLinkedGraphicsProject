27384dcf1045deea19545022a6d100a74fc6253f
Anwesh43 <anweshthecool0@gmail.com>
2021-09-01 01:07:19 +0530

    adding LBDENode class

--- linebardropendview/src/main/java/com/example/linebardropendview/LineBarDropEndView.kt
+++ linebardropendview/src/main/java/com/example/linebardropendview/LineBarDropEndView.kt
@@ -121,4 +121,45 @@ class LineBarDropEndView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBDENode(var i : Int, val state : State = State()) {
+
+        private var next : LBDENode? = null
+        private var prev : LBDENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBDENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBDENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBDENode {
+            var curr : LBDENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
