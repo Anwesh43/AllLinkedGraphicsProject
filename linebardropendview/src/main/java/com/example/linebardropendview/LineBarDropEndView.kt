b6d723cdec4576871f9810c0733c1650c5793c36
Anwesh43 <anweshthecool0@gmail.com>
2021-09-01 00:39:55 +0530

    adding constants and libraries

---
+++ linebardropendview/src/main/java/com/example/linebardropendview/LineBarDropEndView.kt
@@ -0,0 +1,27 @@
+package com.example.linebardropendview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#304FFE",
+    "#C51162",
+    "#F57F17",
+    "#00C853",
+    "#FFD600"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
