95705d6347918a2cd1d3f4e47d0287b4ca84a0dc
Anwesh43 <anweshthecool0@gmail.com>
2021-09-03 03:00:15 +0530

    adding constants and libraries

---
+++ rotatelinethendropview/src/main/java/com/example/rotatelinethendropview/RotateThenLineDropView.kt
@@ -0,0 +1,26 @@
+package com.example.rotatelinethendropview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val rot : Float = 90f
+val colors : Array<Int> = arrayOf(
+    "#304FFE",
+    "#C51162",
+    "#F57F17",
+    "#00C853",
+    "#FFD600"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
