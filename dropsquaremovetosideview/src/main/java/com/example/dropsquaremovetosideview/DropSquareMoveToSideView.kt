ae3c4a86d6aa19f13cbe97d5ad96626133a87131
Anwesh43 <anweshthecool0@gmail.com>
2021-09-04 04:46:30 +0530

    adding DropSquareMoveToSideView class

--- dropsquaremovetosideview/src/main/java/com/example/dropsquaremovetosideview/DropSquareMoveToSideView.kt
+++ dropsquaremovetosideview/src/main/java/com/example/dropsquaremovetosideview/DropSquareMoveToSideView.kt
@@ -51,3 +51,19 @@ fun Canvas.drawDSMTSNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawDropSquareMoveToSide(scale, w, h, paint)
 }
+
+class DropSquareMoveToSideView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
