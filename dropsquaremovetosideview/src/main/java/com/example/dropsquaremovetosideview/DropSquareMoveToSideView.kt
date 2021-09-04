5ecbc15be234442fc8de969e3faeb0772cdae4fd
Anwesh43 <anweshthecool0@gmail.com>
2021-09-04 05:31:33 +0530

    adding DSMTSNode class

--- dropsquaremovetosideview/src/main/java/com/example/dropsquaremovetosideview/DropSquareMoveToSideView.kt
+++ dropsquaremovetosideview/src/main/java/com/example/dropsquaremovetosideview/DropSquareMoveToSideView.kt
@@ -114,4 +114,45 @@ class DropSquareMoveToSideView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DSMTSNode(var i : Int, val state : State = State()) {
+
+        private var next : DSMTSNode? = null
+        private var prev : DSMTSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DSMTSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDSMTSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DSMTSNode {
+            var curr : DSMTSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
