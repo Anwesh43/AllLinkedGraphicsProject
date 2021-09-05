884ccc3aa30db82a204ba3a31320d0ce06b8190f
Anwesh43 <anweshthecool0@gmail.com>
2021-09-06 00:43:38 +0530

    adding TriLineCreateEndView class

--- trilinecreateendview/src/main/java/com/example/trilinecreateendview/TriLineCreateEndView.kt
+++ trilinecreateendview/src/main/java/com/example/trilinecreateendview/TriLineCreateEndView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawTLCENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTriLineCreateEnd(scale, w, h, paint)
 }
+
+class TriLineCreateEndView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
