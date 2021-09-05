bc2911c2f4d397fe265dd35b10ba5987d876bac2
Anwesh43 <anweshthecool0@gmail.com>
2021-09-06 00:54:05 +0530

    adding TLCENode class

--- trilinecreateendview/src/main/java/com/example/trilinecreateendview/TriLineCreateEndView.kt
+++ trilinecreateendview/src/main/java/com/example/trilinecreateendview/TriLineCreateEndView.kt
@@ -120,4 +120,46 @@ class TriLineCreateEndView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TLCENode(var i : Int, val state : State = State()) {
+
+        private var next : TLCENode? = null
+        private var prev : TLCENode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TLCENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTLCENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TLCENode {
+            var curr : TLCENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+    }
 }
