08eaf91aa028ea36dcfaef0f65f8e9ede7162fe6
Anwesh43 <anweshthecool0@gmail.com>
2021-09-06 00:33:59 +0530

    adding Int ad Float extenion methods

--- trilinecreateendview/src/main/java/com/example/trilinecreateendview/TriLineCreateEndView.kt
+++ trilinecreateendview/src/main/java/com/example/trilinecreateendview/TriLineCreateEndView.kt
@@ -22,3 +22,7 @@ val scGap : Float = 0.06f / parts
 val strokeFactor : Float = 90f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.dividScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
