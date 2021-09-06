cf22842456babc947f7941e296c045a54dc7e314
Anwesh43 <anweshthecool0@gmail.com>
2021-09-07 00:59:04 +0530

    adding VLineDropView class

--- vlinedropview/src/main/java/com/example/vlinedropview/VLineDropView.kt
+++ vlinedropview/src/main/java/com/example/vlinedropview/VLineDropView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawVLDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawVLineDrop(scale, w, h, paint)
 }
+
+class VLineDropView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
