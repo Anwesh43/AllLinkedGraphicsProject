468b92b27a76c51cdacb1b3c83346a5fda0aa98f
Anwesh43 <anweshthecool0@gmail.com>
2021-09-08 00:45:56 +0530

    adding Canvas extension methods

--- sidelineupmovedview/src/main/java/com/example/sidelineupmovedview/SideLineUpMovedView.kt
+++ sidelineupmovedview/src/main/java/com/example/sidelineupmovedview/SideLineUpMovedView.kt
@@ -27,3 +27,40 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSideLineUpMove(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        for (k in 0..1) {
+            save()
+            translate(
+                -size / 2 + size * 0.5f * j,
+                -h * 0.5f  * scale.divideScale(2 + (1 - k), parts)
+            )
+            drawLine(
+                0f,
+                0f,
+                0f,
+                (-size / 2 - (size / 2) * j) * scale.divideScale(k, parts),
+                paint
+            )
+            restore()
+        }
+
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSLUMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSideLineUpMove(scale, w, h, paint)
+}
