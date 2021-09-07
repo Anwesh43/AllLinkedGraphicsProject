43761aafe42e80447e0d9aa8cb910fa14f7410bf
Anwesh43 <anweshthecool0@gmail.com>
2021-09-08 00:56:32 +0530

    adding SLUMNode class

--- sidelineupmovedview/src/main/java/com/example/sidelineupmovedview/SideLineUpMovedView.kt
+++ sidelineupmovedview/src/main/java/com/example/sidelineupmovedview/SideLineUpMovedView.kt
@@ -126,5 +126,46 @@ class SideLineUpMovedView(ctx : Context) : View(ctx) {
                 }
             }
         }
+
+        data class SLUMNode(var i : Int, val state : State = State()) {
+
+            private var next : SLUMNode? = null
+            private var prev : SLUMNode? = null
+
+            init {
+                addNeighbor()
+            }
+
+            fun addNeighbor() {
+                if (i < colors.size - 1) {
+                    next = SLUMNode(i + 1)
+                    next?.prev = this
+                }
+            }
+
+            fun draw(canvas : Canvas, paint : Paint) {
+                canvas.drawSLUMNode(i, state.scale, paint)
+            }
+
+            fun update(cb : (Float) -> Unit) {
+                state.update(cb)
+            }
+
+            fun startUpdating(cb : () -> Unit) {
+                state.startUpdating(cb)
+            }
+
+            fun getNext(dir : Int, cb : () -> Unit) : SLUMNode {
+                var curr : SLUMNode? = prev
+                if (dir == 1) {
+                    curr = next
+                }
+                if (curr != null) {
+                    return curr
+                }
+                cb()
+                return this
+            }
+        }
     }
 }
