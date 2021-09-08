1a24b9d6640f71f4953de0065e7879669c5be6c9
Anwesh43 <anweshthecool0@gmail.com>
2021-09-09 01:52:16 +0530

    adding Canvas extension methods

--- linerotatedroprectview/src/main/java/com/example/linerotatedroprectview/LineRotateDropRectView.kt
+++ linerotatedroprectview/src/main/java/com/example/linerotatedroprectview/LineRotateDropRectView.kt
@@ -28,3 +28,35 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineRotateDropRect(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(0f, (h + paint.strokeWidth) * sc4)
+        for (k in 0..1) {
+            save()
+            rotate(deg * (1f - 2 * k) * sc2)
+            drawLine(0f, 0f, 0f, -w * 0.25f * sc1, paint)
+            restore()
+        }
+        restore()
+    }
+    drawRect(RectF(-w / 2, h * 0.5f * sc4, w / 2, h * 0.5f), paint)
+    restore()
+}
+
+fun Canvas.drawLRDRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotateDropRect(scale, w, h, paint)
+}
