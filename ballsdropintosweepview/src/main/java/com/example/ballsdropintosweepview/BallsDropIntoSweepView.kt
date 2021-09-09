1fe493c6b41f20d1ca96e9bf2bc1313d2760ba4b
Anwesh43 <anweshthecool0@gmail.com>
2021-09-10 00:48:09 +0530

    adding Canvas extension methods

--- ballsdropintosweepview/src/main/java/com/example/ballsdropintosweepview/BallsDropIntoSweepView.kt
+++ ballsdropintosweepview/src/main/java/com/example/ballsdropintosweepview/BallsDropIntoSweepView.kt
@@ -28,3 +28,34 @@ val scGap : Float = 0.05f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBallsDropIntoSweep(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..(balls - 1)) {
+        val scj : Float = scale.divideScale(j, parts)
+        save()
+        translate((-w / 4 + (w / 4) * j) * (1 - sc4), (-h / 2 - size / 2) * (1 - scj))
+        drawArc(
+            RectF(
+                -size / 2, -size / 2, size / 2, size / 2
+            ),
+            360f * sc5,
+            360f * (1 - sc5),
+            true,
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBDISNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBallsDropIntoSweep(scale, w, h, paint)
+}
