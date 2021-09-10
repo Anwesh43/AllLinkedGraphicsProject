f6fc01ebaae87fc5b262dfbdb710c307d5d37d79
Anwesh43 <anweshthecool0@gmail.com>
2021-09-11 01:20:04 +0530

    adding constants and libraries

--- circlesdroptosweepview/src/main/java/com/example/circlesdroptosweepview/CirclesDropToSweepView.kt
+++ circlesdroptosweepview/src/main/java/com/example/circlesdroptosweepview/CirclesDropToSweepView.kt
@@ -1,2 +1,28 @@
 package com.example.circlesdroptosweepview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val balls : Int = 3
+val parts : Int = balls + 2
+val scGap : Float = 0.05f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 360f
