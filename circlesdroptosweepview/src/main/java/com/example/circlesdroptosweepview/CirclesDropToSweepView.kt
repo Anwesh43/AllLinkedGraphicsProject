8d7182cca55f8c185b8c45771b62604d70291ef5
Anwesh43 <anweshthecool0@gmail.com>
2021-09-11 01:30:23 +0530

    adding Canvas extension methods

--- circlesdroptosweepview/src/main/java/com/example/circlesdroptosweepview/CirclesDropToSweepView.kt
+++ circlesdroptosweepview/src/main/java/com/example/circlesdroptosweepview/CirclesDropToSweepView.kt
@@ -30,3 +30,29 @@ val deg : Float = 360f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawCirclesDropToSweep(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val x : Float = -w / 2 + size / 2
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..(balls - 1)) {
+        save()
+        translate((x + j * x) * (1 - sc4), (-h / 2 - size / 2) * (1 - scale.divideScale(j, parts)))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), deg * sc5, deg, false, true)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawCDTSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawCirclesDropToSweep(scale, w, h, paint)
+}
