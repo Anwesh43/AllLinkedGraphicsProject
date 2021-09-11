27476c15880ab7f09dfa9a03b8a0dc7c3ae37a9b
Anwesh43 <anweshthecool0@gmail.com>
2021-09-12 02:53:26 +0530

    adding LRSDDNode class

--- linerotsqdowndivideview/src/main/java/com/example/linerotsqdowndivideview/LineRotSqDownDivideView.kt
+++ linerotsqdowndivideview/src/main/java/com/example/linerotsqdowndivideview/LineRotSqDownDivideView.kt
@@ -132,4 +132,45 @@ class LineRotSqDownDivideView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRSDDNode(var i : Int, val state : State = State()) {
+
+        private var next : LRSDDNode? = null
+        private var prev : LRSDDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRSDDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRSDDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRSDDNode {
+            var curr : LRSDDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
