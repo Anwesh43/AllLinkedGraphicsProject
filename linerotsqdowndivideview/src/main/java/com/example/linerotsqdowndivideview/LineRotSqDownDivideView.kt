026e718829c1ab0591a746be532052791826c4bf
Anwesh43 <anweshthecool0@gmail.com>
2021-09-12 02:54:58 +0530

    adding LineRotSqDownDivide class

--- linerotsqdowndivideview/src/main/java/com/example/linerotsqdowndivideview/LineRotSqDownDivideView.kt
+++ linerotsqdowndivideview/src/main/java/com/example/linerotsqdowndivideview/LineRotSqDownDivideView.kt
@@ -173,4 +173,27 @@ class LineRotSqDownDivideView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRotSqDownDivide(var i : Int) {
+
+        private var curr : LRSDDNode = LRSDDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
