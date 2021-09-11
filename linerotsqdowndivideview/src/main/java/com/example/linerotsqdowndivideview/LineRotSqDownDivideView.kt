2af638c37edbb80293f244eac7234434de7bd0be
Anwesh43 <anweshthecool0@gmail.com>
2021-09-12 02:45:31 +0530

    adding Canvas extension methods

--- linerotsqdowndivideview/src/main/java/com/example/linerotsqdowndivideview/LineRotSqDownDivideView.kt
+++ linerotsqdowndivideview/src/main/java/com/example/linerotsqdowndivideview/LineRotSqDownDivideView.kt
@@ -29,3 +29,42 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineRotSqDownDivide(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    save()
+    rotate(90f * sc2)
+    drawLine(0f, 0f, size * (sc1 - sc3), 0f, paint)
+    restore()
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        translate(0f, (h / 2 + size) * sc4)
+        drawRect(
+            RectF(
+                -size * 0.5f * Math.floor(sc1.toDouble()).toFloat(),
+                size - size * sc3,
+                size * 0.5f * Math.floor(sc1.toDouble()).toFloat(),
+                size
+            ),
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLRSDDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotSqDownDivide(scale, w, h, paint)
+}
