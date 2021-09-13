d8cd550608ccb73d1ce7dfc1df55d0e640d224b2
Anwesh43 <anweshthecool0@gmail.com>
2021-09-14 00:46:28 +0530

    adding Canvas extension methods

--- rightanglesquaredivideview/src/main/java/com/example/rightanglesquaredivideview/RightAngleSquareDivideView.kt
+++ rightanglesquaredivideview/src/main/java/com/example/rightanglesquaredivideview/RightAngleSquareDivideView.kt
@@ -19,7 +19,7 @@ val colors : Array<Int> = arrayOf(
     Color.parseColor(it)
 }.toTypedArray()
 val parts : Int = 4
-val scGap : Float = 0.04f / parts
+val scGap : Float = 0.05f / parts
 val strokeFactor : Float = 90f
 val deg : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
@@ -29,3 +29,38 @@ val sizeFactor : Float = 4.5f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawRightAngleSquareDivide(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(w * 0.5f * sc5, 0f)
+        for (k in 0..1) {
+            save()
+            rotate(deg * sc2 * k)
+            drawLine(0f, 0f, 0f, -size * sc1, paint)
+            restore()
+        }
+        drawLine(size, 0f, size, -size * sc3, paint)
+        drawRect(RectF(0f, -size * Math.floor(sc1.toDouble()).toFloat(), size * sc4, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawRASDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightAngleSquareDivide(scale, w, h, paint)
+}
