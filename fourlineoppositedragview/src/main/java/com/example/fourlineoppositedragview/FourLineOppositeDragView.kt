6c7fe6ed4c7cc391d2260d3b932cb5a8760dccb6
Anwesh43 <anweshthecool0@gmail.com>
2021-09-16 03:35:08 +0530

    adding Renderer class

--- fourlineoppositedragview/src/main/java/com/example/fourlineoppositedragview/FourLineOppositeDragView.kt
+++ fourlineoppositedragview/src/main/java/com/example/fourlineoppositedragview/FourLineOppositeDragView.kt
@@ -188,4 +188,27 @@ class FourLineOppositeDragView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : FourLineOppositeDragView) {
+
+        private val animator : Animator = Animator(view)
+        private val flod : FourLineOppositeDrag = FourLineOppositeDrag(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            flod.draw(canvas, paint)
+            animator.animate {
+                flod.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            flod.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
