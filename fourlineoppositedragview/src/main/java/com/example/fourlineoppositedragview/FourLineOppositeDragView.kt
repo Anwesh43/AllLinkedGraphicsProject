7e43fbef8c801c8c697e20a5842eb0a611762d19
Anwesh43 <anweshthecool0@gmail.com>
2021-09-16 03:02:57 +0530

    adding FourLineOppositeDragView class

--- fourlineoppositedragview/src/main/java/com/example/fourlineoppositedragview/FourLineOppositeDragView.kt
+++ fourlineoppositedragview/src/main/java/com/example/fourlineoppositedragview/FourLineOppositeDragView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawFLODNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawFourLineOppositeDrag(scale, w, h, paint)
 }
+
+class FourLineOppositeDragView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
