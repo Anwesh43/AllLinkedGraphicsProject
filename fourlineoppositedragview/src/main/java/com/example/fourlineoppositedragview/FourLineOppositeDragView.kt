1ce3ffcf6fa1895bfee6acdd834bebe2e4921610
Anwesh43 <anweshthecool0@gmail.com>
2021-09-16 03:08:32 +0530

    adding FLODNode class

--- fourlineoppositedragview/src/main/java/com/example/fourlineoppositedragview/FourLineOppositeDragView.kt
+++ fourlineoppositedragview/src/main/java/com/example/fourlineoppositedragview/FourLineOppositeDragView.kt
@@ -124,4 +124,45 @@ class FourLineOppositeDragView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class FLODNode(var i : Int, val state : State = State()) {
+
+        private var next : FLODNode? = null
+        private var prev : FLODNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = FLODNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFLODNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FLODNode {
+            var curr : FLODNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
