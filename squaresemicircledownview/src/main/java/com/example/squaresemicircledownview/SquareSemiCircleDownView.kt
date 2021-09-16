6b75e690c603a64015fc0c0ec1847ee71a8c8671
Anwesh43 <anweshthecool0@gmail.com>
2021-09-17 01:30:45 +0530

    adding Canvas extension methods

--- squaresemicircledownview/src/main/java/com/example/squaresemicircledownview/SquareSemiCircleDownView.kt
+++ squaresemicircledownview/src/main/java/com/example/squaresemicircledownview/SquareSemiCircleDownView.kt
@@ -29,3 +29,34 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSquareSemiCircleDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    save()
+    translate(0f, (h / 2) * (1 - sc1))
+    drawRect(RectF(-size / 2, 0f, size / 2, size), paint)
+    restore()
+    drawArc(
+        RectF(-size / 2, -size / 2, size / 2, size / 2),
+        deg * 2,
+        deg * 2 * sc2,
+        true,
+        paint
+    )
+    restore()
+}
+
+fun Canvas.drawSSCDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSquareSemiCircleDown(scale, w, h, paint)
+}
