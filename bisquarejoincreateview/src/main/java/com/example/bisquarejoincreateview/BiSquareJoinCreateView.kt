2197d1bf6e3d96aa8f98771f77c85183a295f879
Anwesh43 <anweshthecool0@gmail.com>
2021-09-19 01:53:47 +0530

    adding BiSquareJoinCreateView class

--- bisquarejoincreateview/src/main/java/com/example/bisquarejoincreateview/BiSquareJoinCreateView.kt
+++ bisquarejoincreateview/src/main/java/com/example/bisquarejoincreateview/BiSquareJoinCreateView.kt
@@ -55,3 +55,18 @@ fun Canvas.drawBSJCNode(i : Int, scale : Float, paint : Paint) {
     drawBiSquareJoinCreate(scale, w, h, paint)
 }
 
+class BiSquareJoinCreateView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
