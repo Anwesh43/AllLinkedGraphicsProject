363b168d9d74e47603208cbc943b9cff74045882
Anwesh43 <anweshthecool0@gmail.com>
2021-09-22 10:48:38 +0530

    adding BiDivideSquareDivideView in activity

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.bidividesquaredropview.BiDivideSquareDropView
+
 //import com.example.bisquarejoincreateview.BiSquareJoinCreateView
 
 //import com.example.squaresemicircledownview.SquareSemiCircleDownView
@@ -160,6 +162,7 @@ class MainActivity : AppCompatActivity() {
         //FourLineOppositeDragView.create(this)
         //SquareSemiCircleDownView.create(this)
         //BiSquareJoinCreateView.create(this)
+        BiDivideSquareDropView.create(this)
         fullScreen()
     }
 }
