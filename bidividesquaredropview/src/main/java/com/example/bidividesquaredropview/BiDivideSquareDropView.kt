a00f3752ef758bf35b13e87453e9acf1b7b8c138
Anwesh43 <anweshthecool0@gmail.com>
2021-09-22 10:35:57 +0530

    adding Renderer class

--- bidividesquaredropview/src/main/java/com/example/bidividesquaredropview/BiDivideSquareDropView.kt
+++ bidividesquaredropview/src/main/java/com/example/bidividesquaredropview/BiDivideSquareDropView.kt
@@ -183,4 +183,27 @@ class BiDivideSquareDropView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiDivideSquareDropView) {
+
+        private val animator : Animator = Animator(view)
+        private val bdsd : BiDivideSquareDrop = BiDivideSquareDrop(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bdsd.draw(canvas, paint)
+            animator.animate {
+                bdsd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bdsd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
