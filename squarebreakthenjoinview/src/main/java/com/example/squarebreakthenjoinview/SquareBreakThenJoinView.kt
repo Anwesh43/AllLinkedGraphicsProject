d4adbc5e0e3c924b210351467d1ee6406020c298
Anwesh43 <anweshthecool0@gmail.com>
2021-09-25 11:26:13 +0530

    adding Canvas extension methods

--- squarebreakthenjoinview/src/main/java/com/example/squarebreakthenjoinview/SquareBreakThenJoinView.kt
+++ squarebreakthenjoinview/src/main/java/com/example/squarebreakthenjoinview/SquareBreakThenJoinView.kt
@@ -28,3 +28,37 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSquareBreakThenJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size / 2) * sc5)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        translate((size / 2) * (sc2 - sc4), 0f)
+        drawRect(
+            RectF(
+                -size / 2,
+                -size / 2,
+                size / 2,
+                -size / 2 + size * 0.5f * sc1),
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSBTJNode(i : Int, scale : Float, paint : Paint) {
+    paint.color = colors[i]
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    drawSquareBreakThenJoin(scale, w, h, paint)
+}
