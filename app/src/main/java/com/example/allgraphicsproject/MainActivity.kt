ea497e3699c59577118170b9e4c0010c6bdecf40
Anwesh43 <anweshthecool0@gmail.com>
2021-09-25 13:30:05 +0530

    adding SquareBreakThenJoinView in activity

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.squarebreakthenjoinview.SquareBreakThenJoinView
+
 //import com.example.bidividesquaredropview.BiDivideSquareDropView
 
 //import com.example.bisquarejoincreateview.BiSquareJoinCreateView
@@ -163,6 +165,7 @@ class MainActivity : AppCompatActivity() {
         //SquareSemiCircleDownView.create(this)
         //BiSquareJoinCreateView.create(this)
         //BiDivideSquareDropView.create(this)
+        SquareBreakThenJoinView.create(this)
         fullScreen()
     }
 }
