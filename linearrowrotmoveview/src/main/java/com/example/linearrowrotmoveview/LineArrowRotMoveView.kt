e61f5f2b20866434d4136d02659abc7eeef4fa7f
Anwesh43 <anweshthecool0@gmail.com>
2021-09-26 00:51:20 +0530

    adding Canvas extension methods

--- linearrowrotmoveview/src/main/java/com/example/linearrowrotmoveview/LineArrowRotMoveView.kt
+++ linearrowrotmoveview/src/main/java/com/example/linearrowrotmoveview/LineArrowRotMoveView.kt
@@ -8,6 +8,7 @@ import android.graphics.Canvas
 import android.graphics.RectF
 import android.graphics.Color
 import android.content.Context
+import android.graphics.Path
 
 val colors : Array<Int> = arrayOf(
     "#304FFE",
@@ -29,3 +30,34 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawArrow(size : Float, paint : Paint) {
+    val path : Path = Path()
+    path.moveTo(-size / 2, size)
+    path.lineTo(size / 2, size)
+    path.lineTo(0f, 0f)
+    path.lineTo(-size / 2, size)
+}
+
+fun Canvas.drawLineArrowRotMove(w : Float, h : Float, scale : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    drawLine(-size * 0.5f * sc1, 0f, size * 0.5f * sc1, 0f, paint)
+    save()
+    translate(0f, h / 2 - h * 0.5f * sc2)
+    drawArrow(size, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawLARMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawLineArrowRotMove(w, h, scale, paint)
+}
