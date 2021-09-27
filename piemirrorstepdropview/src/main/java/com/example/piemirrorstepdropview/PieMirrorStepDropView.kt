429a82a58dce233d423f2739bbbe9b33cb1a1a8c
Anwesh43 <anweshthecool0@gmail.com>
2021-09-28 02:36:33 +0530

    adding PieMirrorStepDownView class

--- piemirrorstepdropview/src/main/java/com/example/piemirrorstepdropview/PieMirrorStepDropView.kt
+++ piemirrorstepdropview/src/main/java/com/example/piemirrorstepdropview/PieMirrorStepDropView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawPMSDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawPieMirrorStepDown(scale, w, h, paint)
 }
+
+class PieMirrorStepDropView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
