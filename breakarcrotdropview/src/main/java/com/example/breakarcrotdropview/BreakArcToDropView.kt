418e983000dc81c4e2c19762fc0e77d2b8086df5
Anwesh43 <anweshthecool0@gmail.com>
2021-09-29 10:19:52 +0530

    adding BreakArcToDropView class

--- breakarcrotdropview/src/main/java/com/example/breakarcrotdropview/BreakArcToDropView.kt
+++ breakarcrotdropview/src/main/java/com/example/breakarcrotdropview/BreakArcToDropView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawBATDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBreakArcToDrop(scale, w, h, paint)
 }
+
+class BreakArcToDropView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
