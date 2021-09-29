d3b999672cbe24b8e062205c706574a74c566e5d
Anwesh43 <anweshthecool0@gmail.com>
2021-09-30 02:35:47 +0530

    adding BTSDNode class

--- breaktopsquaredropview/src/main/java/com/example/breaktopsquaredropview/BreakTopSquareDropView.kt
+++ breaktopsquaredropview/src/main/java/com/example/breaktopsquaredropview/BreakTopSquareDropView.kt
@@ -113,4 +113,45 @@ class BreakTopSquareDropView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BTSDNode(var i : Int, val state : State = State()) {
+
+        private var next : BTSDNode? = null
+        private var prev : BTSDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BTSDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBTSDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BTSDNode {
+            var curr : BTSDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
