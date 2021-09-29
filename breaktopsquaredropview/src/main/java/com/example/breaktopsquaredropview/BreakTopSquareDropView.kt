0bd8b644c33cf31403eb503012161dbf1689ad15
Anwesh43 <anweshthecool0@gmail.com>
2021-09-30 01:19:04 +0530

    adding Canvas extension methods

--- breaktopsquaredropview/src/main/java/com/example/breaktopsquaredropview/BreakTopSquareDropView.kt
+++ breaktopsquaredropview/src/main/java/com/example/breaktopsquaredropview/BreakTopSquareDropView.kt
@@ -27,3 +27,28 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBreakTopSquareDrop(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size / 2) * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        translate((w / 2) * (1 - sc1) - size * 0.5f * sc3, size * 0.25f * sc2)
+        drawRect(RectF(0f, -size / 4, size, size / 4), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBTSDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBreakTopSquareDrop(scale, w, h, paint)
+}
