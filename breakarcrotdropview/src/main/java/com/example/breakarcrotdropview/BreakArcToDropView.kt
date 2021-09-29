37f2cfce2fd948f6c92f11d36ce8d56596601797
Anwesh43 <anweshthecool0@gmail.com>
2021-09-29 10:14:35 +0530

    adding Canvas extension methods

--- breakarcrotdropview/src/main/java/com/example/breakarcrotdropview/BreakArcToDropView.kt
+++ breakarcrotdropview/src/main/java/com/example/breakarcrotdropview/BreakArcToDropView.kt
@@ -29,3 +29,34 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBreakArcToDrop(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val sc6 : Float = scale.divideScale(5, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size / 2) * sc6)
+    rotate(rot * sc4)
+    for (j in 0..1) {
+        save()
+        translate(-size * 0.5f * (1 - 2 * j) * (sc3 - sc5), 0f)
+        drawArc(
+            RectF(-size / 2, -size / 2, size / 2, size / 2),
+            deg * j,
+            deg * scale.divideScale(j, parts),
+            true,
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBATDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBreakArcToDrop(scale, w, h, paint)
+}
