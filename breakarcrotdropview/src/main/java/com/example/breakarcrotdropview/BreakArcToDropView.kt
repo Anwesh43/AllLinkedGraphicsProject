7a692033f340bf72b1ec2e4b3ce2cb792847d221
Anwesh43 <anweshthecool0@gmail.com>
2021-09-29 10:07:45 +0530

    adding constants and libraries

--- breakarcrotdropview/src/main/java/com/example/breakarcrotdropview/BreakArcToDropView.kt
+++ breakarcrotdropview/src/main/java/com/example/breakarcrotdropview/BreakArcToDropView.kt
@@ -1,2 +1,27 @@
 package com.example.breakarcrotdropview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 6
+val scGap : Float = 0.06f / parts
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
+val rot : Float = 90f
