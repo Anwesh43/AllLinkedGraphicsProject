d0e03ac9193de2bada8eec647977ec1530d68bcb
Anwesh43 <anweshthecool0@gmail.com>
2021-10-02 05:57:27 +0530

    adding WASDNode class

--- wavesweeparcdropview/src/main/java/com/example/wavesweeparcdropview/WaveArcSweepDropView.kt
+++ wavesweeparcdropview/src/main/java/com/example/wavesweeparcdropview/WaveArcSweepDropView.kt
@@ -122,4 +122,45 @@ class WaveArcSweepDropView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class WASDNode(var i : Int, val state : State = State()) {
+
+        private var next : WASDNode? = null
+        private var prev : WASDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size  - 1) {
+                next = WASDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawWASDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : WASDNode {
+            var curr : WASDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
