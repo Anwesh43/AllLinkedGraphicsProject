c2ab3e590a7be1ed5b921bf4a9e00cb49cd42538
Anwesh43 <anweshthecool0@gmail.com>
2021-10-02 05:48:46 +0530

    adding Canvas extension methods

--- wavesweeparcdropview/src/main/java/com/example/wavesweeparcdropview/WaveArcSweepDropView.kt
+++ wavesweeparcdropview/src/main/java/com/example/wavesweeparcdropview/WaveArcSweepDropView.kt
@@ -23,7 +23,39 @@ val scGap : Float = 0.04f / parts
 val deg : Float = 180f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val sizeFactor : Float = 4.5f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawWaveArcSweep(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size / 2) * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(0f, size / 2 * (1f - 2 * j) * (1f - sc3))
+        drawArc(
+            RectF(
+                -size / 2, -size / 2, size / 2, size / 2
+            ),
+            (deg / 2) * (1f - 2 * j),
+            deg * scale.divideScale(j, parts),
+            true,
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawWASDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawWaveArcSweep(scale, w, h, paint)
+}
