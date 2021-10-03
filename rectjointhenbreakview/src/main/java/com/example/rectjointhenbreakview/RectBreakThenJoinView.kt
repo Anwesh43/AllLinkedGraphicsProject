f6894f6d09d080d01b0906055c414a74d895495e
Anwesh43 <anweshthecool0@gmail.com>
2021-10-04 00:42:01 +0530

    adding RBTJNode class

--- rectjointhenbreakview/src/main/java/com/example/rectjointhenbreakview/RectBreakThenJoinView.kt
+++ rectjointhenbreakview/src/main/java/com/example/rectjointhenbreakview/RectBreakThenJoinView.kt
@@ -112,4 +112,45 @@ class RectBreakThenJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RBTJNode(var i : Int, val state : State = State()) {
+
+        private var next : RBTJNode? = null
+        private var prev : RBTJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RBTJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRJTBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RBTJNode {
+            var curr : RBTJNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
