7831b1b98f7d28f26942628a0d835f77174f0537
Anwesh43 <anweshthecool0@gmail.com>
2021-10-04 00:30:28 +0530

    adding constants and libraries

---
+++ rectjointhenbreakview/src/main/java/com/example/rectjointhenbreakview/RectBreakThenJoinView.kt
@@ -0,0 +1,25 @@
+package com.example.rectjointhenbreakview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor("#BDBDBD")
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 5.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
