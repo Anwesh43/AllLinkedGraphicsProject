ca54b533a95dc8a3657f9909b9ef6fce103a4d71
Anwesh43 <anweshthecool0@gmail.com>
2021-10-04 00:34:30 +0530

    ading Canvas extension methdos

--- rectjointhenbreakview/src/main/java/com/example/rectjointhenbreakview/RectBreakThenJoinView.kt
+++ rectjointhenbreakview/src/main/java/com/example/rectjointhenbreakview/RectBreakThenJoinView.kt
@@ -27,3 +27,25 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawRectJoinBreak(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        val scj1 : Float = scale.divideScale(j, parts)
+        val scj2 : Float = scale.divideScale(j + 2, parts)
+        save()
+        translate(w * 0.5f * (1 - scj1), (h * 0.5f + size / 2) * scj2)
+        drawRect(RectF(0f, -size / 4, -size / 4, size / 4), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawRJTBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawRectJoinBreak(scale, w, h, paint)
+}
