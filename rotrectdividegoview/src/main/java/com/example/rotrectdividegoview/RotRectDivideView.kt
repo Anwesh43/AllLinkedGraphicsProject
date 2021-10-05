2f320b2c3ff06bd5e9f4b17f10eac44408f67b81
Anwesh43 <anweshthecool0@gmail.com>
2021-10-06 01:27:17 +0530

    adding RotRectDivide class

--- rotrectdividegoview/src/main/java/com/example/rotrectdividegoview/RotRectDivideView.kt
+++ rotrectdividegoview/src/main/java/com/example/rotrectdividegoview/RotRectDivideView.kt
@@ -157,4 +157,27 @@ class RotRectDivideView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RotRectDivide(var i : Int) {
+
+        private var curr : RRDNode = RRDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
