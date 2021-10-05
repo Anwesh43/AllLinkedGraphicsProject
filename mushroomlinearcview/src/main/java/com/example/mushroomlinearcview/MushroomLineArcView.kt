b5ec66319e87146a744e972f031223c399b7874f
Anwesh43 <anweshthecool0@gmail.com>
2021-10-05 13:26:27 +0530

    adding MLANode class

--- mushroomlinearcview/src/main/java/com/example/mushroomlinearcview/MushroomLineArcView.kt
+++ mushroomlinearcview/src/main/java/com/example/mushroomlinearcview/MushroomLineArcView.kt
@@ -121,4 +121,45 @@ class MushroomLineArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MLANode(var i : Int, val state : State = State()) {
+
+        private var next : MLANode? = null
+        private var prev : MLANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MLANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMLANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb :() -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MLANode {
+            var curr : MLANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
