35afea9bdaf3281ce25a98ef086e3c3ca696e2e8
Anwesh43 <anweshthecool0@gmail.com>
2021-10-06 01:19:27 +0530

    adding Canvas extension methods

--- rotrectdividegoview/src/main/java/com/example/rotrectdividegoview/RotRectDivideView.kt
+++ rotrectdividegoview/src/main/java/com/example/rotrectdividegoview/RotRectDivideView.kt
@@ -28,3 +28,28 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawRotRectDivide(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate((w / 2) * sc3, h * 0.5f * (1 - sc1))
+        rotate(deg * sc2)
+        drawRect(RectF(0f, -size, size / 2, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawRRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawRotRectDivide(scale, w, h, paint)
+}
