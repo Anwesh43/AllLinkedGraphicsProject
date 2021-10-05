0299503311bde32b81d3670050580cf2fef428d7
Anwesh43 <anweshthecool0@gmail.com>
2021-10-06 01:25:55 +0530

    adding RRDNode class

--- rotrectdividegoview/src/main/java/com/example/rotrectdividegoview/RotRectDivideView.kt
+++ rotrectdividegoview/src/main/java/com/example/rotrectdividegoview/RotRectDivideView.kt
@@ -116,4 +116,45 @@ class RotRectDivideView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RRDNode(var i : Int, val state : State = State()) {
+
+        private var next : RRDNode? = null
+        private var prev : RRDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RRDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRRDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RRDNode {
+            var curr : RRDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
