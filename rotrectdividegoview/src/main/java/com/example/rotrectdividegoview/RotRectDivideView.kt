68efdca24419e23125060e9156aa309ec86f5d3d
Anwesh43 <anweshthecool0@gmail.com>
2021-10-06 01:20:33 +0530

    adding RotRectDivideView class

--- rotrectdividegoview/src/main/java/com/example/rotrectdividegoview/RotRectDivideView.kt
+++ rotrectdividegoview/src/main/java/com/example/rotrectdividegoview/RotRectDivideView.kt
@@ -53,3 +53,19 @@ fun Canvas.drawRRDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawRotRectDivide(scale, w, h, paint)
 }
+
+class RotRectDivideView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
