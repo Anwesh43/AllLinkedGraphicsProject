bf8cf55a2e6b0720d26f8145228a21ce54ffcc69
Anwesh43 <anweshthecool0@gmail.com>
2021-10-07 01:18:15 +0530

    adding BCGUNode class

--- barcompletegoupview/src/main/java/com/example/barcompletegoupview/BarCompleteGoUpView.kt
+++ barcompletegoupview/src/main/java/com/example/barcompletegoupview/BarCompleteGoUpView.kt
@@ -113,4 +113,45 @@ class BarCompleteGoUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BCGUNode(var i : Int, val state : State = State()) {
+
+        private var next : BCGUNode? = null
+        private var prev : BCGUNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BCGUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBCGUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BCGUNode {
+            var curr : BCGUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
