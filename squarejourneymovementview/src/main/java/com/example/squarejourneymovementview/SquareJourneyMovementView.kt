9a14aaaa7c9523a3593cc43470ed15fbc42becdd
Anwesh43 <anweshthecool0@gmail.com>
2021-10-08 10:59:39 +0530

    addign SquareJourneyMovementView class

--- squarejourneymovementview/src/main/java/com/example/squarejourneymovementview/SquareJourneyMovementView.kt
+++ squarejourneymovementview/src/main/java/com/example/squarejourneymovementview/SquareJourneyMovementView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawSJMNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawSquareJourneyMovement(scale, w, h, paint)
 }
+
+class SquareJourneyMovementView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action){
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
