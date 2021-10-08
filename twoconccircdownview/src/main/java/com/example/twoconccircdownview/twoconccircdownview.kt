f02787c17b55e66249b3f238bc6d2199cd4b6669
Anwesh43 <anweshthecool0@gmail.com>
2021-10-09 01:24:32 +0530

    adding Canvas extension methods

--- twoconccircdownview/src/main/java/com/example/twoconccircdownview/twoconccircdownview.kt
+++ twoconccircdownview/src/main/java/com/example/twoconccircdownview/twoconccircdownview.kt
@@ -30,3 +30,43 @@ val deg : Float = 360f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawTwoConcCircDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+    rotate(rot * sc3)
+    for(j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        for (k in 0..1) {
+            val sck : Float = scale.divideScale(k, parts)
+            save()
+            translate(k * size * 0.5f, 0f)
+            drawArc(
+                RectF(
+                    -size / 2, -size, size / 2, 0f
+                ),
+                0f,
+                deg * k,
+                false,
+                paint
+            )
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawTCCDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawTwoConcCircDown(scale, w, h, paint)
+}
