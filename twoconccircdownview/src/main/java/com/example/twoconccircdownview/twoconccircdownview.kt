60fb59d7cf6a7c93ff09334ff9a4c7f6e4d57607
Anwesh43 <anweshthecool0@gmail.com>
2021-10-09 01:29:00 +0530

    adding TwoConcCircleDownViw class

--- twoconccircdownview/src/main/java/com/example/twoconccircdownview/twoconccircdownview.kt
+++ twoconccircdownview/src/main/java/com/example/twoconccircdownview/twoconccircdownview.kt
@@ -70,3 +70,19 @@ fun Canvas.drawTCCDNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawTwoConcCircDown(scale, w, h, paint)
 }
+
+class TwoConcCircleDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
