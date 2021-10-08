3e5a6cc432dfb88c27adb7c5a7284ed10b974ca2
Anwesh43 <anweshthecool0@gmail.com>
2021-10-09 02:23:53 +0530

    adding TCCDNode class

--- twoconccircdownview/src/main/java/com/example/twoconccircdownview/twoconccircdownview.kt
+++ twoconccircdownview/src/main/java/com/example/twoconccircdownview/twoconccircdownview.kt
@@ -133,4 +133,45 @@ class TwoConcCircleDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TCCDNode(var i : Int, val state : State = State()) {
+
+        private var next : TCCDNode? = null
+        private var prev : TCCDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TCCDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTCCDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TCCDNode {
+            var curr : TCCDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
