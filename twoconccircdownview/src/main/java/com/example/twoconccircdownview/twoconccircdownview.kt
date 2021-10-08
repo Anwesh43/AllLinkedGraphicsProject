6a814a79d25e89d713a3f249ce90ddf161145412
Anwesh43 <anweshthecool0@gmail.com>
2021-10-09 01:16:59 +0530

    twoconccircdownview:adding constants and libraries

---
+++ twoconccircdownview/src/main/java/com/example/twoconccircdownview/twoconccircdownview.kt
@@ -0,0 +1,28 @@
+package com.example.twoconccircdownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#D32F2F",
+    "#006064",
+    "#01579B",
+    "#BF360C",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
+val deg : Float = 360f
