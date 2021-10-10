247e18f811d49a53804959c8be06391845d5626b
Anwesh43 <anweshthecool0@gmail.com>
2021-10-10 16:13:01 +0530

    adding Renderer class

--- powerbuttonshooterview/src/main/java/com/example/powerbuttonshooterview/PowerButtonShooterView.kt
+++ powerbuttonshooterview/src/main/java/com/example/powerbuttonshooterview/PowerButtonShooterView.kt
@@ -185,4 +185,27 @@ class PowerButtonShooterView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : PowerButtonShooterView) {
+
+        private val animator : Animator = Animator(view)
+        private val pbs : PowerButtonShooter = PowerButtonShooter(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            pbs.draw(canvas, paint)
+            animator.animate {
+                pbs.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            pbs.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
