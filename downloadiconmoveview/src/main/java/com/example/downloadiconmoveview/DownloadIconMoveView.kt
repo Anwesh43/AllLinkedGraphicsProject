f93888677c8d837534f31e22c42d387c96465d0e
Anwesh43 <anweshthecool0@gmail.com>
2021-10-11 01:37:59 +0530

    adding DIMNode class

--- downloadiconmoveview/src/main/java/com/example/downloadiconmoveview/DownloadIconMoveView.kt
+++ downloadiconmoveview/src/main/java/com/example/downloadiconmoveview/DownloadIconMoveView.kt
@@ -139,4 +139,45 @@ class DownloadIconMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DIMNode(var i : Int, val state : State = State()) {
+
+        private var next : DIMNode? = null
+        private var prev : DIMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DIMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDIMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DIMNode {
+            var curr : DIMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
