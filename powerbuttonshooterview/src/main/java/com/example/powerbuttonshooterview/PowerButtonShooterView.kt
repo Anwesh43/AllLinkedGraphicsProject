f7dc6860a746844c2ad3e60387eaddc66d0bf1d9
Anwesh43 <anweshthecool0@gmail.com>
2021-10-10 15:59:14 +0530

    adding PowerButtonShooterView class

--- powerbuttonshooterview/src/main/java/com/example/powerbuttonshooterview/PowerButtonShooterView.kt
+++ powerbuttonshooterview/src/main/java/com/example/powerbuttonshooterview/PowerButtonShooterView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawPBSNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawPowerButtonShooter(scale, w, h, paint)
 }
+
+class PowerButtonShooterView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
