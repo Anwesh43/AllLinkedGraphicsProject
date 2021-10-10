a98aedba6c192ba029fa86fa6aa466b5ca83a89b
Anwesh43 <anweshthecool0@gmail.com>
2021-10-11 01:40:50 +0530

    adding DownloadIconMove clas

--- downloadiconmoveview/src/main/java/com/example/downloadiconmoveview/DownloadIconMoveView.kt
+++ downloadiconmoveview/src/main/java/com/example/downloadiconmoveview/DownloadIconMoveView.kt
@@ -180,4 +180,27 @@ class DownloadIconMoveView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class DownloadIconMove(var i : Int, val state : State = State()) {
+
+        private var curr : DIMNode = DIMNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
