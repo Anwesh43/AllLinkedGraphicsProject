4a2e3045bc6c070cc30cfa8ef90764390cca8a39
Anwesh43 <anweshthecool0@gmail.com>
2021-10-10 15:58:03 +0530

    adding Canvas extension methods

--- powerbuttonshooterview/src/main/java/com/example/powerbuttonshooterview/PowerButtonShooterView.kt
+++ powerbuttonshooterview/src/main/java/com/example/powerbuttonshooterview/PowerButtonShooterView.kt
@@ -30,3 +30,31 @@ val deg : Float = 300f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawPowerButtonShooter(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2)
+    drawArc(
+        RectF(-size / 2, size / 2, size / 2, size / 2),
+        start + deg * sc5,
+        deg * (sc1 - sc5),
+        false,
+        paint
+    )
+    restore()
+}
+fun Canvas.drawPBSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawPowerButtonShooter(scale, w, h, paint)
+}
