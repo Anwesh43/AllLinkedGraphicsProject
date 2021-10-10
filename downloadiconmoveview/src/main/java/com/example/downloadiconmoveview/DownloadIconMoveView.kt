569ec1a5bff264c6b06e96e17b20ea14b5a0b019
Anwesh43 <anweshthecool0@gmail.com>
2021-10-11 01:09:22 +0530

    adding DownloadIconMoveView class

--- downloadiconmoveview/src/main/java/com/example/downloadiconmoveview/DownloadIconMoveView.kt
+++ downloadiconmoveview/src/main/java/com/example/downloadiconmoveview/DownloadIconMoveView.kt
@@ -76,3 +76,19 @@ fun Canvas.drawDIMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawDownloadIconMove(scale, w, h, paint)
 }
+
+class DownloadIconMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
