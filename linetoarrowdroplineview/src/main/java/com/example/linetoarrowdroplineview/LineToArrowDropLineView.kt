97cc43edb1b6b00745480179e62a82fac504f26b
Anwesh43 <anweshthecool0@gmail.com>
2021-10-14 01:25:22 +0530

    adding constants and libraries: LineToArrowDropLineView

---
+++ linetoarrowdroplineview/src/main/java/com/example/linetoarrowdroplineview/LineToArrowDropLineView.kt
@@ -0,0 +1,26 @@
+package com.example.linetoarrowdroplineview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#D32F2F",
+    "#006064",
+    "#01579B",
+    "#BF360C",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.03f / parts
+val delay : Long = 20
+val sizeFactor : Float = 4.9f
+val strokeFactor : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 45f
