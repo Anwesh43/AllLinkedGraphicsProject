c8b295096d688274a5cb70a48ef60c49365ff78c
Anwesh43 <anweshthecool0@gmail.com>
2021-10-14 01:52:28 +0530

    adding LDATLNode class

--- linetoarrowdroplineview/src/main/java/com/example/linetoarrowdroplineview/LineToArrowDropLineView.kt
+++ linetoarrowdroplineview/src/main/java/com/example/linetoarrowdroplineview/LineToArrowDropLineView.kt
@@ -121,4 +121,45 @@ class LineToArrowDropLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTADLNode(var i : Int, val state : State = State()) {
+
+        private var prev : LTADLNode? = null
+        private var next : LTADLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTADLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTADLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTADLNode {
+            var curr : LTADLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
