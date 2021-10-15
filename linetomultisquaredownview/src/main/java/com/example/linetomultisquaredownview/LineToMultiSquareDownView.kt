89f4a2aeb6ec6505e57c1e6330edad333ee4c473
Anwesh43 <anweshthecool0@gmail.com>
2021-10-15 14:51:01 +0530

    adding Canvas extension methods

--- linetomultisquaredownview/src/main/java/com/example/linetomultisquaredownview/LineToMultiSquareDownView.kt
+++ linetomultisquaredownview/src/main/java/com/example/linetomultisquaredownview/LineToMultiSquareDownView.kt
@@ -29,3 +29,35 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineToMultiSquareDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(0f, h * sc3)
+    for (j in 0..(lines - 1)) {
+        val sc1j : Float = sc1.divideScale(j, lines)
+        val sc2j : Float = sc2.divideScale(j, lines)
+        val upSize : Float = size * 0.5f * sc2j
+        save()
+        translate(0f, size * j + size / 2)
+        drawLine(w * 0.5f * sc2j, 0f, w * 0.5f * sc1j, 0f, paint)
+        save()
+        translate(w / 2, 0f)
+        drawRect(RectF(-upSize, -upSize, upSize, upSize), paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLMSDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawLineToMultiSquareDown(scale, w, h, paint)
+}
