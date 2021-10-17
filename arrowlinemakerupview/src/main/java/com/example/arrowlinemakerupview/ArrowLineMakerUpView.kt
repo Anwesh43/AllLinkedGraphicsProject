1248c5f982b9219cf7bb4cac9088168405dd1b42
Anwesh43 <anweshthecool0@gmail.com>
2021-10-17 12:01:33 +0530

    adding constants and libraries

---
+++ arrowlinemakerupview/src/main/java/com/example/arrowlinemakerupview/ArrowLineMakerUpView.kt
@@ -0,0 +1,27 @@
+package com.example.arrowlinemakerupview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#E53935",
+    "#004D40",
+    "#6200EA",
+    "#FFD600",
+    "#AA00FF"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.03f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+
+
