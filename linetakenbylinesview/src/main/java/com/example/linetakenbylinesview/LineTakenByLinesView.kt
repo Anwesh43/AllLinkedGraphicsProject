857bf6cb8acb57649c0df53adc72863271f316c1
Anwesh43 <anweshthecool0@gmail.com>
2021-10-18 02:01:34 +0530

    adding Canvas extension methods

--- linetakenbylinesview/src/main/java/com/example/linetakenbylinesview/LineTakenByLinesView.kt
+++ linetakenbylinesview/src/main/java/com/example/linetakenbylinesview/LineTakenByLinesView.kt
@@ -28,3 +28,32 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineTakenByLines(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2  + w * 0.5f * sc4, h / 2)
+    rotate(deg * sc3)
+    drawLine(-size * 0.5f * sc1, 0f, size * 0.5f * sc1, 0f, paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-size / 2, h * 0.5f * (1 - sc2))
+        drawLine(0f, 0f, 0f, size, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLTBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineTakenByLines(scale, w, h, paint)
+}
