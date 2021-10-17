efe2e0124b2ed152cb5063f27a9ed9d60de7642c
Anwesh43 <anweshthecool0@gmail.com>
2021-10-18 02:02:40 +0530

    adding LineTakenByLinesView class

--- linetakenbylinesview/src/main/java/com/example/linetakenbylinesview/LineTakenByLinesView.kt
+++ linetakenbylinesview/src/main/java/com/example/linetakenbylinesview/LineTakenByLinesView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLTBLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineTakenByLines(scale, w, h, paint)
 }
+
+class LineTakenByLinesView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
