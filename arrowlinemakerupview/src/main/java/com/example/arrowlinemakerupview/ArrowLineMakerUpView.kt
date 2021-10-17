a0f6e53c5ece7b29ca71c4ce797aacdccf4ee983
Anwesh43 <anweshthecool0@gmail.com>
2021-10-17 22:54:29 +0530

    adding ArrowLineMakerUp class

--- arrowlinemakerupview/src/main/java/com/example/arrowlinemakerupview/ArrowLineMakerUpView.kt
+++ arrowlinemakerupview/src/main/java/com/example/arrowlinemakerupview/ArrowLineMakerUpView.kt
@@ -166,5 +166,28 @@ class ArrowLineMakerUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArrowLineMakerUp(var i : Int) {
+
+        private var curr : ALMUNode = ALMUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
 
