4025e88aae5001244410d85499e25a1813c5ac5b
Anwesh43 <anweshthecool0@gmail.com>
2021-10-19 04:12:40 +0530

    adding SLMNode class

--- sharelinemoverview/src/main/java/com/example/sharelinemoverview/ShareLineMoverView.kt
+++ sharelinemoverview/src/main/java/com/example/sharelinemoverview/ShareLineMoverView.kt
@@ -121,4 +121,45 @@ class ShareLineMoverView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SLMNode(var i : Int, val state : State = State()) {
+
+        private var prev : SLMNode? = null
+        private var next : SLMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SLMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSLMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SLMNode {
+            var curr : SLMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
