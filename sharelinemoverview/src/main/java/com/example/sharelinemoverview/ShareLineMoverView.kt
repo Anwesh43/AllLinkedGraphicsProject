ca1d9d1af07d22c84969409c34a6adf75a9a60c8
Anwesh43 <anweshthecool0@gmail.com>
2021-10-19 03:53:38 +0530

    adding ShareLineMoverView class

--- sharelinemoverview/src/main/java/com/example/sharelinemoverview/ShareLineMoverView.kt
+++ sharelinemoverview/src/main/java/com/example/sharelinemoverview/ShareLineMoverView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawSLMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawShareLineMover(scale, w, h, paint)
 }
+
+class ShareLineMoverView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
