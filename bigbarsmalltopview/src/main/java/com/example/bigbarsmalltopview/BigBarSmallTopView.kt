d67f3dbd9221f615ada842ce6d3436713912fbbb
Anwesh43 <anweshthecool0@gmail.com>
2021-10-21 02:51:57 +0530

    adding Canvas extension methods

--- bigbarsmalltopview/src/main/java/com/example/bigbarsmalltopview/BigBarSmallTopView.kt
+++ bigbarsmalltopview/src/main/java/com/example/bigbarsmalltopview/BigBarSmallTopView.kt
@@ -28,3 +28,29 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBigBarSmallTop(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val smallSize : Float = Math.min(w, h) / smallSizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    save()
+    translate(-w / 2 - size / 2 + (w / 2 + size  / 2) * sc1, 0f)
+    drawRect(RectF(-size / 2, -size / 2, size / 2, size / 2), paint)
+    restore()
+    save()
+    translate(size / 2, -h / 2 - smallSize + (h / 2 + smallSize - size / 2) * sc2)
+    drawRect(RectF(-smallSize, -smallSize, 0f, 0f), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawBBSTNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBigBarSmallTop(scale, w, h, paint)
+}
