a1bc2256556e7136bcead0dd2d56d157a63217f4
Anwesh43 <anweshthecool0@gmail.com>
2021-10-21 03:04:58 +0530

    adding BBSTNode class

--- bigbarsmalltopview/src/main/java/com/example/bigbarsmalltopview/BigBarSmallTopView.kt
+++ bigbarsmalltopview/src/main/java/com/example/bigbarsmalltopview/BigBarSmallTopView.kt
@@ -119,4 +119,45 @@ class BigBarSmallTopView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class BBSTNode(var i : Int, val state : State = State()) {
+
+        private var next : BBSTNode? = null
+        private var prev : BBSTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BBSTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBBSTNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BBSTNode {
+            var curr : BBSTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
+
 }
