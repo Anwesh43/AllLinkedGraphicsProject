3f91bdd786e48e44c8ec2239a4b7e8a807ca31b0
Anwesh43 <anweshthecool0@gmail.com>
2021-10-22 01:41:24 +0530

    adding DirecLinesCreate class

--- direclinescreateview/src/main/java/com/example/direclinescreateview/DirecLinesCreateView.kt
+++ direclinescreateview/src/main/java/com/example/direclinescreateview/DirecLinesCreateView.kt
@@ -165,4 +165,27 @@ class DirecLinesCreateView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class DirecLinesCreate(var i : Int, val state : State = State()) {
+
+        private var curr : DLCNode = DLCNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+    }
 }
