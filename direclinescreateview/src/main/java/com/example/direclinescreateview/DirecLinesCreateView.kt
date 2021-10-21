d94aed8dd7b0df6c1fea7898e4152bb0ddb0f03a
Anwesh43 <anweshthecool0@gmail.com>
2021-10-22 01:44:47 +0530

    adding static create method

--- direclinescreateview/src/main/java/com/example/direclinescreateview/DirecLinesCreateView.kt
+++ direclinescreateview/src/main/java/com/example/direclinescreateview/DirecLinesCreateView.kt
@@ -213,4 +213,13 @@ class DirecLinesCreateView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : DirecLinesCreateView {
+            val view : DirecLinesCreateView = DirecLinesCreateView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
