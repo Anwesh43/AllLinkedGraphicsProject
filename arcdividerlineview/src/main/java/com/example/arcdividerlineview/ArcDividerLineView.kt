20be14ca37459ef5d2ebffc9c0b28adcba46fb71
Anwesh43 <anweshthecool0@gmail.com>
2021-10-23 17:56:56 +0530

    adding Renderer class

--- arcdividerlineview/src/main/java/com/example/arcdividerlineview/ArcDividerLineView.kt
+++ arcdividerlineview/src/main/java/com/example/arcdividerlineview/ArcDividerLineView.kt
@@ -202,4 +202,27 @@ class ArcDividerLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcDividerLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val adl : ArcDividerLine = ArcDividerLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            adl.draw(canvas, paint)
+            animator.animate {
+                adl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            adl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
