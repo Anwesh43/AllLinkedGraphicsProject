3d23562c92c5bc80e33e8265e8daad34e8401b57
Anwesh43 <anweshthecool0@gmail.com>
2021-10-23 17:25:03 +0530

    adding Canvas extension methods

--- arcdividerlineview/src/main/java/com/example/arcdividerlineview/ArcDividerLineView.kt
+++ arcdividerlineview/src/main/java/com/example/arcdividerlineview/ArcDividerLineView.kt
@@ -28,3 +28,49 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawArcDividerLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        translate(-w / 2, h / 2)
+        drawArc(
+            RectF(
+                -size / 2,
+                -size /2,
+                size / 2,
+                size / 2
+            )
+            , -deg * sc3,
+            deg * (sc1 - sc3),
+            false,
+            paint
+        )
+        restore()
+    }
+    drawLine(
+        0f,
+        -h / 2 + h * 0.5f * sc4,
+        0f,
+        -h / 2 + h * 0.5f * sc2,
+        paint
+    )
+    restore()
+}
+
+fun Canvas.drawADLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawArcDividerLine(scale, w, h, paint)
+}
