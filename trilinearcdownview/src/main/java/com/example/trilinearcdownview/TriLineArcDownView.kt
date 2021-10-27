5836443c6d0dd5ddb7a27efbf32f1b0bad7cdf98
Anwesh43 <anweshthecool0@gmail.com>
2021-10-27 23:02:18 +0530

    adding TriLineArcDown class

--- trilinearcdownview/src/main/java/com/example/trilinearcdownview/TriLineArcDownView.kt
+++ trilinearcdownview/src/main/java/com/example/trilinearcdownview/TriLineArcDownView.kt
@@ -175,4 +175,27 @@ class TriLineArcDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TriLineArcDown(var i : Int) {
+
+        private var curr : TLADNode = TLADNode(0)
+        private var dir : Int = 1
+        
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+        
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
