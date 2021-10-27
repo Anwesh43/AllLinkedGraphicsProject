5a52ce36a170cca480e79fc1e18e2e0dd2d010f2
Anwesh43 <anweshthecool0@gmail.com>
2021-10-27 21:19:38 +0530

    adding TLADNode class

--- trilinearcdownview/src/main/java/com/example/trilinearcdownview/TriLineArcDownView.kt
+++ trilinearcdownview/src/main/java/com/example/trilinearcdownview/TriLineArcDownView.kt
@@ -134,4 +134,45 @@ class TriLineArcDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TLADNode(var i : Int, val state : State = State()) {
+
+        private var prev : TLADNode? = null
+        private var next : TLADNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TLADNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTLADNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TLADNode {
+            var curr : TLADNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
