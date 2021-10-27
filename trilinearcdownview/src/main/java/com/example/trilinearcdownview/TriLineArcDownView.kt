68246b136b6e8e91ef5340bbf8d10c343ae60054
Anwesh43 <anweshthecool0@gmail.com>
2021-10-27 20:49:25 +0530

    adding TriLineArcDownView class

--- trilinearcdownview/src/main/java/com/example/trilinearcdownview/TriLineArcDownView.kt
+++ trilinearcdownview/src/main/java/com/example/trilinearcdownview/TriLineArcDownView.kt
@@ -71,3 +71,19 @@ fun Canvas.drawTLADNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTriLineArcDown(scale, w, h, paint)
 }
+
+class TriLineArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
