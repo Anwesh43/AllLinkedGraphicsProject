64615bbe8da48b204d0a507e8e9cc988d9d70874
Anwesh43 <anweshthecool0@gmail.com>
2021-10-28 14:49:39 +0530

    adding Canvas extension methods

--- circlequadlineview/src/main/java/com/example/circlequadlineview/CircleQuadLineView.kt
+++ circlequadlineview/src/main/java/com/example/circlequadlineview/CircleQuadLineView.kt
@@ -29,3 +29,31 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawCircleQuadLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2 + (h * 0.5f + size) * sc3)
+    rotate(180f * sc3)
+    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -90f, 360f * sc1, false, paint)
+    for (j in 0..3) {
+        save()
+        rotate(90f * j)
+        drawLine(size / 2, 0f, size / 2 - size * 0.5f * sc2, -size * 0.5f * sc2, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawCQLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    paint.strokeCap = Paint.Cap.ROUND
+    drawCircleQuadLine(scale, w, h, paint)
+}
