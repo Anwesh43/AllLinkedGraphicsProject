dea27a8a2e541008515b12c05432d680af5a8c99
Anwesh43 <anweshthecool0@gmail.com>
2021-10-28 14:50:41 +0530

    adding CircleQuadLineView class

--- circlequadlineview/src/main/java/com/example/circlequadlineview/CircleQuadLineView.kt
+++ circlequadlineview/src/main/java/com/example/circlequadlineview/CircleQuadLineView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawCQLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawCircleQuadLine(scale, w, h, paint)
 }
+
+class CircleQuadLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
