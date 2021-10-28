4b1d1bb1df6b4679f596c8d3e7e82c24c61113e4
Anwesh43 <anweshthecool0@gmail.com>
2021-10-29 02:39:30 +0530

    adding HLTNode class

--- hlinetravellerview/src/main/java/com/example/hlinetravellerview/HLineTravellerView.kt
+++ hlinetravellerview/src/main/java/com/example/hlinetravellerview/HLineTravellerView.kt
@@ -124,4 +124,45 @@ class HLineTravellerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HLTNode(var i : Int, val state : State = State()) {
+
+        private var next : HLTNode? = null
+        private var prev : HLTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HLTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHLTNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HLTNode {
+            var curr : HLTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
