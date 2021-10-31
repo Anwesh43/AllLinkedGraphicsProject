0f3905c4ad4df399d7aca42c91fde2e3e8644520
Anwesh43 <anweshthecool0@gmail.com>
2021-11-01 02:41:59 +0530

    adding BarJoinToGoUp class

--- barjointogoupview/src/main/java/com/example/barjointogoupview/BarJoinToGoUpView.kt
+++ barjointogoupview/src/main/java/com/example/barjointogoupview/BarJoinToGoUpView.kt
@@ -158,4 +158,27 @@ class BarJoinToGoUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BarJoinToGoUp(var i : Int) {
+
+        private var curr : BJTGUNode = BJTGUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
