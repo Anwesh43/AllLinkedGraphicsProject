95e8233778180e86cbcc81766f7ed66cfa6bc298
Anwesh43 <anweshthecool0@gmail.com>
2021-11-01 01:56:49 +0530

    adding barjointogoupview setup

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.hlinetravellerview.HLineTravellerView
+//import com.example.hlinetravellerview.HLineTravellerView
 
 //import com.example.circlequadlineview.CircleQuadLineView
 
@@ -242,7 +242,7 @@ class MainActivity : AppCompatActivity() {
         //LineDivideBarMoveView.create(this)
         //TriLineArcDownView.create(this)
         //CircleQuadLineView.create(this)
-        HLineTravellerView.create(this)
+        //HLineTravellerView.create(this)
         fullScreen()
     }
 }
