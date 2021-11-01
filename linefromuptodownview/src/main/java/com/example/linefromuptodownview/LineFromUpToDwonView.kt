075065afc3ad5cada726ce0c956a5cb4decb098a
Anwesh43 <anweshthecool0@gmail.com>
2021-11-02 00:42:52 +0530

    adding LineFromUpToDownView class

--- linefromuptodownview/src/main/java/com/example/linefromuptodownview/LineFromUpToDwonView.kt
+++ linefromuptodownview/src/main/java/com/example/linefromuptodownview/LineFromUpToDwonView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLFUDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineFromUpToDown(scale, w, h, paint)
 }
+
+class LineFromUpToDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
