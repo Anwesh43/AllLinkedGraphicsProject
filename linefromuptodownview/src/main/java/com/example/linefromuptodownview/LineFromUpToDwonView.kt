3c4492c30810681da3a85bc1db7d1a614ab8a7c2
Anwesh43 <anweshthecool0@gmail.com>
2021-11-02 00:40:01 +0530

    adding Canvas extension methods

--- linefromuptodownview/src/main/java/com/example/linefromuptodownview/LineFromUpToDwonView.kt
+++ linefromuptodownview/src/main/java/com/example/linefromuptodownview/LineFromUpToDwonView.kt
@@ -28,3 +28,36 @@ val sizeFactor : Float = 4.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineFromUpToDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        translate(0f, -(h / 2 - size) * (1 - sc2))
+        for (k in 0..1) {
+            save()
+            scale(1f - 2 * k, 1f)
+            translate(-size, 0f)
+            rotate(deg * sc3)
+            drawLine(0f, -size + size * sc1, 0f, -size, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLFUDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineFromUpToDown(scale, w, h, paint)
+}
