f9fdc32b42c751afe6a34d1e4020ad2a938e8a2e
Anwesh43 <anweshthecool0@gmail.com>
2021-11-03 04:06:55 +0530

    adding Renderer class

--- arcfromuptosideview/src/main/java/com/example/arcfromuptosideview/ArcFromUpToSideView.kt
+++ arcfromuptosideview/src/main/java/com/example/arcfromuptosideview/ArcFromUpToSideView.kt
@@ -189,4 +189,27 @@ class ArcFromUpToSideView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcFromUpToSideView) {
+
+        private val animator : Animator = Animator(view)
+        private val afuts : ArcFromUpToSide = ArcFromUpToSide(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            afuts.draw(canvas, paint)
+            animator.animate {
+                afuts.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            afuts.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
