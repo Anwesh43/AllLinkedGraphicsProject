933f06833493af13a83b501b61901293040f723f
Anwesh43 <anweshthecool0@gmail.com>
2021-11-03 04:08:30 +0530

    adding static create methid

--- arcfromuptosideview/src/main/java/com/example/arcfromuptosideview/ArcFromUpToSideView.kt
+++ arcfromuptosideview/src/main/java/com/example/arcfromuptosideview/ArcFromUpToSideView.kt
@@ -214,4 +214,13 @@ class ArcFromUpToSideView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : ArcFromUpToSideView {
+            val view : ArcFromUpToSideView = ArcFromUpToSideView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
