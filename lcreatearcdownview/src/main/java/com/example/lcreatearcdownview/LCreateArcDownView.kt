60aa3473f474dc0f55a3481650330fafa1f68a3e
Anwesh43 <anweshthecool0@gmail.com>
2021-11-04 16:57:44 +0530

    adding Renderer class

--- lcreatearcdownview/src/main/java/com/example/lcreatearcdownview/LCreateArcDownView.kt
+++ lcreatearcdownview/src/main/java/com/example/lcreatearcdownview/LCreateArcDownView.kt
@@ -25,6 +25,7 @@ val sizeFactor : Float = 4.9f
 val arcSizeFactor : Float = 14.9f
 val delay : Long = 20
 val deg : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -196,6 +197,29 @@ class LCreateArcDownView(ctx : Context) : View(ctx) {
 
         fun startUpdating(cb : () -> Unit) {
             curr.startUpdating(cb)
+        }
+    }
+
+    data class Renderer(var view : LCreateArcDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lcad : LCreateArcDown = LCreateArcDown(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lcad.draw(canvas, paint)
+            animator.animate {
+                lcad.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lcad.startUpdating {
+                animator.start()
+            }
         }
     }
 }
