1fa6cd147141375119a71057009194d434dfd540
Anwesh43 <anweshthecool0@gmail.com>
2021-11-04 16:35:09 +0530

    adding Canvas extension methods

--- lcreatearcdownview/src/main/java/com/example/lcreatearcdownview/LCreateArcDownView.kt
+++ lcreatearcdownview/src/main/java/com/example/lcreatearcdownview/LCreateArcDownView.kt
@@ -29,3 +29,45 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLCreateArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val arcSize : Float = Math.min(w, h) / arcSizeFactor
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val initY : (Int) -> Float = {
+        ((-h / 2) * (1 - it) + (w / 2) * it)  * (1 - scale.divideScale(it, parts))
+    }
+    save()
+    translate(w / 2, h / 2 + (h / 2) * sc5)
+    rotate(deg * 1.5f * sc4)
+    for (j in 0..1) {
+        save()
+        translate(0f, initY(j))
+        rotate(deg * j)
+        drawLine(0f, 0f, 0f, -size, paint)
+        restore()
+    }
+    drawArc(
+        RectF(
+            -arcSize / 2,
+            -arcSize / 2,
+            arcSize / 2,
+            arcSize / 2),
+        -deg,
+        deg * sc3,
+        true,
+        paint
+    )
+    restore()
+}
+
+fun Canvas.drawLCADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLCreateArcDown(scale, w ,h, paint)
+}
