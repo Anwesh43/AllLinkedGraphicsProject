b4be0ad3855590e3e3b957a0bca6d77405d0ad10
Anwesh43 <anweshthecool0@gmail.com>
2021-11-04 16:51:44 +0530

    adding LCADNode class

--- lcreatearcdownview/src/main/java/com/example/lcreatearcdownview/LCreateArcDownView.kt
+++ lcreatearcdownview/src/main/java/com/example/lcreatearcdownview/LCreateArcDownView.kt
@@ -134,4 +134,45 @@ class LCreateArcDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LCADNode(var i : Int, val state : State = State()) {
+
+        private var prev : LCADNode? = null
+        private var next : LCADNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LCADNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLCADNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LCADNode {
+            var curr : LCADNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
