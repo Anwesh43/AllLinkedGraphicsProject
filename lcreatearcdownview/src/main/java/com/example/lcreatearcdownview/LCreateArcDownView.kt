9414e6b29625b4622f69dd5524460075d03fbe22
Anwesh43 <anweshthecool0@gmail.com>
2021-11-04 16:53:36 +0530

    adding LCreateArcDown class

--- lcreatearcdownview/src/main/java/com/example/lcreatearcdownview/LCreateArcDownView.kt
+++ lcreatearcdownview/src/main/java/com/example/lcreatearcdownview/LCreateArcDownView.kt
@@ -175,4 +175,27 @@ class LCreateArcDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LCreateArcDown(var i : Int) {
+
+        private var curr : LCADNode = LCADNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
