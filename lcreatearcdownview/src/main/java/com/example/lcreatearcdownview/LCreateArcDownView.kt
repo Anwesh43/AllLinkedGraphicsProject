5148f156952b9445a14e6eb40ea9721a9863f3b4
Anwesh43 <anweshthecool0@gmail.com>
2021-11-04 16:38:22 +0530

    adding LCreateArcDownView class

--- lcreatearcdownview/src/main/java/com/example/lcreatearcdownview/LCreateArcDownView.kt
+++ lcreatearcdownview/src/main/java/com/example/lcreatearcdownview/LCreateArcDownView.kt
@@ -71,3 +71,19 @@ fun Canvas.drawLCADNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLCreateArcDown(scale, w ,h, paint)
 }
+
+class LCreateArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
