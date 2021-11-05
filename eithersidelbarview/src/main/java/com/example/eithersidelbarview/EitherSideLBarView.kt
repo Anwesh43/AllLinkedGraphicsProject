936f4eea90b3f37ad62f2a1033cbbb830651fd34
Anwesh43 <anweshthecool0@gmail.com>
2021-11-06 02:46:28 +0530

    adding renderer in view

--- eithersidelbarview/src/main/java/com/example/eithersidelbarview/EitherSideLBarView.kt
+++ eithersidelbarview/src/main/java/com/example/eithersidelbarview/EitherSideLBarView.kt
@@ -62,14 +62,16 @@ fun Canvas.drawESLBNode(i : Int, scale : Float, paint : Paint) {
 
 class EitherSideLBarView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
