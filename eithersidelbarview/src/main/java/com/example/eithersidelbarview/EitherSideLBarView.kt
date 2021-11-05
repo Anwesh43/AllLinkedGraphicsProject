2b7e42665cc858464db48ea003b4f549c591d184
Anwesh43 <anweshthecool0@gmail.com>
2021-11-06 02:22:51 +0530

    adding Canvas extension methods : EitherSideLBarView

--- eithersidelbarview/src/main/java/com/example/eithersidelbarview/EitherSideLBarView.kt
+++ eithersidelbarview/src/main/java/com/example/eithersidelbarview/EitherSideLBarView.kt
@@ -24,7 +24,37 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val deg : Float = 90f
+val lSizeFactor : Float = 5.6f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawEitherSideLBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val lSize : Float = size / lSizeFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2 + paint.strokeWidth / 2) * sc4)
+    rotate(deg * sc3)
+    drawLine(-size * 0.5f * sc1, 0f, size * 0.5f * sc1, 0f, paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawRect(RectF(-size / 2, -size * 0.5f * sc2, -size / 2 + lSize, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawESLBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawEitherSideLBar(scale, w, h, paint)
+}
