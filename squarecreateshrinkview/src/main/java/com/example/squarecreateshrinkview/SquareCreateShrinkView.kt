43a681957f61e318a209353cd35134a28a9e8f0a
Anwesh43 <anweshthecool0@gmail.com>
2021-11-05 17:04:52 +0530

    adding Canvas extension methods

--- squarecreateshrinkview/src/main/java/com/example/squarecreateshrinkview/SquareCreateShrinkView.kt
+++ squarecreateshrinkview/src/main/java/com/example/squarecreateshrinkview/SquareCreateShrinkView.kt
@@ -27,3 +27,36 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSquareCreateShrink(scale : Float, w : Float, h : Float, paint : Paint) {
+    val newSize : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val size : Float = newSize * (1 - sc3)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        save()
+        translate(0f, (-h / 2 - size / 2) * (1 - sc1))
+        drawLine(0f, -size / 2, 0f, size / 2, paint)
+        restore()
+        save()
+        translate((w /2 + size / 2) * (1 - sc2), -size / 2 +  size * j)
+        drawLine(-size / 2, 0f, size / 2, 0f, paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSCSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSquareCreateShrink(scale, w, h, paint)
+}
