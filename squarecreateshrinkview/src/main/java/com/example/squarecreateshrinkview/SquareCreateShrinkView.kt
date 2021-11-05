631f484f0bf8b84aeed1d46fbd60997475460602
Anwesh43 <anweshthecool0@gmail.com>
2021-11-05 16:56:56 +0530

    adding constants and libraries

---
+++ squarecreateshrinkview/src/main/java/com/example/squarecreateshrinkview/SquareCreateShrinkView.kt
@@ -0,0 +1,25 @@
+package com.example.squarecreateshrinkview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#01579B",
+    "#D50000",
+    "#DD2C00",
+    "#2962FF",
+    "#00838F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.03f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
