578c8e025537e7c36b7823dfc5f96cc1d80e732e
Anwesh43 <anweshthecool0@gmail.com>
2021-11-07 02:38:54 +0530

    adding QAFNode class

--- quarterarcfillview/src/main/java/com/example/quarterarcfillview/QuarterArcFillView.kt
+++ quarterarcfillview/src/main/java/com/example/quarterarcfillview/QuarterArcFillView.kt
@@ -129,4 +129,45 @@ class QuarterArcFillView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class QAFNode(var i : Int, val state : State = State()) {
+
+        private var next : QAFNode? = null
+        private var prev : QAFNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i  < colors.size - 1) {
+                next = QAFNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawQAFNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : QAFNode {
+            var curr : QAFNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
