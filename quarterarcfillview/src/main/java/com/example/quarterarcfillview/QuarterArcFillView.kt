45a9a30381551fa43aa29240819d680dd1916846
Anwesh43 <anweshthecool0@gmail.com>
2021-11-07 02:20:45 +0530

    adding constants and libraries

--- quarterarcfillview/src/main/java/com/example/quarterarcfillview/QuarterArcFillView.kt
+++ quarterarcfillview/src/main/java/com/example/quarterarcfillview/QuarterArcFillView.kt
@@ -1,2 +1,29 @@
 package com.example.quarterarcfillview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#01579B",
+    "#D50000",
+    "#DD2C00",
+    "#2962FF",
+    "#00838F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.03f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val deg : Float = 300f
+val start : Float = 60f
+val rot : Float = 180f
