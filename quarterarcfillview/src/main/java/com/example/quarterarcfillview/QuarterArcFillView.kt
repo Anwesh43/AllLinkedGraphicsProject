a31969a90768998e8e7737cfb0bde5d43c7286a7
Anwesh43 <anweshthecool0@gmail.com>
2021-11-07 02:21:40 +0530

    adding Int and Float extension methdods

--- quarterarcfillview/src/main/java/com/example/quarterarcfillview/QuarterArcFillView.kt
+++ quarterarcfillview/src/main/java/com/example/quarterarcfillview/QuarterArcFillView.kt
@@ -27,3 +27,7 @@ val delay : Long = 20
 val deg : Float = 300f
 val start : Float = 60f
 val rot : Float = 180f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
