18415350acca26f90b5fcf70c5e1164f9e81c8e2
Anwesh43 <anweshthecool0@gmail.com>
2021-11-08 01:04:33 +0530

    adding constants and libraries

---
+++ vlineencloserview/src/main/java/com/example/vlineencloserview/VLineEncloserView.kt
@@ -0,0 +1,27 @@
+package com.example.vlineencloserview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#01579B",
+    "#D50000",
+    "#DD2C00",
+    "#2962FF",
+    "#00838F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.03f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val rot : Float = 180f
+val backColor : Int = Color.parseColor("#BDBDBD")
+
