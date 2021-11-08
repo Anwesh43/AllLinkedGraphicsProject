c59649df259d53e81fd7685da39effe1eba626e5
Anwesh43 <anweshthecool0@gmail.com>
2021-11-09 01:37:13 +0530

    adding LinePeakRotMoveView cclass

--- linepeakrotmoveview/src/main/java/com/example/linepeakrotmoveview/LinePeakRotMoveView.kt
+++ linepeakrotmoveview/src/main/java/com/example/linepeakrotmoveview/LinePeakRotMoveView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLPRMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLinePeakRotMove(w, h, scale, paint)
 }
+
+class LinePeakRotMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
