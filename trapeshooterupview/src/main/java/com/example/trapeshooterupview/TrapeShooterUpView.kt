d16f41dde077526be48e56fc91b83b71ebb0574e
Anwesh43 <anweshthecool0@gmail.com>
2021-11-11 01:28:10 +0530

    adding constants and libraries

---
+++ trapeshooterupview/src/main/java/com/example/trapeshooterupview/TrapeShooterUpView.kt
@@ -0,0 +1,25 @@
+package com.example.trapeshooterupview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#01579B",
+    "#D50000",
+    "#DD2C00",
+    "#2962FF",
+    "#00838F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
