8266cf900d15493a7160f99d232898f80685273a
Anwesh43 <anweshthecool0@gmail.com>
2021-11-11 01:39:26 +0530

    adding Canvas extension methods

--- trapeshooterupview/src/main/java/com/example/trapeshooterupview/TrapeShooterUpView.kt
+++ trapeshooterupview/src/main/java/com/example/trapeshooterupview/TrapeShooterUpView.kt
@@ -17,7 +17,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,36 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawTrapeShooterUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2)
+    save()
+    translate(0f, h / 2 - size - (h) * sc5)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawLine(-size * 0.25f * sc1, 0f, 0f, 0f, paint)
+        drawLine(-size / 4, 0f, -size / 4, size * 0.25f * sc2, paint)
+        drawLine(-size / 4, size / 4, -size / 4 - size * 0.25f * sc3, size / 4, paint)
+        drawLine(-size / 2, size / 4, -size / 2, size / 4 + size * 0.25f * sc4, paint)
+        restore()
+    }
+    restore()
+    restore()
+}
+
+fun Canvas.drawTSUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTrapeShooterUp(scale, w, h, paint)
+}
