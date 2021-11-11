2c0804180659be47f193ed16a07bd2c2921cfde8
Anwesh43 <anweshthecool0@gmail.com>
2021-11-12 03:03:02 +0530

    adding Canvas extension methods

--- bararcextenderview/src/main/java/com/example/bararcextenderview/BarArcExtenderView.kt
+++ bararcextenderview/src/main/java/com/example/bararcextenderview/BarArcExtenderView.kt
@@ -29,3 +29,38 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBarArcExtender(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val arcR : Float = Math.min(w, h) / arcFactor
+    val barH : Float = Math.min(w, h) / barHFactor
+    save()
+    translate(w / 2, h / 2)
+    save()
+    translate(0f, h /2 - h * sc4)
+    drawArc(
+        RectF(-arcR / 2, -arcR / 2, arcR / 2, arcR / 2),
+        180f,
+        180f * sc2,
+        true,
+        paint
+    )
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawRect(RectF(-w / 2 + w * 0.5f * sc3, -barH, -w / 2 + w * 0.5f * sc1, 0f), paint)
+        restore()
+    }
+    restore()
+    restore()
+}
+
+fun Canvas.drawBAENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBarArcExtender(scale, w, h, paint)
+}
