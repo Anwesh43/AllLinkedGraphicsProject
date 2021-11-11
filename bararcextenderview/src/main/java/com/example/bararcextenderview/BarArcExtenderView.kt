7bf4c267bf8b3d54601d7cbbb97f4fa7b55a1d9c
Anwesh43 <anweshthecool0@gmail.com>
2021-11-12 03:33:52 +0530

    adding BarArcExtender class

--- bararcextenderview/src/main/java/com/example/bararcextenderview/BarArcExtenderView.kt
+++ bararcextenderview/src/main/java/com/example/bararcextenderview/BarArcExtenderView.kt
@@ -134,7 +134,7 @@ class BarArcExtenderView(ctx : Context) : View(ctx) {
         private var prev : BAENode? = null
 
         init {
-
+            addNeighbor()
         }
 
         fun addNeighbor() {
@@ -166,6 +166,29 @@ class BarArcExtenderView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class BarArcExtender(var i : Int) {
+
+        private var curr : BAENode = BAENode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
