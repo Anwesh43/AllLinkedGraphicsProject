b4fe60267541a1c1f78c3efbf50d49408c7d9b75
Anwesh43 <anweshthecool0@gmail.com>
2021-11-13 04:15:31 +0530

    adding constants and libraries

---
+++ linebartravellerview/src/main/java/com/example/linebartravellerview/LineBarTravellerView.kt
@@ -0,0 +1,26 @@
+package com.example.linebartravellerview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#01579B",
+    "#D50000",
+    "#DD2C00",
+    "#2962FF",
+    "#00838F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val sizeFactor : Float = 5.9f
+val strokeFactor : Float = 90f
