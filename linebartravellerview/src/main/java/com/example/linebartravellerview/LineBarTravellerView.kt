c83baf9efe0c5f18aa908269211ecce1a0676c0a
Anwesh43 <anweshthecool0@gmail.com>
2021-11-13 04:33:53 +0530

    adding LineBarTravellerView class

--- linebartravellerview/src/main/java/com/example/linebartravellerview/LineBarTravellerView.kt
+++ linebartravellerview/src/main/java/com/example/linebartravellerview/LineBarTravellerView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLBTNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBarTraveller(scale, w, h, paint)
 }
+
+class LineBarTravellerView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
