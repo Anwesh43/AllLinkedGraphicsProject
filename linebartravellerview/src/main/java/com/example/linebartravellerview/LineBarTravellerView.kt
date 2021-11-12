9255ee3a286f1c2a898cbc3bf6682d2258c6e76d
Anwesh43 <anweshthecool0@gmail.com>
2021-11-13 04:55:33 +0530

    adding LBTNode class

--- linebartravellerview/src/main/java/com/example/linebartravellerview/LineBarTravellerView.kt
+++ linebartravellerview/src/main/java/com/example/linebartravellerview/LineBarTravellerView.kt
@@ -121,4 +121,45 @@ class LineBarTravellerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBTNode(var i : Int, val state : State = State()) {
+
+        private var next : LBTNode? = null
+        private var prev : LBTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBTNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBTNode {
+            var curr : LBTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
