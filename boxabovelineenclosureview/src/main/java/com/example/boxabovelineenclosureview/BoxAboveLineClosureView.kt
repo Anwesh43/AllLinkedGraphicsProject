9ed7e3f4b5659cb5f1c35acc348add533d0373b3
Anwesh43 <anweshthecool0@gmail.com>
2021-11-15 12:21:19 +0530

    adding Canvas extension methods

--- boxabovelineenclosureview/src/main/java/com/example/boxabovelineenclosureview/BoxAboveLineClosureView.kt
+++ boxabovelineenclosureview/src/main/java/com/example/boxabovelineenclosureview/BoxAboveLineClosureView.kt
@@ -30,3 +30,37 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBoxAboveLineClosure(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val boxSize : Float = Math.min(w, h)/
+    save()
+    translate(w / 2, h / 2 + (h / 2) * sc5)
+    rotate(deg * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawLine(-size / 2, 0f, -size / 2, -size * sc1, paint)
+        restore()
+    }
+    drawLine(-size / 2, -size, -size / 2 + size * sc2, -size, paint)
+    save()
+    translate(0f, -h / 2 + (h / 2 - size) * sc3)
+    drawRect(RectF(-boxSize / 2, -boxSize, boxSize / 2, 0f), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawBALCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBoxAboveLineClosure(scale, w, h, paint)
+}
