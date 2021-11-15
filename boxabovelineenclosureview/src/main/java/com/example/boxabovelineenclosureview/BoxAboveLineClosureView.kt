741b5facac9205bb648486b74b33687aef1379b9
Anwesh43 <anweshthecool0@gmail.com>
2021-11-15 12:22:40 +0530

    adding BoxAboveLineClosureView class

--- boxabovelineenclosureview/src/main/java/com/example/boxabovelineenclosureview/BoxAboveLineClosureView.kt
+++ boxabovelineenclosureview/src/main/java/com/example/boxabovelineenclosureview/BoxAboveLineClosureView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawBALCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBoxAboveLineClosure(scale, w, h, paint)
 }
+
+class BoxAboveLineClosureView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
