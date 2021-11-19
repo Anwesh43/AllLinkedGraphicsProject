35961f750426e98e6539e73671964580e392b232
Anwesh43 <anweshthecool0@gmail.com>
2021-11-20 03:59:18 +0530

    adding VDRMNode class

--- vdoorrotmoveview/src/main/java/com/example/vdoorrotmoveview/VDoorRotMoveView.kt
+++ vdoorrotmoveview/src/main/java/com/example/vdoorrotmoveview/VDoorRotMoveView.kt
@@ -130,4 +130,45 @@ class VDoorRotMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class VDRMNode(var i : Int, val state : State = State()) {
+
+        private var next : VDRMNode? = null
+        private var prev : VDRMNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = VDRMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawVDRMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : VDRMNode {
+            var curr : VDRMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
