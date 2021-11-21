a05dfd90a4658d019ce8c2a0a38126765c8d4de6
Anwesh43 <anweshthecool0@gmail.com>
2021-11-22 02:18:48 +0530

    adding Renderer class

--- altlinetreedownview/src/main/java/com/example/altlinetreedownview/AltLineTreeDownView.kt
+++ altlinetreedownview/src/main/java/com/example/altlinetreedownview/AltLineTreeDownView.kt
@@ -192,4 +192,27 @@ class AllLineTreeDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : AllLineTreeDownView) {
+
+        private val altd : AltLineTree = AltLineTree(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val animator : Animator = Animator(view)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            altd.draw(canvas, paint)
+            animator.animate {
+                altd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            altd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
