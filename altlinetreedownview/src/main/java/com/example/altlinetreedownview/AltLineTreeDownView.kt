ff9afa0ac3760627836ebbc46c71c715772fe86a
Anwesh43 <anweshthecool0@gmail.com>
2021-11-22 02:21:26 +0530

    adding static create method

--- altlinetreedownview/src/main/java/com/example/altlinetreedownview/AltLineTreeDownView.kt
+++ altlinetreedownview/src/main/java/com/example/altlinetreedownview/AltLineTreeDownView.kt
@@ -215,4 +215,13 @@ class AllLineTreeDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity)  : AllLineTreeDownView {
+            val view : AllLineTreeDownView = AllLineTreeDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
