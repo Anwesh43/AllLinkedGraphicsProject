c28953a4c0482af3ee246d48ef09567dc0ffb9fd
Anwesh43 <anweshthecool0@gmail.com>
2021-11-21 16:36:15 +0530

    adding CrossTopLineView class

--- crosstoplineview/src/main/java/com/example/crosstoplineview/CrossTopLineView.kt
+++ crosstoplineview/src/main/java/com/example/crosstoplineview/CrossTopLineView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawCTLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawCrossTopLine(scale, w, h, paint)
 }
+
+class CrossTopLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
