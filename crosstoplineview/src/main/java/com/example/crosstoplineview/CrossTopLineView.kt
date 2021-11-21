eb669996a87fe2d0fd41a7eb5db3a2cc6f9fcf23
Anwesh43 <anweshthecool0@gmail.com>
2021-11-21 16:04:45 +0530

    adding Canvas extension methods

--- crosstoplineview/src/main/java/com/example/crosstoplineview/CrossTopLineView.kt
+++ crosstoplineview/src/main/java/com/example/crosstoplineview/CrossTopLineView.kt
@@ -28,3 +28,40 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawCrossTopLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + size / 2) * sc4, h / 2)
+    rotate(rot * sc3)
+    for (j in 0..1) {
+        save()
+        rotate(45f * (1f - 2 * j))
+        drawLine(
+            0f,
+            -size * 0.5f * sc1,
+            0f,
+            size * 0.5f * sc1,
+            paint
+        )
+        restore()
+    }
+    save()
+    translate(0f, -h / 2 + (h / 2 - size / 2) * sc2)
+    drawLine(-size / 2, 0f, size / 2, 0f, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawCTLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawCrossTopLine(scale, w, h, paint)
+}
