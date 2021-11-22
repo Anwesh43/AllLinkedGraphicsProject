37a0472614a42899107a04acd310baed87eba0fb
Anwesh43 <anweshthecool0@gmail.com>
2021-11-23 01:55:42 +0530

    adding Canvas extension methods

--- minipiedownview/src/main/java/com/example/minipiedownview/MiniPieDownView.kt
+++ minipiedownview/src/main/java/com/example/minipiedownview/MiniPieDownView.kt
@@ -29,3 +29,26 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i  * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawMiniPieDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+    for (j in 0..(lines - 1)) {
+        save()
+        rotate(-deg * j)
+        drawLine(0f, 0f, 0f, size * scale.divideScale(j, parts), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawMPDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawMiniPieDown(scale, w, h, paint)
+}
