0db810714a1feb1a1a1809d87c148ed697cf0366
Anwesh43 <anweshthecool0@gmail.com>
2021-11-24 01:46:41 +0530

    adding Canvas extension methods: LineTsHIFTdOWNvIEW

--- linetshiftdownview/src/main/java/com/example/linetshiftdownview/LineTShiftDownView.kt
+++ linetshiftdownview/src/main/java/com/example/linetshiftdownview/LineTShiftDownView.kt
@@ -27,3 +27,34 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineTShiftDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-size * 0.5f * sc3, 0f)
+        drawLine(0f, 0f, 0f, -size * sc1, paint)
+        restore()
+    }
+    save()
+    translate(0f, -h / 2 + (h / 2 - size) * sc2)
+    drawLine(-size / 2, 0f, size / 2, 0f, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawLTSDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineTShiftDown(scale, w, h, paint)
+}
