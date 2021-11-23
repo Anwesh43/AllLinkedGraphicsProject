c3b920edd3fb3056815ea7c7347656b4891dc056
Anwesh43 <anweshthecool0@gmail.com>
2021-11-24 01:40:38 +0530

    adding constants and libraries : LineTShiftDownView

---
+++ linetshiftdownview/src/main/java/com/example/linetshiftdownview/LineTShiftDownView.kt
@@ -0,0 +1,25 @@
+package com.example.linetshiftdownview
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.view.MotionEvent
+import android.view.View
+
+val colors : Array<Int> = arrayOf(
+    "#D32F2F",
+    "#006064",
+    "#01579B",
+    "#BF360C",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val sizeFactor : Float = 4.9f
+val strokeFactor : Float = 90f
