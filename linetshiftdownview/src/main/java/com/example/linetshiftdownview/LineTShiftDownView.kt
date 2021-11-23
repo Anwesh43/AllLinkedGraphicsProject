8ad85d76a2005009b2ce7d5546560acd0e399bf8
Anwesh43 <anweshthecool0@gmail.com>
2021-11-24 01:50:22 +0530

    adding LineTShiftDownView class

--- linetshiftdownview/src/main/java/com/example/linetshiftdownview/LineTShiftDownView.kt
+++ linetshiftdownview/src/main/java/com/example/linetshiftdownview/LineTShiftDownView.kt
@@ -58,3 +58,18 @@ fun Canvas.drawLTSDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineTShiftDown(scale, w, h, paint)
 }
+
+class LineTShiftDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas :  Canvas) {
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
