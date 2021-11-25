fe06acfbc4bfed3cbd9f9899bce3d7911118dddc
Anwesh43 <anweshthecool0@gmail.com>
2021-11-25 14:41:31 +0530

    LineExpandRotatorView: adding Canvas extension methods

--- lineexpandrotatorview/src/main/java/com/example/lineexpandrotatorview/LineExandRotatorView.kt
+++ lineexpandrotatorview/src/main/java/com/example/lineexpandrotatorview/LineExandRotatorView.kt
@@ -17,8 +17,8 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
-val scGap : Float = 0.02f
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 180f
@@ -28,3 +28,31 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineExpandRotator(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..2) {
+        save()
+        translate((-size / 2 + size * j) * sc3, 0f)
+        drawLine(0f, 0f, 0f, -size * sc2, paint)
+        restore()
+    }
+    drawLine(-size * 0.5f * sc1, 0f, size * 0.5f * sc1, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawLERNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineExpandRotator(scale, w, h, paint)
+}
