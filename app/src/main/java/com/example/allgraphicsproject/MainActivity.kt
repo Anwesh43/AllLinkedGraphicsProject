e1983baea2e8de44ab11bb1ba3458d4dab935817
Anwesh43 <anweshthecool0@gmail.com>
2021-11-27 01:46:45 +0530

    adding ERotDownView in activity

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.erotdownview.ERotDownView
+
 //import com.example.lineexpandrotatorview.LineExpandRotatorView
 
 //import com.example.linetshiftdownview.LineTShiftDownView
@@ -303,6 +305,7 @@ class MainActivity : AppCompatActivity() {
         //MiniPieDownView.create(this)
         //LineTShiftDownView.create(this)
         //LineExpandRotatorView.create(this)
+        ERotDownView.create(this)
         fullScreen()
     }
 }
