05f5fa76ef78bfc1a5fca8307ec218473a29d776
Anwesh43 <anweshthecool0@gmail.com>
2021-11-27 01:24:10 +0530

    adding ERotDownView class

--- erotdownview/src/main/java/com/example/erotdownview/ERotDownView.kt
+++ erotdownview/src/main/java/com/example/erotdownview/ERotDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawERDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawERotDown(scale, w, h, paint)
 }
+
+class ERotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
