d0e73169de8ff50b1ebaf24ee1f31e015060123d
Anwesh43 <anweshthecool0@gmail.com>
2021-11-27 01:02:19 +0530

    adding constants and libraries

---
+++ erotdownview/src/main/java/com/example/erotdownview/ERotDownView.kt
@@ -0,0 +1,26 @@
+package com.example.erotdownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#01579B",
+    "#D50000",
+    "#DD2C00",
+    "#2962FF",
+    "#00838F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
