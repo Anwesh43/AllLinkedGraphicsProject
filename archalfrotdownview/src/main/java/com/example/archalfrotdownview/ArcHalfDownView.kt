1b089424b9f3af52a5cd4339c556564dac9dfaaa
Anwesh43 <anweshthecool0@gmail.com>
2021-11-28 03:09:33 +0530

    adding ArcHalfDown class

--- archalfrotdownview/src/main/java/com/example/archalfrotdownview/ArcHalfDownView.kt
+++ archalfrotdownview/src/main/java/com/example/archalfrotdownview/ArcHalfDownView.kt
@@ -161,4 +161,27 @@ class ArcHalfDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArcHalfDown(var i : Int) {
+
+        private var curr : AHDNode = AHDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
