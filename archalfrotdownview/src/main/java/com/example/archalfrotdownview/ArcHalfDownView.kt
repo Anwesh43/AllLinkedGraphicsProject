6fce15c60000cd91a3ecce064a75a3a94f28cbbc
Anwesh43 <anweshthecool0@gmail.com>
2021-11-28 02:56:52 +0530

    adding Canvas extension methods

--- archalfrotdownview/src/main/java/com/example/archalfrotdownview/ArcHalfDownView.kt
+++ archalfrotdownview/src/main/java/com/example/archalfrotdownview/ArcHalfDownView.kt
@@ -28,3 +28,32 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawArcHalfDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2) * sc4)
+    rotate(deg * sc3)
+    drawArc(
+        RectF(-size / 2, -size / 2, size / 2, size / 2),
+        180f,
+        180f * sc1,
+        false,
+        paint
+    )
+    restore()
+}
+
+fun Canvas.drawAHDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcHalfDown(scale, w, h, paint)
+}
