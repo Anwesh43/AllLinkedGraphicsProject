711579cc96f5c41151a326344183ed4584e0bd02
Anwesh43 <anweshthecool0@gmail.com>
2021-11-30 03:00:26 +0530

    adding Canvas extension methods

--- boxsmallboxdropview/src/main/java/com/example/boxsmallboxdropview/BoxSmallBoxDripView.kt
+++ boxsmallboxdropview/src/main/java/com/example/boxsmallboxdropview/BoxSmallBoxDripView.kt
@@ -29,3 +29,36 @@ val boxSizeFactor : Float = 14.4f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBoxSmallBoxDrop(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val boxSize : Float = Math.min(w, h) / boxSizeFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc5)
+    drawRect(RectF(-size / 2, -size * sc1, size / 2, size / 2), paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(
+            -size / 2 - boxSize * sc2,
+            (-h / 2 + size / 2) * sc2 + (h/ 2 + size / 2 + boxSize) * sc4
+        )
+        drawRect(RectF(0f, -boxSize, boxSize, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBSBDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBoxSmallBoxDrop(scale, w, h, paint)
+}
