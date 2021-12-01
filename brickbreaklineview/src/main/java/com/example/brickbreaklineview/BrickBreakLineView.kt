d726db83b5a70297bc47467a144586e10b4e0cbe
Anwesh43 <anweshthecool0@gmail.com>
2021-12-02 01:31:37 +0530

    brickbreaklineview: adding Canvas Extension method

--- brickbreaklineview/src/main/java/com/example/brickbreaklineview/BrickBreakLineView.kt
+++ brickbreaklineview/src/main/java/com/example/brickbreaklineview/BrickBreakLineView.kt
@@ -26,3 +26,40 @@ val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val deg : Float = 90f
 val rot : Float = 180f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBrickBreakLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2) * sc4)
+    rotate(rot * sc3)
+    save()
+    translate(0f, -h / 2 + h * 0.5f * sc2)
+    drawLine(0f, 0f, 0f, -size, paint)
+    restore()
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-size / 2, 0f)
+        rotate(deg * sc2)
+        drawRect(RectF(0f, -size, size / 2, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBrickBreakLine(scale, w, h, paint)
+}
