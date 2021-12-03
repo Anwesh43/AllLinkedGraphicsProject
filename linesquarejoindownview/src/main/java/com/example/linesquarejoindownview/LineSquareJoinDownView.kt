56ec16b34fd454fc1371c716d12f250696434c45
Anwesh43 <anweshthecool0@gmail.com>
2021-12-04 02:15:43 +0530

    adding static create method

--- linesquarejoindownview/src/main/java/com/example/linesquarejoindownview/LineSquareJoinDownView.kt
+++ linesquarejoindownview/src/main/java/com/example/linesquarejoindownview/LineSquareJoinDownView.kt
@@ -216,4 +216,13 @@ class LineSquareJoinDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineSquareJoinDownView {
+            val view : LineSquareJoinDownView = LineSquareJoinDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
