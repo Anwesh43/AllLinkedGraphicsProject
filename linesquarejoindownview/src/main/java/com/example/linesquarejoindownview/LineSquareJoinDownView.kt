a71ab0356cdcd0d96e587524abc27e9675a01128
Anwesh43 <anweshthecool0@gmail.com>
2021-12-04 01:30:39 +0530

    adding libraries and constants : linesquarejoindownview setup

---
+++ linesquarejoindownview/src/main/java/com/example/linesquarejoindownview/LineSquareJoinDownView.kt
@@ -0,0 +1,27 @@
+package com.example.linesquarejoindownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#2962FF",
+    "#BF360C",
+    "#00C853",
+    "#C51162",
+    "#FFD600"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
+val lineSizeFactor : Float = 3.2f
