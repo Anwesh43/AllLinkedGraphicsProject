f426ae274451f3992d0d4dd8ba822b88f603a58c
Anwesh43 <anweshthecool0@gmail.com>
2021-12-04 01:38:38 +0530

    linesquarejoindownview : adding Canvas extension methods

--- linesquarejoindownview/src/main/java/com/example/linesquarejoindownview/LineSquareJoinDownView.kt
+++ linesquarejoindownview/src/main/java/com/example/linesquarejoindownview/LineSquareJoinDownView.kt
@@ -29,3 +29,37 @@ val lineSizeFactor : Float = 3.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineSquareJoinDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val lineSize : Float = Math.min(w, h) / lineSizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2  + (h / 2 + size) * sc4)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        translate(-lineSize / 2, -lineSize / 4 + (lineSize / 2) * j)
+        drawLine(0f, 0f, lineSize * sc2, 0f, paint)
+        restore()
+    }
+    for (j in 0..3) {
+        save()
+        translate(-size / 2, -size / 2)
+        drawLine(0f, 0f, size * sc1.divideScale(j, parts), 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLSJDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineSquareJoinDown(scale, w, h, paint)
+}
