578e9b66cf4fbaf9c7a690f72df3a2e268decc0d
Anwesh43 <anweshthecool0@gmail.com>
2021-12-05 03:58:37 +0530

    trilinejoindownview : adding Canvas extension methods

--- trilinedownjoinview/src/main/java/com/example/trilinedownjoinview/TriLineJoinDownView.kt
+++ trilinedownjoinview/src/main/java/com/example/trilinedownjoinview/TriLineJoinDownView.kt
@@ -28,3 +28,31 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawTriLineJoinDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawLine(-size, size / 2, -size + size * sc1, size / 2 - size * sc1, paint)
+        restore()
+    }
+    drawLine(-size * 0.5f * sc2, 0f, size * 0.5f * sc2, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawTLJDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTriLineJoinDown(scale, w, h, paint)
+}
