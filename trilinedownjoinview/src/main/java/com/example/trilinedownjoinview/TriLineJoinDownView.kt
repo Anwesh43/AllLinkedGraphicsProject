0dc6f1eed46b99266a81b0f56065323aaf56fa14
Anwesh43 <anweshthecool0@gmail.com>
2021-12-05 04:04:09 +0530

    adding TriLineJoinDownView class

--- trilinedownjoinview/src/main/java/com/example/trilinedownjoinview/TriLineJoinDownView.kt
+++ trilinedownjoinview/src/main/java/com/example/trilinedownjoinview/TriLineJoinDownView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawTLJDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTriLineJoinDown(scale, w, h, paint)
 }
+
+class TriLineJoinDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
