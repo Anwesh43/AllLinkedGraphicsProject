84a1a30eb7efee3ec62513aeb570aaf2746f0438
Anwesh43 <anweshthecool0@gmail.com>
2021-12-05 04:22:37 +0530

    adding TLJDNode class

--- trilinedownjoinview/src/main/java/com/example/trilinedownjoinview/TriLineJoinDownView.kt
+++ trilinedownjoinview/src/main/java/com/example/trilinedownjoinview/TriLineJoinDownView.kt
@@ -120,4 +120,45 @@ class TriLineJoinDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TLJDNode(var i : Int, val state : State = State()) {
+
+        private var prev : TLJDNode? = null
+        private var next : TLJDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TLJDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTLJDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TLJDNode {
+            var curr : TLJDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
