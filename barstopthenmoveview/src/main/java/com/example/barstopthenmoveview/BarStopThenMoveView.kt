86fa480b26f01149347fa05c6badd088f7a97cba
Anwesh43 <anweshthecool0@gmail.com>
2021-12-06 01:34:07 +0530

    barstopthenmoveview: adding Canvas extension methods

--- barstopthenmoveview/src/main/java/com/example/barstopthenmoveview/BarStopThenMoveView.kt
+++ barstopthenmoveview/src/main/java/com/example/barstopthenmoveview/BarStopThenMoveView.kt
@@ -28,3 +28,29 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBarStopThenMove(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    save()
+    translate(0f, -h / 2 + h * 0.5f * sc2 + (h * 0.5f + size) * sc4)
+    drawRect(RectF(-size / 4, -size, size / 4, 0f), paint)
+    restore()
+    drawLine(-w / 2 + w * sc3, 0f, -w / 2 + w * sc1, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawBSTMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarStopThenMove(scale, w, h, paint)
+}
+
