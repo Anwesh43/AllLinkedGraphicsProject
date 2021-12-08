78c8631acf7bd17db7a8548b47d17022790aeabd
Anwesh43 <anweshthecool0@gmail.com>
2021-12-08 05:49:09 +0530

    rightanglelineenderview: adding RightAngleLineEnderView class

--- rightanglelineenderview/src/main/java/com/example/rightanglelineenderview/RightAngleLineEnderView.kt
+++ rightanglelineenderview/src/main/java/com/example/rightanglelineenderview/RightAngleLineEnderView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawRALENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRightAngleLineEnder(scale, w, h, paint)
 }
+
+class RightAngleLineEnderView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
