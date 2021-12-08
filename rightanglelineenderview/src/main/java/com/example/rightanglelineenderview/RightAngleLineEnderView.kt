927e4adb8929e0b7884f010d0f0e074704ef4f4b
Anwesh43 <anweshthecool0@gmail.com>
2021-12-08 05:46:45 +0530

    rightanglelineenderview: adding Canvas extension methods

--- rightanglelineenderview/src/main/java/com/example/rightanglelineenderview/RightAngleLineEnderView.kt
+++ rightanglelineenderview/src/main/java/com/example/rightanglelineenderview/RightAngleLineEnderView.kt
@@ -30,3 +30,33 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawRightAngleLineEnder(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val lSize : Float = size / lineFactor
+    val ulSize : Float = lSize * 0.5f * sc2
+    save()
+    translate(w / 2, h / 2)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        rotate(rot * j)
+        drawLine(0f, 0f, size * sc1, 0f, paint)
+        drawLine(size, -ulSize, size, ulSize, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawRALENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightAngleLineEnder(scale, w, h, paint)
+}
