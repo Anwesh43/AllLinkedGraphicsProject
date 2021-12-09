dc6daf00e75b18425074b4fde9c5bc2b1aaf8401
Anwesh43 <anweshthecool0@gmail.com>
2021-12-09 12:59:13 +0530

    adding BBRFENode class

--- biboxrotfromeithersideview/src/main/java/com/example/biboxrotfromeithersideview/BiBoxRotFromEitherSideView.kt
+++ biboxrotfromeithersideview/src/main/java/com/example/biboxrotfromeithersideview/BiBoxRotFromEitherSideView.kt
@@ -117,4 +117,45 @@ class BiBoxRotFromEitherSideView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BBRFENode(var i : Int, val state : State = State()) {
+
+        private var next : BBRFENode? = null
+        private var prev : BBRFENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BBRFENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBBRFENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BBRFENode {
+            var curr : BBRFENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
