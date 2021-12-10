e0d7a0b7e075cba77e22cf28ff13e15c972a8221
Anwesh43 <anweshthecool0@gmail.com>
2021-12-10 09:46:22 +0530

    joinrecthalfarcrotview : adding Canvas extension methods

--- joinrecthalfarcrotview/src/main/java/com/example/joinrecthalfarcrotview/JoinRectHalfArcView.kt
+++ joinrecthalfarcrotview/src/main/java/com/example/joinrecthalfarcrotview/JoinRectHalfArcView.kt
@@ -30,3 +30,35 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawJoinRectHalfArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val sc6 : Float = scale.divideScale(5, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2 + (w /2 + size / 2) * sc6, h / 2)
+    rotate(rot * sc5)
+    for (j in 0..2) {
+        val scj : Float = scale.divideScale(j, parts)
+        val start : Float = (-size / 2) * (j % 2)
+        val end : Float = start + (size / 2 + size * 0.5f * (j % 2)) * scj
+        save()
+        rotate(rot * j)
+        drawLine(size / 2, start, size / 2, end, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawJRHANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawJoinRectHalfArc(scale, w, h, paint)
+}
