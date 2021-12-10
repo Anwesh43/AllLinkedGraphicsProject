6e053170f971a0f3781afa874f4f7c50c74e74d5
Anwesh43 <anweshthecool0@gmail.com>
2021-12-10 09:08:50 +0530

    adding joinrothalfarcview setup

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.biboxrotfromeithersideview.BiBoxRotFromEitherSideView
+//import com.example.biboxrotfromeithersideview.BiBoxRotFromEitherSideView
 
 //import com.example.rightanglelineenderview.RightAngleLineEnderView
 
@@ -329,7 +329,7 @@ class MainActivity : AppCompatActivity() {
         //TriLineJoinDownView.create(this)
         //BarStopThenMoveView.create(this)
         //RightAngleLineEnderView.create(this)
-        BiBoxRotFromEitherSideView.create(this)
+        //BiBoxRotFromEitherSideView.create(this)
         fullScreen()
     }
 }
