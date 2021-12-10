0450bfd400a0489309db14a4d1769080d241f028
Anwesh43 <anweshthecool0@gmail.com>
2021-12-10 10:29:36 +0530

    joinrecthalfarcview: adding renderer in view

--- joinrecthalfarcrotview/src/main/java/com/example/joinrecthalfarcrotview/JoinRectHalfArcView.kt
+++ joinrecthalfarcrotview/src/main/java/com/example/joinrecthalfarcrotview/JoinRectHalfArcView.kt
@@ -73,14 +73,15 @@ fun Canvas.drawJRHANode(i : Int, scale : Float, paint : Paint) {
 
 class JoinRectHalfArcView(ctx : Context) : View(ctx) {
 
+    val renderer : Renderer = Renderer(this)
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
