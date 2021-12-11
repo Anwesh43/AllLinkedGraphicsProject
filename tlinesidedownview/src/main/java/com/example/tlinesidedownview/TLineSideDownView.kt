7a0edaa3d296431db7cc9d7c3c35f89b8035047b
Anwesh43 <anweshthecool0@gmail.com>
2021-12-11 10:29:50 +0530

    adding TLineSideDownView class

--- tlinesidedownview/src/main/java/com/example/tlinesidedownview/TLineSideDownView.kt
+++ tlinesidedownview/src/main/java/com/example/tlinesidedownview/TLineSideDownView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawTLSDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTLineSideDown(scale, w, h, paint)
 }
+
+class TLineSideDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
