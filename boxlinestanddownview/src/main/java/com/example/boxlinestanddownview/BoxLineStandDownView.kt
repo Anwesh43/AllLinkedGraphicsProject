826ef9e0a16f35d23848b3c59a8697c51ad3048c
Anwesh43 <anweshthecool0@gmail.com>
2021-12-13 01:51:30 +0530

    boxlinestanddownview: adding BoxLineStandDown class

--- boxlinestanddownview/src/main/java/com/example/boxlinestanddownview/BoxLineStandDownView.kt
+++ boxlinestanddownview/src/main/java/com/example/boxlinestanddownview/BoxLineStandDownView.kt
@@ -161,4 +161,27 @@ class BoxLineStandDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BoxLineStandDown(var i : Int) {
+
+        private var curr : BLSDNode = BLSDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUdpating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
