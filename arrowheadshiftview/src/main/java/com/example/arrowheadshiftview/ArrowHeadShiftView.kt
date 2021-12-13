eff12b1965fa6b3a9876f1b685a944d6ed0efe20
Anwesh43 <anweshthecool0@gmail.com>
2021-12-14 01:04:05 +0530

    adding arrowheadshiftview: adding Renderer class

--- arrowheadshiftview/src/main/java/com/example/arrowheadshiftview/ArrowHeadShiftView.kt
+++ arrowheadshiftview/src/main/java/com/example/arrowheadshiftview/ArrowHeadShiftView.kt
@@ -185,4 +185,27 @@ class ArrowHeadShiftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArrowHeadShiftView) {
+
+        private val animator : Animator = Animator(view)
+        private val ahs : ArrowHeadShift = ArrowHeadShift(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            ahs.draw(canvas, paint)
+            animator.animate {
+                ahs.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            ahs.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
