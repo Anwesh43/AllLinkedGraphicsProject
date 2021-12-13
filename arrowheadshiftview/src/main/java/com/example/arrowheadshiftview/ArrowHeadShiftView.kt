51ffa7ec65ab2fcba7f554da4b60e72f805f0bbb
Anwesh43 <anweshthecool0@gmail.com>
2021-12-14 00:59:15 +0530

    arrowheadshiftview: adding AHSNode class

--- arrowheadshiftview/src/main/java/com/example/arrowheadshiftview/ArrowHeadShiftView.kt
+++ arrowheadshiftview/src/main/java/com/example/arrowheadshiftview/ArrowHeadShiftView.kt
@@ -121,4 +121,45 @@ class ArrowHeadShiftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AHSNode(var i : Int, val state : State = State()) {
+
+        private var next : AHSNode? = null
+        private var prev : AHSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AHSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAHSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AHSNode {
+            var curr : AHSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
