63962f244e60af70df39f687e9ca9cc14fbd5bf5
Anwesh43 <anweshthecool0@gmail.com>
2021-12-14 00:38:40 +0530

    arrowheadshiftview: adding Canvas extension methods

--- arrowheadshiftview/src/main/java/com/example/arrowheadshiftview/ArrowHeadShiftView.kt
+++ arrowheadshiftview/src/main/java/com/example/arrowheadshiftview/ArrowHeadShiftView.kt
@@ -18,14 +18,43 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 3
+val parts : Int = 4
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val rot : Float = 45f
+val deg : Float = 180f
 val backColor : Int = Color.parseColor("#BDBDBD")
+val scGap : Float = 0.03f / parts
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawArrowHeadShift(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        rotate(-45f + rot * 2 * j * sc2)
+        drawLine(0f, 0f, size * sc1, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawAHSNode(i  : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArrowHeadShift(scale, w, h, paint)
+}
