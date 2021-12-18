a49707ab34b1170811cb3c4059e2ba339b79645e
Anwesh43 <anweshthecool0@gmail.com>
2021-12-18 14:26:07 +0530

    squareballcatcherview : adding SquareBallCatcherView class

--- squareballcatcherview/src/main/java/com/example/squareballcatcherview/SquareBallCatcherView.kt
+++ squareballcatcherview/src/main/java/com/example/squareballcatcherview/SquareBallCatcherView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawSBCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSquareBallCatcher(scale, w, h, paint)
 }
+
+class SquareBallCatcherView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
