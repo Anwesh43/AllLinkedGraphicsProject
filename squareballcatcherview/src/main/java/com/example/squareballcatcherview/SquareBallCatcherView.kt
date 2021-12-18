8369f887e2e130fabfe9f015d3cf2862e03ad25a
Anwesh43 <anweshthecool0@gmail.com>
2021-12-18 14:48:54 +0530

    squareballcatcherview: adding SBCNode class

--- squareballcatcherview/src/main/java/com/example/squareballcatcherview/SquareBallCatcherView.kt
+++ squareballcatcherview/src/main/java/com/example/squareballcatcherview/SquareBallCatcherView.kt
@@ -121,4 +121,45 @@ class SquareBallCatcherView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBCNode(var i : Int, val state : State = State()) {
+
+        private var next : SBCNode? = null
+        private var prev : SBCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SBCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBCNode {
+            var curr : SBCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
