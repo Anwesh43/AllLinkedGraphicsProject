5357474185f46a132058b4544b8e9c4cc2e28778
Anwesh43 <anweshthecool0@gmail.com>
2021-12-20 01:21:23 +0530

    linesplitbarview : adding Canvas extension methods

--- linesplitbarview/src/main/java/com/example/linesplitbarview/LineSplitBarView.kt
+++ linesplitbarview/src/main/java/com/example/linesplitbarview/LineSplitBarView.kt
@@ -28,3 +28,34 @@ val colors : Array<Int> = arrayOf(
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineSplitBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-size * 0.5f * sc3, 0f)
+        drawLine(0f, size - size * sc1, 0f, size, paint)
+        restore()
+    }
+    save()
+    translate(-size / 2 - w / 2 + (w / 2 + size / 2) * sc2, 0f)
+    drawRect(RectF(-size / 2, -size, size / 2, 0f), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawLSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineSplitBar(scale, w, h, paint)
+}
