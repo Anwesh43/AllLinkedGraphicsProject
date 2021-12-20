807f7421bcb670969d89f3217478e9b251352676
Anwesh43 <anweshthecool0@gmail.com>
2021-12-21 02:51:40 +0530

    wheelarcbarview: adding WheelArcBarView class

--- wheelarcbarview/src/main/java/com/example/wheelarcbarview/WheelArcBarView.kt
+++ wheelarcbarview/src/main/java/com/example/wheelarcbarview/WheelArcBarView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawWABNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawWheelArcBar(scale, w, h, paint)
 }
+
+class WheelArcBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
