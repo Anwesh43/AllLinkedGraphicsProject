5107eac18715e0324d3eb3f85454ea20b950534b
Anwesh43 <anweshthecool0@gmail.com>
2021-12-22 04:09:02 +0530

    altsquarefillupview: adding Canvas extension methods

--- altsquarefillupview/src/main/java/com/example/altsquarefillupview/AltSquareFillUpView.kt
+++ altsquarefillupview/src/main/java/com/example/altsquarefillupview/AltSquareFillUpView.kt
@@ -28,3 +28,32 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawAltSquareFillUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size / 2) * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        drawLine(-size / 2, size / 2, -size / 2, size / 2 - size * sc1, paint)
+        drawLine(-size / 2, -size / 2, -size/ 2 + size * sc2, -size / 2, paint)
+        restore()
+    }
+    drawRect(RectF(-size / 2, size / 2, size / 2, size / 2 - size * sc3), paint)
+    restore()
+}
+
+fun Canvas.drawASFUNode(i  : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawAltSquareFillUp(scale, w, h, paint)
+}
+
