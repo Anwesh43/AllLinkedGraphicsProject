65166112c3d2ac15efa3634ffd04519644f60a99
Anwesh43 <anweshthecool0@gmail.com>
2021-12-24 01:18:11 +0530

    arclinedirecview: adding Canvas extension methods

--- arclinedirecview/src/main/java/com/example/arclinedirecview/ArcLineDirecView.kt
+++ arclinedirecview/src/main/java/com/example/arclinedirecview/ArcLineDirecView.kt
@@ -28,4 +28,39 @@ val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
-fun Float.divideScalae(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawArcLineDirec(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..3) {
+        val y : Float = (h / 2 - size) * (1 - j % 2) + (w / 2 - size) * (j % 2)
+        save()
+        rotate(deg * j)
+        translate(0f, y * sc3)
+        drawArc(
+            RectF(-size / 2, -size / 2, size / 2, size / 2),
+            deg * sc2,
+            deg * (sc1 - sc2),
+            false,
+            paint
+        )
+        drawLine(0f, size, 0f, size + size * sc3, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawALDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawArcLineDirec(scale, w, h, paint)
+}
