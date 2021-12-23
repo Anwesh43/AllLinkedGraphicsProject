24e1b9287355a41e081bf3c6117b15f7bc106097
Anwesh43 <anweshthecool0@gmail.com>
2021-12-24 01:38:28 +0530

    arclinedirecview: adding Renderer class

--- arclinedirecview/src/main/java/com/example/arclinedirecview/ArcLineDirecView.kt
+++ arclinedirecview/src/main/java/com/example/arclinedirecview/ArcLineDirecView.kt
@@ -191,4 +191,27 @@ class ArcLineDirecView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcLineDirecView) {
+
+        private val animator : Animator = Animator(view)
+        private val ald : ArcLineDirec = ArcLineDirec(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            ald.draw(canvas, paint)
+            animator.animate {
+                ald.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            ald.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
