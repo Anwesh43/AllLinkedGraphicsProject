b9a75a954ce265105ec13a8b9e6e3d64465717b5
Anwesh43 <anweshthecool0@gmail.com>
2021-12-27 01:42:52 +0530

    targetarclineview: adding Canvas extension methods

--- targetarclineview/src/main/java/com/example/targetarclineview/TargetArcLineView.kt
+++ targetarclineview/src/main/java/com/example/targetarclineview/TargetArcLineView.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.graphics.Canvas
 import android.view.View
 import android.view.MotionEvent
+import android.graphics.RectF
 
 val colors : Array<Int> = arrayOf(
     "#01579B",
@@ -26,7 +27,43 @@ val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val deg : Float = 90f
 val rot : Float = 180f
+val end : Float = 360f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawTargetArcLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val lSize : Float = Math.min(w, h) / lSizeFactor
+    save()
+    translate(w / 2 + (w / 2 + size) * sc3, h / 2)
+    rotate(rot * sc3)
+    drawArc(
+        RectF(-size / 2, -size / 2, size / 2, size / 2),
+        0f,
+        end * sc1,
+        false,
+        paint
+    )
+    for (j in 0..3) {
+        save()
+        rotate(rot * j)
+        drawLine(size / 2, 0f, size / 2 - lSize * sc2, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawTALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawTargetArcLine(scale, w, h, paint)
+}
