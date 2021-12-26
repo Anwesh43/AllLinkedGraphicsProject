fddb0f216246f121492fa6ded6ce0b3437d7d726
Anwesh43 <anweshthecool0@gmail.com>
2021-12-27 01:34:55 +0530

    targetarclineview: adding constants and libraries

---
+++ targetarclineview/src/main/java/com/example/targetarclineview/TargetArcLineView.kt
@@ -0,0 +1,28 @@
+package com.example.targetarclineview
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+
+val colors : Array<Int> = arrayOf(
+    "#01579B",
+    "#D50000",
+    "#DD2C00",
+    "#2962FF",
+    "#00838F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.03f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val lSizeFactor : Float = 11.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
+val rot : Float = 180f
