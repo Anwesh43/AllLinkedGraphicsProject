c813271bb202a1b3246ecc01d3606df85497bf18
Anwesh43 <anweshthecool0@gmail.com>
2021-12-27 01:53:26 +0530

    targetarclineview: adding Renderer class

--- targetarclineview/src/main/java/com/example/targetarclineview/TargetArcLineView.kt
+++ targetarclineview/src/main/java/com/example/targetarclineview/TargetArcLineView.kt
@@ -194,4 +194,27 @@ class TargetArcLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : TargetArcLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val tal : TargetArcLine = TargetArcLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            tal.draw(canvas, paint)
+            animator.animate {
+                tal.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            tal.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
