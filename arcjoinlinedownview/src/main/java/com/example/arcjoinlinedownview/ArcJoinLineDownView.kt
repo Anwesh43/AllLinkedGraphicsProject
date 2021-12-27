1da14e758e943a5dcc88f79440bb378e589e90e5
Anwesh43 <anweshthecool0@gmail.com>
2021-12-28 01:49:54 +0530

    arcjoinlinedownview: adding Canvas exension methods

--- arcjoinlinedownview/src/main/java/com/example/arcjoinlinedownview/ArcJoinLineDownView.kt
+++ arcjoinlinedownview/src/main/java/com/example/arcjoinlinedownview/ArcJoinLineDownView.kt
@@ -29,3 +29,33 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawArcJoinLineDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2  + (h / 2 + size) * sc4)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-w / 2 + (w / 2 - size / 2) * sc1, -size / 2)
+        drawLine(0f, 0f, 0f, size, paint)
+        restore()
+    }
+    drawArc(RectF(-size / 2, -size / 2 -size / 2, size / 2, 0f), deg, deg * sc2, false, paint)
+    restore()
+}
+
+fun Canvas.drawAJLDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style  = Paint.Style.STROKE
+    drawArcJoinLineDown(scale, w, h, paint)
+}
