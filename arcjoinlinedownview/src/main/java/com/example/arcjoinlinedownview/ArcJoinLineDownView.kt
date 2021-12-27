226bbfa3162dc5943778e52a596f6ddd22544b46
Anwesh43 <anweshthecool0@gmail.com>
2021-12-28 01:34:08 +0530

    arcjoinlinedownview: adding constants nad libraries

---
+++ arcjoinlinedownview/src/main/java/com/example/arcjoinlinedownview/ArcJoinLineDownView.kt
@@ -0,0 +1,28 @@
+package com.example.arcjoinlinedownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#B71C1C",
+    "#64DD17",
+    "#2962FF",
+    "#C51162",
+    "#DD2C00"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val parts : Int = 4
+val scGap : Float = 0.03f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val deg : Float = 180f
+
