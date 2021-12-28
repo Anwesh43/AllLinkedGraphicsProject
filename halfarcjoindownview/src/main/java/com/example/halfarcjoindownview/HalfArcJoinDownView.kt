7c3da556f519b4ce64e4772c57a986206a1fc790
Anwesh43 <anweshthecool0@gmail.com>
2021-12-29 00:19:22 +0530

    halfarcjoindownview: adding Canvas extension methods

--- halfarcjoindownview/src/main/java/com/example/halfarcjoindownview/HalfArcJoinDownView.kt
+++ halfarcjoindownview/src/main/java/com/example/halfarcjoindownview/HalfArcJoinDownView.kt
@@ -29,3 +29,30 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawHalfArcJoinDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + h * 0.5f * sc4)
+    rotate(deg * sc3)
+    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), deg, deg * sc2, false, paint)
+    save()
+    translate(-size / 2, h * 0.5f * (1 - sc2))
+    drawLine(0f, 0f, size, 0f, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawHAJDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.style = Paint.Style.STROKE
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawHalfArcJoinDown(scale, w, h, paint)
+}
