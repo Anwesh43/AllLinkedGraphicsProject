acbd58ceba2b6fdb53d7f70b9e0b808cc87dcee4
Anwesh43 <anweshthecool0@gmail.com>
2021-12-30 02:17:06 +0530

    arcsweepfromsideview: adding Canvas extension methods

--- arcsweepfromsideview/src/main/java/com/example/arcsweepfromsideview/ArcSweepFromSideView.kt
+++ arcsweepfromsideview/src/main/java/com/example/arcsweepfromsideview/ArcSweepFromSideView.kt
@@ -29,3 +29,27 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawArcSweepFromSide(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -deg, deg * sc1, true, paint)
+        restore()
+    }
+    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), deg, rot * sc2, true, paint)
+    restore()
+}
+
+fun Canvas.drawASFSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawArcSweepFromSide(scale, w, h, paint)
+}
