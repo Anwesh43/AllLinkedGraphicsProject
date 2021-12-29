4fdc98b7e9bd9b9da676c74cb05c571e28e4e1c9
Anwesh43 <anweshthecool0@gmail.com>
2021-12-30 02:11:56 +0530

    arcsweepfromsideview: adding constant and libraries

---
+++ arcsweepfromsideview/src/main/java/com/example/arcsweepfromsideview/ArcSweepFromSideView.kt
@@ -0,0 +1,27 @@
+package com.example.arcsweepfromsideview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#B71C1C",
+    "#64DD17",
+    "#2962FF",
+    "#C51162",
+    "#DD2C00"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.03f / parts
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val deg : Float = 90f
+val rot : Float = 180f
+val backColor : Int = Color.parseColor("#BDBDBD")
