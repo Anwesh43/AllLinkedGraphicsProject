ab22eb8e31cf7b20f84f7ef8ff0da6ca77c79b2b
Anwesh43 <anweshthecool0@gmail.com>
2021-12-30 02:23:04 +0530

    adding ArcSweepFromSideView class

--- arcsweepfromsideview/src/main/java/com/example/arcsweepfromsideview/ArcSweepFromSideView.kt
+++ arcsweepfromsideview/src/main/java/com/example/arcsweepfromsideview/ArcSweepFromSideView.kt
@@ -53,3 +53,19 @@ fun Canvas.drawASFSNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawArcSweepFromSide(scale, w, h, paint)
 }
+
+class ArcSweepFromSideView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
