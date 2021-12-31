0230e1f983f5663799413123d13615dd03d6e380
Anwesh43 <anweshthecool0@gmail.com>
2022-01-01 01:16:12 +0530

    hundredonerotview: adding Renderer class

--- hundredonerotview/src/main/java/com/example/hundredonerotview/HundredOneRotView.kt
+++ hundredonerotview/src/main/java/com/example/hundredonerotview/HundredOneRotView.kt
@@ -185,4 +185,27 @@ class HundredOneRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : HundredOneRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val hor : HundredOneRot = HundredOneRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            hor.draw(canvas, paint)
+            animator.animate {
+                hor.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            hor.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
