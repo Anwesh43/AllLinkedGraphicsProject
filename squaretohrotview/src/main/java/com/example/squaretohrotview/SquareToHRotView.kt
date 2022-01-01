ee396ce279e18010aa0db8a00f21139c5b4a56c8
Anwesh43 <anweshthecool0@gmail.com>
2022-01-02 01:27:35 +0530

    squaretohrotview: adding SquareToHrOTvIEW CLASS

--- squaretohrotview/src/main/java/com/example/squaretohrotview/SquareToHRotView.kt
+++ squaretohrotview/src/main/java/com/example/squaretohrotview/SquareToHRotView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawSTHRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSquareToHRot(scale, w, h, paint)
 }
+
+class SquareToHRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
