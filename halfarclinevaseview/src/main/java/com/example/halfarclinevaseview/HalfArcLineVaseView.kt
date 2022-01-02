ce4d78f5274225f8d1853539d8d312f8e8572ba0
Anwesh43 <anweshthecool0@gmail.com>
2022-01-03 01:58:50 +0530

    halfarclinevaseview: adding Canvas extension methods

--- halfarclinevaseview/src/main/java/com/example/halfarclinevaseview/HalfArcLineVaseView.kt
+++ halfarclinevaseview/src/main/java/com/example/halfarclinevaseview/HalfArcLineVaseView.kt
@@ -22,6 +22,7 @@ val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val deg : Float = 90f
+val rot : Float = 180f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
@@ -29,3 +30,43 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawHalfArcLineVase(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+    rotate(deg * sc3)
+    drawArc(
+        RectF(-size / 2, -size / 2, size / 2, size / 2),
+        0f,
+        rot * sc1,
+        false,
+        paint
+    )
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawLine(
+            size / 2,
+            0f,
+            size / 2 + size * 0.5f * sc2,
+            -size * 0.5f * sc2,
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawHALVNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawHalfArcLineVase(scale, w, h, paint)
+}
