8b254716e7668952b2b4320b086bd7eefa046a16
Anwesh43 <anweshthecool0@gmail.com>
2022-01-04 00:39:22 +0530

    umbrellaarclineview : adding UmbrellaArcLineView

--- umbrellarclineview/src/main/java/com/example/umbrellarclineview/UmbrellaArcLineView.kt
+++ umbrellarclineview/src/main/java/com/example/umbrellarclineview/UmbrellaArcLineView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawUALNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawUmbrellaArcLine(scale, w, h, paint)
 }
+
+class UmbreallaArcLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
