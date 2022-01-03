bb730658f86a4ae86a2fb056fc20ecbcab6ad9ad
Anwesh43 <anweshthecool0@gmail.com>
2022-01-04 00:50:58 +0530

    umbrellaarclineview: adding UALNode class

--- umbrellarclineview/src/main/java/com/example/umbrellarclineview/UmbrellaArcLineView.kt
+++ umbrellarclineview/src/main/java/com/example/umbrellarclineview/UmbrellaArcLineView.kt
@@ -121,4 +121,45 @@ class UmbreallaArcLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class UALNode(var i : Int, val state : State = State()) {
+
+        private var next : UALNode? = null
+        private var prev : UALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = UALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawUALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : UALNode {
+            var curr : UALNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
