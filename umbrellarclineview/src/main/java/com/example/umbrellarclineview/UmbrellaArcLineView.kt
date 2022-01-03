d87c077c01a6a98f506e5fd48ebd8c909362b364
Anwesh43 <anweshthecool0@gmail.com>
2022-01-04 00:28:24 +0530

    umbrellaarclineview: adding Canvas extension methods

--- umbrellarclineview/src/main/java/com/example/umbrellarclineview/UmbrellaArcLineView.kt
+++ umbrellarclineview/src/main/java/com/example/umbrellarclineview/UmbrellaArcLineView.kt
@@ -29,3 +29,33 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawUmbrellaArcLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size / 2) * sc4)
+    rotate(deg * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(0f, (h / 2 + size) * (1 - sc1))
+        rotate(rot * sc2)
+        drawArc(RectF(-size / 2, -size, size / 2, 0f), -90f, 180f, true, paint)
+        restore()
+    }
+    drawLine(0f, 0f, 0f, -size * sc3, paint)
+    restore()
+}
+
+fun Canvas.drawUALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawUmbrellaArcLine(scale, w, h, paint)
+}
