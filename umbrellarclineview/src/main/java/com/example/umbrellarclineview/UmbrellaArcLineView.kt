5943dc0f707233d3f091180690cfe39977d6739f
Anwesh43 <anweshthecool0@gmail.com>
2022-01-04 00:17:03 +0530

    umbrellaarclineview: adding constant and libraries

---
+++ umbrellarclineview/src/main/java/com/example/umbrellarclineview/UmbrellaArcLineView.kt
@@ -0,0 +1,28 @@
+package com.example.umbrellarclineview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#B71C1C",
+    "#64DD17",
+    "#2962FF",
+    "#C51162",
+    "#DD2C00"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val rot : Float = 90f
+val deg : Float = 180f
+
