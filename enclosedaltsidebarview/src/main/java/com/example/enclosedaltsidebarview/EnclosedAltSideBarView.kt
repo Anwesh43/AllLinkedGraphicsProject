2b35ca43584c796c1b3abcba4a267553e1a96e19
Anwesh43 <anweshthecool0@gmail.com>
2022-01-05 00:27:12 +0530

    enclosedaltsidebarview: adding Canvas extension methods

--- enclosedaltsidebarview/src/main/java/com/example/enclosedaltsidebarview/EnclosedAltSideBarView.kt
+++ enclosedaltsidebarview/src/main/java/com/example/enclosedaltsidebarview/EnclosedAltSideBarView.kt
@@ -19,7 +19,8 @@ val colors : Array<Int> = arrayOf(
     Color.parseColor(it)
 }.toTypedArray()
 val delay : Long = 20
-val sizeFactor : Float = 5.9f
+val sizeFactor : Float = 3.2f
+val barSizeFactor : Float = 11.2f
 val strokeFactor : Float = 90f
 val parts : Int = 6
 val scGap : Float = 0.04f / parts
@@ -29,3 +30,43 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawEnclosedAltSideBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val sc6 : Float = scale.divideScale(5, parts)
+    val barSize : Float = Math.min(w, h) / barSizeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        save()
+        save()
+        translate(-size * sc3, 0f)
+        drawLine(0f, 0f, 0f, -barSize * 0.25f * sc1 - (size - barSize * 0.25f) * sc4, paint)
+        restore()
+        save()
+        translate(-(size - barSize * 0.5f) * sc3, -(size - barSize * 0.25f) * sc4)
+        drawRect(RectF(-barSize * 0.5f * sc1, -barSize / 4, 0f, 0f), paint)
+        restore()
+        save()
+        drawLine(0f, 0f, -barSize * 0.5f * sc2 - (size - barSize * 0.5f) * sc3, 0f, paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawEASBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawEnclosedAltSideBar(scale, w, h, paint)
+}
