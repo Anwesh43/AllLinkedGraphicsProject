f7ca4e1daffa14e7f237006633eab392bf461034
Anwesh43 <anweshthecool0@gmail.com>
2022-01-06 00:27:04 +0530

    dividebarupview: adding constants and libraries

---
+++ dividebarupview/src/main/java/com/example/dividebarupview/DivideBarUpView.kt
@@ -0,0 +1,29 @@
+package com.example.dividebarupview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#01579B",
+    "#D50000",
+    "#DD2C00",
+    "#2962FF",
+    "#00838F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val bars : Int = 3
+val parts : Int = bars + 2
+val sizeFactor : Float = 3.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
+val scGap : Float = 0.04f / parts
+
+
