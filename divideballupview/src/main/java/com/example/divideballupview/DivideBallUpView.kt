e960f0e47637a63427b870b5e7e98a121b73eabb
Anwesh43 <anweshthecool0@gmail.com>
2022-01-07 01:21:13 +0530

    divideballupview: adding DivideBallUpView class

--- divideballupview/src/main/java/com/example/divideballupview/DivideBallUpView.kt
+++ divideballupview/src/main/java/com/example/divideballupview/DivideBallUpView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawDBUNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawDivideBallUp(scale, w, h, paint)
 }
+
+class DivideBallUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
