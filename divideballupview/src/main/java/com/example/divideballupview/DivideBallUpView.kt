381012d5d10bb46ced0bb6262d0acd36ccbbee3e
Anwesh43 <anweshthecool0@gmail.com>
2022-01-07 01:42:02 +0530

    divideballupview: adding static create method

--- divideballupview/src/main/java/com/example/divideballupview/DivideBallUpView.kt
+++ divideballupview/src/main/java/com/example/divideballupview/DivideBallUpView.kt
@@ -216,4 +216,13 @@ class DivideBallUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : DivideBallUpView {
+            val view : DivideBallUpView = DivideBallUpView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
