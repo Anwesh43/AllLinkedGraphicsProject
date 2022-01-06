aff91da304f6df0419a0a93847a488a4e3f30719
Anwesh43 <anweshthecool0@gmail.com>
2022-01-07 01:18:58 +0530

    divideballupview: adding Canvas exension methods

--- divideballupview/src/main/java/com/example/divideballupview/DivideBallUpView.kt
+++ divideballupview/src/main/java/com/example/divideballupview/DivideBallUpView.kt
@@ -25,7 +25,42 @@ val ballDivideFactor : Float = 2.3f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val deg : Float = 180f
+val scGap : Float = 0.04f / parts
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawDivideBallUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(3, parts)
+    val sc2 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc2)
+    rotate(deg * sc1)
+    var y : Float = 0f
+    var k : Float = 1f
+    for (j in 0..(balls - 1)) {
+        val r : Float = size / (2 * k)
+        save()
+        translate(0f, y)
+        drawArc(
+            RectF(-r, -r, r, r),
+            0f,
+            deg * 2 * scale.divideScale(j, parts),
+            true,
+            paint
+        )
+        restore()
+        k *= ballDivideFactor
+        y -= (r + r / ballDivideFactor)
+    }
+    restore()
+}
+
+fun Canvas.drawDBUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawDivideBallUp(scale, w, h, paint)
+}
