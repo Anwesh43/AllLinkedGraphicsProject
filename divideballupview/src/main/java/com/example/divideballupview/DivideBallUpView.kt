ce7e477b2df415c5d654859817afbe60b5241d84
Anwesh43 <anweshthecool0@gmail.com>
2022-01-07 01:32:27 +0530

    divideballupview: adding DBUNode class

--- divideballupview/src/main/java/com/example/divideballupview/DivideBallUpView.kt
+++ divideballupview/src/main/java/com/example/divideballupview/DivideBallUpView.kt
@@ -127,4 +127,45 @@ class DivideBallUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DBUNode(var i : Int, val state : State = State()) {
+
+        private var next : DBUNode? = null
+        private var prev : DBUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DBUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDBUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DBUNode {
+            var curr : DBUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
