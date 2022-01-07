cbc0ebfbfd286521d65254d261430d905d137891
Anwesh43 <anweshthecool0@gmail.com>
2022-01-08 02:32:22 +0530

    barsideupgradeview: adding BarSideUpgradeView

--- barsideupgradeview/src/main/java/com/example/barsideupgradeview/BarSideUpgradeView.kt
+++ barsideupgradeview/src/main/java/com/example/barsideupgradeview/BarSideUpgradeView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawBSUNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBarSideUpgrade(scale, w, h, paint)
 }
+
+class BarSideUpgradeView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
