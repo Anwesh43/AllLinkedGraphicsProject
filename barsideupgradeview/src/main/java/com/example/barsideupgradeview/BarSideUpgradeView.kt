73c69bb5ce495e39121491791a255db835fc0272
Anwesh43 <anweshthecool0@gmail.com>
2022-01-08 02:30:35 +0530

    barsideupgradeview: adding Canvas extensionmethods

--- barsideupgradeview/src/main/java/com/example/barsideupgradeview/BarSideUpgradeView.kt
+++ barsideupgradeview/src/main/java/com/example/barsideupgradeview/BarSideUpgradeView.kt
@@ -20,7 +20,7 @@ val colors : Array<Int> = arrayOf(
 }.toTypedArray()
 val bars : Int = 3
 val delay : Long = 20
-val parts : Int = 4
+val parts : Int = bars + 2
 val scGap : Float = 0.04f / parts
 val backColor : Int = Color.parseColor("#BDBDBD")
 val sizeFactor : Float = 2.3f
@@ -30,3 +30,40 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBarSideUpgrade(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(bars, parts)
+    val sc2 : Float = scale.divideScale(bars + 1, parts)
+    var x : Float = 0f
+    save()
+    translate(w / 2 + (w / 2) * sc2, h / 2)
+    rotate(rot * sc1)
+    var barSize : Float = size
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        for (k in 0..(bars - 1)) {
+            save()
+            drawRect(
+                RectF(
+                    0f,
+                    -barSize,
+                    barSize * scale.divideScale(j, parts),
+                    0f),
+                paint)
+            restore()
+        }
+        restore()
+        x += barSize
+        barSize /= divideFactor
+    }
+    restore()
+}
+
+fun Canvas.drawBSUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBarSideUpgrade(scale, w, h, paint)
+}
