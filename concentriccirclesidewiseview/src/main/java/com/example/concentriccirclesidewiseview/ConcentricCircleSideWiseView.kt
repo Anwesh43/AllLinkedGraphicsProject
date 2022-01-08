18eec0ce001bd8980e231df38dc9b46d6c7162b5
Anwesh43 <anweshthecool0@gmail.com>
2022-01-09 01:52:10 +0530

    concentriccirclesidewiseview: adding ConcentricCircleSideWise class

--- concentriccirclesidewiseview/src/main/java/com/example/concentriccirclesidewiseview/ConcentricCircleSideWiseView.kt
+++ concentriccirclesidewiseview/src/main/java/com/example/concentriccirclesidewiseview/ConcentricCircleSideWiseView.kt
@@ -181,4 +181,27 @@ class ConcentricCircleSideWiseView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ConcentricCircleSideWise(var i : Int) {
+
+        private var curr : CCSWNode = CCSWNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+             curr.startUpdating(cb)
+        }
+    }
 }
