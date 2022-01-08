26527c3eaac1a10516200d28be7c1b13288bfe00
Anwesh43 <anweshthecool0@gmail.com>
2022-01-09 01:35:16 +0530

    concentriccirclesidewiseview: adding DrawingUtil class

--- concentriccirclesidewiseview/src/main/java/com/example/concentriccirclesidewiseview/ConcentricCircleSideWiseView.kt
+++ concentriccirclesidewiseview/src/main/java/com/example/concentriccirclesidewiseview/ConcentricCircleSideWiseView.kt
@@ -30,3 +30,49 @@ val divideFactor : Float = 1.4f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawConcentricCircleSideWise(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(circles, parts)
+    val sc2 : Float = scale.divideScale(circles + 1, parts)
+    save()
+    translate(w / 2 , h / 2 + (h / 2 + size * circles) * sc2)
+    rotate(deg * sc2 )
+    for (k in 0..1) {
+        save()
+        rotate(deg * (1f - 2 * k) * sc1)
+        for (j in 0..1) {
+            var x : Float = 0f
+            var barSize : Float = size
+            save()
+            scale(1f - 2 * j, 1f)
+            for (i in 0..(circles - 1)) {
+                save()
+                translate(x, 0f)
+                drawArc(
+                    RectF(-barSize / 2, -barSize / 2, barSize / 2, barSize / 2),
+                    0f,
+                    360f * scale.divideScale(i, parts),
+                    false,
+                    paint
+                )
+                restore()
+                x += (barSize + barSize / divideFactor)
+                barSize /= divideFactor
+            }
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawCCSWNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawConcentricCircleSideWise(scale, w, h, paint)
+}
