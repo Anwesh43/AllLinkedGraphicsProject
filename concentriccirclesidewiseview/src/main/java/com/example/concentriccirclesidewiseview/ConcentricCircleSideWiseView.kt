9ae215379ad2f0fa8184727256ee5c3a5b6841df
Anwesh43 <anweshthecool0@gmail.com>
2022-01-09 01:36:33 +0530

    concentriccirclesidewiseview: adding ConcentricCircleSideWiseView class

--- concentriccirclesidewiseview/src/main/java/com/example/concentriccirclesidewiseview/ConcentricCircleSideWiseView.kt
+++ concentriccirclesidewiseview/src/main/java/com/example/concentriccirclesidewiseview/ConcentricCircleSideWiseView.kt
@@ -76,3 +76,19 @@ fun Canvas.drawCCSWNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawConcentricCircleSideWise(scale, w, h, paint)
 }
+
+class ConcentricCircleSideWiseView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
