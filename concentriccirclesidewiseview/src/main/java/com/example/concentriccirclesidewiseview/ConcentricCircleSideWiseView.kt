47d1ce3774a9b1331a5ad73ec771f3a39edef6f2
Anwesh43 <anweshthecool0@gmail.com>
2022-01-09 01:38:34 +0530

    concentriccirclesidewiseview: adding State class

--- concentriccirclesidewiseview/src/main/java/com/example/concentriccirclesidewiseview/ConcentricCircleSideWiseView.kt
+++ concentriccirclesidewiseview/src/main/java/com/example/concentriccirclesidewiseview/ConcentricCircleSideWiseView.kt
@@ -26,6 +26,7 @@ val sizeFactor : Float = 4.9f
 val strokeFactor : Float = 90f
 val bacKColor : Int = Color.parseColor("#BDBDBD")
 val divideFactor : Float = 1.4f
+val scGap : Float = 0.05f / parts
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -90,5 +91,25 @@ class ConcentricCircleSideWiseView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var prevScale : Float = 0f, var dir : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scGap * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
