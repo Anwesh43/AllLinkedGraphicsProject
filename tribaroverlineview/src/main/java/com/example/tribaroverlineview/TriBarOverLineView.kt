21c1fbf97018cecb150c9a56be7f3a1d04f1fe24
Anwesh43 <anweshthecool0@gmail.com>
2022-01-11 00:53:02 +0530

    tribaroverlineview: adding TBOLNode class

--- tribaroverlineview/src/main/java/com/example/tribaroverlineview/TriBarOverLineView.kt
+++ tribaroverlineview/src/main/java/com/example/tribaroverlineview/TriBarOverLineView.kt
@@ -120,4 +120,45 @@ class TriBarOverLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TBOLNode(var i : Int, val state : State = State()) {
+
+        private var prev : TBOLNode? = null
+        private var next : TBOLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TBOLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTBOLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TBOLNode {
+            var curr : TBOLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
