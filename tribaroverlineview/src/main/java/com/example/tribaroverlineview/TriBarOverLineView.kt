e0fa30f0b2c15a1e535c376db27140fd247de988
Anwesh43 <anweshthecool0@gmail.com>
2022-01-11 00:25:16 +0530

    tribaroverlineview: adding Canvas extension methods

--- tribaroverlineview/src/main/java/com/example/tribaroverlineview/TriBarOverLineView.kt
+++ tribaroverlineview/src/main/java/com/example/tribaroverlineview/TriBarOverLineView.kt
@@ -24,8 +24,36 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val deg : Float = 90f
+val deg : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawTriBarOverLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + paint.strokeWidth) * sc4)
+    rotate(deg * sc3)
+    drawLine(-size * 0.5f * sc1, 0f, size * 0.5f * sc1, 0f, paint)
+    for (j in 0..2) {
+        save()
+        translate(-size / 2 + (w / 2) * j)
+        drawRect(RectF(0f, -size * sc1, size / 10, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawTBOLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTriBarOverLine(scale, w, h, paint)
+}
