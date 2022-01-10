0d70a21280c460bc7a0d0f7a5f8f532defd71592
Anwesh43 <anweshthecool0@gmail.com>
2022-01-11 00:14:26 +0530

    tribaroverlineview: adding constants and libraries

---
+++ tribaroverlineview/src/main/java/com/example/tribaroverlineview/TriBarOverLineView.kt
@@ -0,0 +1,27 @@
+package com.example.tribaroverlineview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#C62828",
+    "#00C853",
+    "#0D47A1",
+    "#FFD600",
+    "#9C27B0"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
