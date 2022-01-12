bdefd8851bd2168f7b6cc0a268f2b5744a774e97
Anwesh43 <anweshthecool0@gmail.com>
2022-01-13 00:43:30 +0530

    barhammerdivideview: adding Canvas extension methods

--- barhammerdivideview/src/main/java/com/example/barhammerdivideview/BarHammerDivideView.kt
+++ barhammerdivideview/src/main/java/com/example/barhammerdivideview/BarHammerDivideView.kt
@@ -29,3 +29,40 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBarHammerDivide(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val barSize : Float = Math.min(w, h) / barSizeFactor
+    save()
+    translate(w  / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        rotate(rot * sc3)
+        translate((-w / 2) * sc4, 0f)
+        save()
+        translate(0f, (h / 2 + size) * (1 - sc1))
+        drawLine(0f, 0f, 0f, -size, paint)
+        restore()
+        save()
+        translate(0f, -h / 2 + (h / 2 - size) * sc2)
+        drawRect(RectF(-barSize, -barSize, barSize, 0f), paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBHDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarHammerDivide(scale, w, h, paint)
+}
+
