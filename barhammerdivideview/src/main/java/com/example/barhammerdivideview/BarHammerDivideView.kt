8161b10d1cefa138a4e672e1d320ea953663d5d4
Anwesh43 <anweshthecool0@gmail.com>
2022-01-13 00:57:08 +0530

    barhammerdivideview: adding BHDNode class

--- barhammerdivideview/src/main/java/com/example/barhammerdivideview/BarHammerDivideView.kt
+++ barhammerdivideview/src/main/java/com/example/barhammerdivideview/BarHammerDivideView.kt
@@ -128,4 +128,45 @@ class BarHammerDivideView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BHDNode(var i : Int, val state : State = State()) {
+
+        private var prev : BHDNode? = null
+        private var next : BHDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BHDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBHDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BHDNode {
+            var curr : BHDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr !=  null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
