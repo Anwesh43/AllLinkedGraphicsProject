00dfddd1a07f3d2d88a8d4c1c1ce1a0127e8e30e
Anwesh43 <anweshthecool0@gmail.com>
2022-01-15 01:25:32 +0530

    arrowsignshootview: adding ASSVNode class

--- arrowsignshootview/src/main/java/com/example/arrowsignshootview/ArrowSignShootView.kt
+++ arrowsignshootview/src/main/java/com/example/arrowsignshootview/ArrowSignShootView.kt
@@ -129,4 +129,45 @@ class ArrowSignShootView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ASSVNode(var i : Int, val state : State = State()) {
+
+        private var prev : ASSVNode? = null
+        private var next : ASSVNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ASSVNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawASSVNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ASSVNode {
+            var curr : ASSVNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
