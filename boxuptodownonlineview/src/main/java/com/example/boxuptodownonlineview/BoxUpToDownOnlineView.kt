bff2c73e236f6e07bb15aa96f2b97999aef0db90
Anwesh43 <anweshthecool0@gmail.com>
2022-01-16 01:45:33 +0530

    boxuptodownonlineview: adding Renderer class

--- boxuptodownonlineview/src/main/java/com/example/boxuptodownonlineview/BoxUpToDownOnlineView.kt
+++ boxuptodownonlineview/src/main/java/com/example/boxuptodownonlineview/BoxUpToDownOnlineView.kt
@@ -193,4 +193,27 @@ class BoxUpToDownOnlineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BoxUpToDownOnlineView) {
+
+        private val animator : Animator = Animator(view)
+        private val butdo: BoxUpToDownOnline = BoxUpToDownOnline(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            butdo.draw(canvas, paint)
+            animator.animate {
+                butdo.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            butdo.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
