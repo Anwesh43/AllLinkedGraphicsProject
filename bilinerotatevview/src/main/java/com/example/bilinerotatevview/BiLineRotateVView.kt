22776198be0e693af11a4b3a6032bb4dc6b13ee0
Anwesh43 <anweshthecool0@gmail.com>
2022-01-20 23:01:23 +0530

    bilinerotatevview: adding BiLineRotateVView class

--- bilinerotatevview/src/main/java/com/example/bilinerotatevview/BiLineRotateVView.kt
+++ bilinerotatevview/src/main/java/com/example/bilinerotatevview/BiLineRotateVView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawBLRVNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineRotateV(scale, w, h, paint)
 }
+
+class BiLineRotateVView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
