8914479eccc61f8926b99b9037bd10718a054269
Anwesh43 <anweshthecool0@gmail.com>
2022-01-20 23:14:48 +0530

    bilinerotatevview: adding BLRVNode class

--- bilinerotatevview/src/main/java/com/example/bilinerotatevview/BiLineRotateVView.kt
+++ bilinerotatevview/src/main/java/com/example/bilinerotatevview/BiLineRotateVView.kt
@@ -123,4 +123,45 @@ class BiLineRotateVView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLRVNode(var i : Int, val state : State = State()) {
+
+        private var prev : BLRVNode? = null
+        private var next : BLRVNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLRVNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLRVNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLRVNode {
+            var curr : BLRVNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
