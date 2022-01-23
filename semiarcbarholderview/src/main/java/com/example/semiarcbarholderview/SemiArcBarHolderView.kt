b57ec74de8e6c918348a35e50a444bba752292fb
Anwesh43 <anweshthecool0@gmail.com>
2022-01-24 01:06:28 +0530

    semiarcbarholderview: adding Canvas extension methods

--- semiarcbarholderview/src/main/java/com/example/semiarcbarholderview/SemiArcBarHolderView.kt
+++ semiarcbarholderview/src/main/java/com/example/semiarcbarholderview/SemiArcBarHolderView.kt
@@ -30,3 +30,32 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSemiArcBarHolder(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val barSize : Float = Math.min(w, h) / barSizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + barSize) * sc4)
+    save()
+    translate(0f, -h / 2 + (h / 2) * sc3)
+    paint.style = Paint.Style.FILL
+    drawRect(RectF(-barSize / 2, -barSize, barSize / 2, 0f), paint)
+    restore()
+    paint.style = Paint.Style.STROKE
+    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, deg * sc1, false, paint)
+    drawLine(size / 2, 0f, size / 2 - size * sc2, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawSABHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSemiArcBarHolder(scale, w, h, paint)
+}
