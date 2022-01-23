c52d2afbaea8254bbae53302c9688ee88e0ab896
Anwesh43 <anweshthecool0@gmail.com>
2022-01-24 01:14:39 +0530

    semiarcbarholderview: adding SABHNode class

--- semiarcbarholderview/src/main/java/com/example/semiarcbarholderview/SemiArcBarHolderView.kt
+++ semiarcbarholderview/src/main/java/com/example/semiarcbarholderview/SemiArcBarHolderView.kt
@@ -122,4 +122,45 @@ class SemiArcBarHolderView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SABHNode(var i : Int, val state : State = State()) {
+
+        private var next : SABHNode? = null
+        private var prev : SABHNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SABHNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSABHNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SABHNode {
+            var curr : SABHNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
