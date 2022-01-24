14825bef2975da73b4e0c755812654416ec64b8d
Anwesh43 <anweshthecool0@gmail.com>
2022-01-25 00:40:52 +0530

    halfbarrotdownview: adding renderer in view

--- halfbarrotdownview/src/main/java/com/example/halfbarrotdownview/HalfBarRotDownView.kt
+++ halfbarrotdownview/src/main/java/com/example/halfbarrotdownview/HalfBarRotDownView.kt
@@ -57,14 +57,16 @@ fun Canvas.drawHBRDNode(i : Int, scale : Float, paint : Paint) {
 
 class HalfBarRotDownView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
