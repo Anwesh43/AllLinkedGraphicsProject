2c7ac910ad1fc59199854e9b1b07c21e0128ff67
Anwesh43 <anweshthecool0@gmail.com>
2022-01-25 00:20:57 +0530

    halfbarrotdownview: adding Canvas extension methods

--- halfbarrotdownview/src/main/java/com/example/halfbarrotdownview/HalfBarRotDownView.kt
+++ halfbarrotdownview/src/main/java/com/example/halfbarrotdownview/HalfBarRotDownView.kt
@@ -28,3 +28,29 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawHalfBarRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size / 2) * sc4)
+    save()
+    rotate(deg * sc2)
+    drawRect(RectF(-size / 2, -size * 0.5f * sc1, size / 2, 0f), paint)
+    restore()
+    save()
+    translate(0f, -h / 2 + h * 0.5f * sc3)
+    drawRect(RectF(-size / 2, -size * 0.5f, size / 2, 0f), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawHBRDNode(i : Int, scale : Float, paint : Paint) {
+    paint.color = colors[i]
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    drawHalfBarRotDown(scale, w, h, paint)
+}
