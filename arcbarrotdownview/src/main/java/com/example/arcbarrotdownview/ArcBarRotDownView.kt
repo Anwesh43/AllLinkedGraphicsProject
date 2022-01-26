41c5c45e971c820996fc0b500c66dff65ddf466a
Anwesh43 <anweshthecool0@gmail.com>
2022-01-27 00:51:01 +0530

    arcbarrotdownview: adding Renderer class

--- arcbarrotdownview/src/main/java/com/example/arcbarrotdownview/ArcBarRotDownView.kt
+++ arcbarrotdownview/src/main/java/com/example/arcbarrotdownview/ArcBarRotDownView.kt
@@ -187,4 +187,27 @@ class ArcBarRotDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcBarRotDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val abrd : ArcBarRotDown = ArcBarRotDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            abrd.draw(canvas, paint)
+            animator.animate {
+                abrd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            abrd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
