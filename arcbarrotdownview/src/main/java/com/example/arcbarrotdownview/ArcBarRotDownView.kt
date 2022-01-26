b5b9af8498520877515859d71957b7703f3cf2ea
Anwesh43 <anweshthecool0@gmail.com>
2022-01-27 00:42:26 +0530

    arcbarrotdownview: adding Canvas extension methods

--- arcbarrotdownview/src/main/java/com/example/arcbarrotdownview/ArcBarRotDownView.kt
+++ arcbarrotdownview/src/main/java/com/example/arcbarrotdownview/ArcBarRotDownView.kt
@@ -31,3 +31,33 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawArcBarRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+    rotate(rot * sc3)
+    save()
+    translate(0f, -(size / 2 + h / 2)  * (1 - sc1))
+    paint.style = Paint.Style.FILL
+    drawRect(RectF(-size / 2, -size / 2, size / 2, size / 2), paint)
+    restore()
+    paint.style = Paint.Style.STROKE
+    drawArc(RectF(size / 2, -r, size / 2 + 2 * r, r), 0f, deg * sc2, false, paint)
+    restore()
+}
+
+fun Canvas.drawABRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcBarRotDown(scale, w, h, paint)
+}
+
