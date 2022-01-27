43b57e5b2deae64fb52fadd10a62e4d1db2ecf77
Anwesh43 <anweshthecool0@gmail.com>
2022-01-28 00:37:48 +0530

    squarelineextenderview: adding constats and libraries

---
+++ squarelineextenderview/src/main/java/com/example/squarelineextenderview/SquareExtenderLineView.kt
@@ -0,0 +1,27 @@
+package com.example.squarelineextenderview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#4A148C",
+    "#004D40",
+    "#DD2C00",
+    "#D50000",
+    "#43A047"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val rot : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
