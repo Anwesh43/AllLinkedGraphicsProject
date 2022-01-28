ca69d57b89494b9da919d86c7d88d2741b148ca2
Anwesh43 <anweshthecool0@gmail.com>
2022-01-29 02:47:03 +0530

    peaksquareholderview: adding PeakSquareHolder class

--- peaksquareholderview/src/main/java/com/example/peaksquareholderview/PeakSquareHolderView.kt
+++ peaksquareholderview/src/main/java/com/example/peaksquareholderview/PeakSquareHolderView.kt
@@ -160,5 +160,28 @@ class PeakSquareHolderView(ctx : Context) : View(ctx) {
                 return this
             }
         }
+
+        data class PeakSquareHolder(var i : Int) {
+
+            private var curr : PSHNode = PSHNode(0)
+            private var dir : Int = 1
+
+            fun draw(canvas : Canvas, paint : Paint) {
+                curr.draw(canvas, paint)
+            }
+
+            fun update(cb : (Float) -> Unit) {
+                curr.update {
+                    curr = curr.getNext(dir) {
+                        dir *= -1
+                    }
+                    cb(it)
+                }
+            }
+
+            fun startUpdating(cb : () -> Unit) {
+                curr.startUpdating(cb)
+            }
+        }
     }
 }
