d0641026131d98a7109763bead92d737abb32990
Anwesh43 <anweshthecool0@gmail.com>
2022-01-29 02:51:46 +0530

    peaksquareholderview: reformating file

--- peaksquareholderview/src/main/java/com/example/peaksquareholderview/PeakSquareHolderView.kt
+++ peaksquareholderview/src/main/java/com/example/peaksquareholderview/PeakSquareHolderView.kt
@@ -9,7 +9,7 @@ import android.graphics.Color
 import android.graphics.Canvas
 import android.content.Context
 
-val colors : Array<Int> = arrayOf(
+val colors: Array<Int> = arrayOf(
     "#E53935",
     "#00C853",
     "#0D47A1",
@@ -18,24 +18,24 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
-val scGap : Float = 0.04f / parts
-val strokeFactor : Float = 90f
-val sizeFactor : Float = 4.9f
-val delay : Long = 20
-val deg : Float = 90f
-val backColor : Int = Color.parseColor("#BDBDBD")
+val parts: Int = 4
+val scGap: Float = 0.04f / parts
+val strokeFactor: Float = 90f
+val sizeFactor: Float = 4.9f
+val delay: Long = 20
+val deg: Float = 90f
+val backColor: Int = Color.parseColor("#BDBDBD")
 
-fun Int.inverse() : Float = 1f / this
-fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
-fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Int.inverse(): Float = 1f / this
+fun Float.maxScale(i: Int, n: Int): Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i: Int, n: Int): Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
-fun Canvas.drawPeakSquareHolder(scale : Float, w : Float, h : Float, paint : Paint) {
-    val size : Float = Math.min(w, h) / sizeFactor
-    val sc1 : Float = scale.divideScale(0, parts)
-    val sc2 : Float = scale.divideScale(1, parts)
-    val sc3 : Float = scale.divideScale(2, parts)
-    val sc4 : Float = scale.divideScale(3, parts)
+fun Canvas.drawPeakSquareHolder(scale: Float, w: Float, h: Float, paint: Paint) {
+    val size: Float = Math.min(w, h) / sizeFactor
+    val sc1: Float = scale.divideScale(0, parts)
+    val sc2: Float = scale.divideScale(1, parts)
+    val sc3: Float = scale.divideScale(2, parts)
+    val sc4: Float = scale.divideScale(3, parts)
     save()
     translate(w / 2 + (w / 2 + size) * sc4, h / 2)
     rotate(deg * sc3)
@@ -49,22 +49,24 @@ fun Canvas.drawPeakSquareHolder(scale : Float, w : Float, h : Float, paint : Pai
     restore()
 }
 
-fun Canvas.drawPSHNode(i : Int, scale : Float, paint : Paint) {
-    val w : Float = width.toFloat()
-    val h : Float = height.toFloat()
+fun Canvas.drawPSHNode(i: Int, scale: Float, paint: Paint) {
+    val w: Float = width.toFloat()
+    val h: Float = height.toFloat()
     paint.color = colors[i]
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawPeakSquareHolder(scale, w, h, paint)
 }
 
-class PeakSquareHolderView(ctx : Context) : View(ctx) {
+class PeakSquareHolderView(ctx: Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer: Renderer = Renderer(this)
 
+    override fun onDraw(canvas: Canvas) {
+        renderer.render(canvas)
     }
 
-    override fun onTouchEvent(event : MotionEvent) : Boolean {
+    override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
 
@@ -73,9 +75,9 @@ class PeakSquareHolderView(ctx : Context) : View(ctx) {
         return true
     }
 
-    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+    data class State(var scale: Float = 0f, var dir: Float = 0f, var prevScale: Float = 0f) {
 
-        fun update(cb : (Float) -> Unit) {
+        fun update(cb: (Float) -> Unit) {
             scale += scGap * dir
             if (Math.abs(scale - prevScale) > 1) {
                 scale = prevScale + dir
@@ -85,125 +87,125 @@ class PeakSquareHolderView(ctx : Context) : View(ctx) {
             }
         }
 
-        fun startUpdating(cb : () -> Unit) {
+        fun startUpdating(cb: () -> Unit) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
             }
         }
+    }
 
-        data class Animator(var view : View, var animated : Boolean = false) {
+    data class Animator(var view: View, var animated: Boolean = false) {
 
-            fun animate(cb : () -> Unit) {
-                if (animated) {
-                    cb()
-                    try {
-                        Thread.sleep(delay)
-                        view.invalidate()
-                    } catch(ex : Exception) {
-
-                    }
-                }
-            }
-
-            fun start() {
-                if (!animated) {
-                    animated = true
-                    view.postInvalidate()
-                }
-            }
-
-            fun stop() {
-                if (animated) {
-                    animated = false
-                }
-            }
-        }
-
-        data class PSHNode(var i : Int) {
-            private val state : State = State()
-            private var prev : PSHNode? = null
-            private var next : PSHNode? = null
-
-            init {
-                addNeighbor()
-            }
-
-            fun addNeighbor() {
-                if (i < colors.size - 1) {
-                    next = PSHNode(i + 1)
-                    next?.prev = this
-                }
-            }
-
-            fun draw(canvas : Canvas, paint : Paint) {
-                canvas.drawPSHNode(i, state.scale, paint)
-            }
-
-            fun update(cb : (Float) -> Unit) {
-                state.update(cb)
-            }
-
-            fun startUpdating(cb : () -> Unit) {
-                state.startUpdating(cb)
-            }
-
-            fun getNext(dir : Int, cb : () -> Unit) : PSHNode {
-                var curr : PSHNode? = prev
-                if (dir == 1) {
-                    curr = next
-                }
-                if (curr != null) {
-                    return curr
-                }
+        fun animate(cb: () -> Unit) {
+            if (animated) {
                 cb()
-                return this
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch (ex: Exception) {
+
+                }
             }
         }
 
-        data class PeakSquareHolder(var i : Int) {
-
-            private var curr : PSHNode = PSHNode(0)
-            private var dir : Int = 1
-
-            fun draw(canvas : Canvas, paint : Paint) {
-                curr.draw(canvas, paint)
-            }
-
-            fun update(cb : (Float) -> Unit) {
-                curr.update {
-                    curr = curr.getNext(dir) {
-                        dir *= -1
-                    }
-                    cb(it)
-                }
-            }
-
-            fun startUpdating(cb : () -> Unit) {
-                curr.startUpdating(cb)
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
             }
         }
 
-        data class Renderer(var view : PeakSquareHolderView) {
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 
-            private var psh : PeakSquareHolder = PeakSquareHolder(0)
-            private val animator : Animator = Animator(view)
-            private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    data class PSHNode(var i: Int) {
+        private val state: State = State()
+        private var prev: PSHNode? = null
+        private var next: PSHNode? = null
 
-            fun render(canvas : Canvas) {
-                canvas.drawColor(backColor)
-                psh.draw(canvas, paint)
-                animator.animate {
-                    psh.update {
-                        animator.stop()
-                    }
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PSHNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas: Canvas, paint: Paint) {
+            canvas.drawPSHNode(i, state.scale, paint)
+        }
+
+        fun update(cb: (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb: () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir: Int, cb: () -> Unit): PSHNode {
+            var curr: PSHNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
+
+    data class PeakSquareHolder(var i: Int) {
+
+        private var curr: PSHNode = PSHNode(0)
+        private var dir: Int = 1
+
+        fun draw(canvas: Canvas, paint: Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb: (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb: () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
+
+    data class Renderer(var view: PeakSquareHolderView) {
+
+        private var psh : PeakSquareHolder = PeakSquareHolder(0)
+        private val animator: Animator = Animator(view)
+        private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas: Canvas) {
+            canvas.drawColor(backColor)
+            psh.draw(canvas, paint)
+            animator.animate {
+                psh.update {
+                    animator.stop()
                 }
             }
+        }
 
-            fun handleTap() {
-                psh.startUpdating {
-                    animator.start()
-                }
+        fun handleTap() {
+            psh.startUpdating {
+                animator.start()
             }
         }
     }
