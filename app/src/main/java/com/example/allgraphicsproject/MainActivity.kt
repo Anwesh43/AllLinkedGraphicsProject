a2eb8ab75b4fdca87988c32a71d8b98faaefaf10
Anwesh43 <anweshthecool0@gmail.com>
2022-01-29 02:53:02 +0530

    peaksquareholderview: adding view in activity

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.peaksquareholderview.PeakSquareHolderView
+
 //import com.example.squarelineextenderview.SquareExtenderLineView
 
 //import com.example.arcbarrotdownview.ArcBarRotDownView
@@ -434,6 +436,7 @@ class MainActivity : AppCompatActivity() {
         //TrapeBarJoinView.create(this)
         //ArcBarRotDownView.create(this)
         //SquareExtenderLineView.create(this)
+        PeakSquareHolderView.create(this)
         fullScreen()
     }
 }
