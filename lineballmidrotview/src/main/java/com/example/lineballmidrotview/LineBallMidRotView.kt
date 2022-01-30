330fcf7cafcb54457745f3f4958adb66ba8705d2
Anwesh43 <anweshthecool0@gmail.com>
2022-01-31 01:22:08 +0530

    lineballmidrotview: adding Canvas extension methods

--- lineballmidrotview/src/main/java/com/example/lineballmidrotview/LineBallMidRotView.kt
+++ lineballmidrotview/src/main/java/com/example/lineballmidrotview/LineBallMidRotView.kt
@@ -30,3 +30,36 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineBallMidRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / ballRFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc5)
+    rotate(deg * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-size / 2, 0f)
+        rotate(deg * sc2)
+        drawLine(0f, 0f, 0f, -size * 0.5f * sc1, paint)
+        restore()
+    }
+    drawCircle(0f, 0f, r * sc3, paint)
+    restore()
+}
+
+fun Canvas.drawLBMRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBallMidRot(scale, w, h, paint)
+}
+
