cd01ec12e3faaeb4e5ae36eda27b6285d84d883b
Anwesh43 <anweshthecool0@gmail.com>
2022-01-31 01:40:41 +0530

    lineballmidrotview: adding static create method

--- lineballmidrotview/src/main/java/com/example/lineballmidrotview/LineBallMidRotView.kt
+++ lineballmidrotview/src/main/java/com/example/lineballmidrotview/LineBallMidRotView.kt
@@ -214,4 +214,13 @@ class LineBallMidRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineBallMidRotView {
+            val view : LineBallMidRotView = LineBallMidRotView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
