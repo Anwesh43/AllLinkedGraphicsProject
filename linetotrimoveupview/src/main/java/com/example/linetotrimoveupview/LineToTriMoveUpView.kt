fb50a853cd34a9e82b3024d6407af72bc29371e7
Anwesh43 <anweshthecool0@gmail.com>
2022-01-30 11:24:48 +0530

    linetotrimoveupview: adding constants and libraries

---
+++ linetotrimoveupview/src/main/java/com/example/linetotrimoveupview/LineToTriMoveUpView.kt
@@ -0,0 +1,27 @@
+package com.example.linetotrimoveupview
+
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+import android.view.View
+import android.view.MotionEvent
+
+val colors : Array<Int> = arrayOf(
+    "#E53935",
+    "#00C853",
+    "#0D47A1",
+    "#AA00FF",
+    "#FF3D00"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 45f
