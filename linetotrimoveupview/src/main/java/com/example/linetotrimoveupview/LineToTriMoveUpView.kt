b416f8b795ffbeda053ec18f20ca5f4303c099c0
Anwesh43 <anweshthecool0@gmail.com>
2022-01-30 11:54:03 +0530

    linetotrimoveupview: adding LineToTriMoveUpView class

--- linetotrimoveupview/src/main/java/com/example/linetotrimoveupview/LineToTriMoveUpView.kt
+++ linetotrimoveupview/src/main/java/com/example/linetotrimoveupview/LineToTriMoveUpView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLTTMUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineToTriMoveUp(scale, w, h, paint)
 }
+
+class LineToTriMoveUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
