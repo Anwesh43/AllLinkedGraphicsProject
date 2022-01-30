cad3c224d2ac0ed2cec95ae8a94764d6ee919432
Anwesh43 <anweshthecool0@gmail.com>
2022-01-31 01:36:19 +0530

    lineballmidrotview: adding LBMRNode class

--- lineballmidrotview/src/main/java/com/example/lineballmidrotview/LineBallMidRotView.kt
+++ lineballmidrotview/src/main/java/com/example/lineballmidrotview/LineBallMidRotView.kt
@@ -125,4 +125,45 @@ class LineBallMidRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBMRNode(var i : Int, val state : State = State()) {
+
+        private var next : LBMRNode? = null
+        private var prev : LBMRNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBMRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBMRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBMRNode {
+            var curr : LBMRNode? = null
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
