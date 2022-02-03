d4803721fdc6567f0d337f2a560bdf9298335010
Anwesh43 <anweshthecool0@gmail.com>
2022-02-04 02:15:47 +0530

    barpushlineupview: adding Canvas extensioon methods

--- barpushlineupview/src/main/java/com/example/barpushlineupview/BarPushLineUpView.kt
+++ barpushlineupview/src/main/java/com/example/barpushlineupview/BarPushLineUpView.kt
@@ -24,7 +24,37 @@ val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
+val barWFactor : Float = 5.2f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBarPushLineUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val barSize : Float = size / barWFactor
+    save()
+    translate(w / 2, h / 2 - (h / 2) * sc4)
+    drawLine(-size * 0.5f * sc1, -size / 2, size * 0.5f * sc1, -size / 2, paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate((-size / 2 + barSize / 2) * (1 - sc3), 0f)
+        drawRect(RectF(-barSize / 2, size * sc2, barSize / 2, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBPLUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarPushLineUp(scale, w, h, paint)
+}
