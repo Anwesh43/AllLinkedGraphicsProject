1b73cc66eade9d29010159ad63353fa643b13320
Anwesh43 <anweshthecool0@gmail.com>
2022-02-04 02:17:00 +0530

    barpushlineupview: adding BarPushLineUpView class

--- barpushlineupview/src/main/java/com/example/barpushlineupview/BarPushLineUpView.kt
+++ barpushlineupview/src/main/java/com/example/barpushlineupview/BarPushLineUpView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawBPLUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBarPushLineUp(scale, w, h, paint)
 }
+
+class BarPushLineUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
