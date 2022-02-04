341274bb02a42a35e413675e77d8671393b9cef7
Anwesh43 <anweshthecool0@gmail.com>
2022-02-05 05:00:43 +0530

    arrowleggedmoverview: adding Canvas extension methods

--- arrowleggedmoverview/src/main/java/com/example/arrowleggedmoverview/ArrowLeggedMoverView.kt
+++ arrowleggedmoverview/src/main/java/com/example/arrowleggedmoverview/ArrowLeggedMoverView.kt
@@ -8,7 +8,7 @@ import android.graphics.Paint
 import android.graphics.Color
 import android.graphics.RectF
 import android.graphics.Canvas
-
+import android.graphics.Path
 val colors : Array<Int> = arrayOf(
     "#F44336",
     "#2196F3",
@@ -29,3 +29,40 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) *  n
+
+fun Canvas.drawArrowLeggedMover(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    save()
+    translate(w / 2 - (w / 2) * sc1 + (w / 2) * sc4, 0f)
+    val path : Path = Path()
+    path.moveTo(0f, -size / 2)
+    path.lineTo(size, 0f)
+    path.lineTo(0f, size / 2)
+    path.lineTo(0f, -size / 2)
+    drawPath(path, paint)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        translate(0f, -size / 2)
+        rotate(deg * sc3)
+        drawLine(0f, 0f, size * sc2, 0f, paint)
+        restore()
+    }
+    restore()
+    restore()
+}
+
+fun Canvas.drawALMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArrowLeggedMover(scale, w, h, paint)
+}
