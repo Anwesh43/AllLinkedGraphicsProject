d4137e637a571e1b98d4997a9db9fee5c645e7f2
Anwesh43 <anweshthecool0@gmail.com>
2022-02-06 04:22:27 +0530

    uprectwingsview: adding URWNode class

--- uprectwingsview/src/main/java/com/example/uprectwingsview/UpRectWingsView.kt
+++ uprectwingsview/src/main/java/com/example/uprectwingsview/UpRectWingsView.kt
@@ -121,4 +121,45 @@ class UpRectWingsView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class URWNode(var i : Int, val state : State = State()) {
+
+        private var next : URWNode? = null
+        private var prev : URWNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = URWNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawURWNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : URWNode {
+            var curr : URWNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
