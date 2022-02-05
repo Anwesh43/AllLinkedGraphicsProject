fcdf46ad0ea056454a0da581dfdf75d8bb17cbe8
Anwesh43 <anweshthecool0@gmail.com>
2022-02-06 04:13:08 +0530

    uprectwingsview: adding UpRectWingsView class

--- uprectwingsview/src/main/java/com/example/uprectwingsview/UpRectWingsView.kt
+++ uprectwingsview/src/main/java/com/example/uprectwingsview/UpRectWingsView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawURWNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) /  strokeFactor
     drawUpRectWings(scale, w, h, paint)
 }
+
+class UpRectWingsView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
