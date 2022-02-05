7603951a8374fbec5a8a9a9e056be3586bc7e772
Anwesh43 <anweshthecool0@gmail.com>
2022-02-06 04:06:23 +0530

    uprectwingsview: adding Canvas extension methods

--- uprectwingsview/src/main/java/com/example/uprectwingsview/UpRectWingsView.kt
+++ uprectwingsview/src/main/java/com/example/uprectwingsview/UpRectWingsView.kt
@@ -29,3 +29,32 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawUpRectWings(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 - (h / 2) * sc4)
+    drawRect(RectF(-size / 2, -size * sc1, size / 2, 0f), paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-size / 2, -size)
+        rotate(deg * sc3)
+        drawLine(0f, 0f, -size * sc2, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawURWNode(i : Int, scale : Float, paint : Paint) {
+    val w: Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) /  strokeFactor
+    drawUpRectWings(scale, w, h, paint)
+}
