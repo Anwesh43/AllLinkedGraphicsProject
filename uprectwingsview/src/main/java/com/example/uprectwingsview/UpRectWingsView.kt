fe0aa4112f0da0db9cabc315395a46b382e1caf0
Anwesh43 <anweshthecool0@gmail.com>
2022-02-06 04:25:48 +0530

    uprectwingsview: adding Renderer class

--- uprectwingsview/src/main/java/com/example/uprectwingsview/UpRectWingsView.kt
+++ uprectwingsview/src/main/java/com/example/uprectwingsview/UpRectWingsView.kt
@@ -185,4 +185,27 @@ class UpRectWingsView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : UpRectWingsView) {
+
+        private val urw : UpRectWings = UpRectWings(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            urw.draw(canvas, paint)
+            animator.animate {
+                urw.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            urw.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
