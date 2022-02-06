e74b1e89658dc83e6577dbdba21c96c0ed216dc6
Anwesh43 <anweshthecool0@gmail.com>
2022-02-07 01:11:06 +0530

    linesiderotbarview: adding Canvas extension methods

--- linesiderotbarview/src/main/java/com/example/linesiderotbarview/LineSideRotView.kt
+++ linesiderotbarview/src/main/java/com/example/linesiderotbarview/LineSideRotView.kt
@@ -30,3 +30,33 @@ val sizeFactor : Float = 4.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineSideRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + paint.strokeWidth) * sc5)
+    rotate(rot * sc4)
+    for (j in 0..1) {
+        save()
+        translate(-size / 2 + size * j, 0f)
+        rotate(deg * sc2)
+        drawLine(0f, 0f, 0f, -size * 0.5f * sc1, paint)
+        restore()
+    }
+    drawRect(RectF(-size / 2, 0f, size / 2, -size * sc3), paint)
+    restore()
+}
+
+fun Canvas.drawLSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineSideRot(scale, w, h, paint)
+}
