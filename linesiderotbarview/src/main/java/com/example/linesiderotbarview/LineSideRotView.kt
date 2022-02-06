5d307cb3431d325e4a8d6734bd1d0c0ff0292e2c
Anwesh43 <anweshthecool0@gmail.com>
2022-02-07 01:21:34 +0530

    linesiderotbarview: adding LSRNode class

--- linesiderotbarview/src/main/java/com/example/linesiderotbarview/LineSideRotView.kt
+++ linesiderotbarview/src/main/java/com/example/linesiderotbarview/LineSideRotView.kt
@@ -123,4 +123,45 @@ class LineSideRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSRNode(var i : Int, val state : State = State()) {
+
+        private var prev : LSRNode? = null
+        private var next : LSRNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSRNode {
+            var curr : LSRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
