048427605a2499aace6b9803bda5067378b14bea
Anwesh43 <anweshthecool0@gmail.com>
2022-02-08 01:37:04 +0530

    siderectrotlineview : adding SideRectRotLineView class

--- siderectrotlineview/src/main/java/com/example/siderectrotlineview/SideRectRotLineView.kt
+++ siderectrotlineview/src/main/java/com/example/siderectrotlineview/SideRectRotLineView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawSRRLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawSideRectRotLine(scale, w, h, paint)
 }
+
+class SideRectRotLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
