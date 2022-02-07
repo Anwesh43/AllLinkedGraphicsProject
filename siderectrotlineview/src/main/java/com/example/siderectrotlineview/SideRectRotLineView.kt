ce426b12df7ee4df4c6937f76f6b8120eee4df20
Anwesh43 <anweshthecool0@gmail.com>
2022-02-08 01:35:22 +0530

    siderectrotlineview : adding Canvas extension methods

--- siderectrotlineview/src/main/java/com/example/siderectrotlineview/SideRectRotLineView.kt
+++ siderectrotlineview/src/main/java/com/example/siderectrotlineview/SideRectRotLineView.kt
@@ -29,3 +29,30 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSideRectRotLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 - (h / 2 + paint.strokeWidth) * sc4)
+    for (j in 0..1) {
+        save()
+        rotate(deg * (1f - 2 * j) * (1f - sc2))
+        drawLine(0f, 0f, 0f, -size * sc1, paint)
+        restore()
+    }
+    drawRect(RectF(-size * 0.5f * sc3, -size / 2, size * 0.5f * sc3, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawSRRLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawSideRectRotLine(scale, w, h, paint)
+}
