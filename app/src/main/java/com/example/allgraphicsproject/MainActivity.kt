11c65304b630db2f1f8fe057d8c53b2eced44181
Anwesh43 <anweshthecool0@gmail.com>
2022-02-08 01:51:10 +0530

    adding SideRectRotLineView in activity

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.siderectrotlineview.SideRectRotLineView
+
 //import com.example.linesiderotbarview.LineSideRotView
 
 //import com.example.uprectwingsview.UpRectWingsView
@@ -458,6 +460,7 @@ class MainActivity : AppCompatActivity() {
         //ArrowLeggedMoverView.create(this)
         //UpRectWingsView.create(this)
         //LineSideRotView.create(this)
+        SideRectRotLineView.create(this)
         fullScreen()
     }
 }
