8023efb36b18c6d9d285476479e8ddc05f69a833
Anwesh43 <anweshthecool0@gmail.com>
2022-02-09 00:35:40 +0530

    openboxcrossmoverview: adding Canvas extension methods

--- openboxcrossmoverview/src/main/java/com/example/openboxcrossmoverview/OpenBoxCrossMoverView.kt
+++ openboxcrossmoverview/src/main/java/com/example/openboxcrossmoverview/OpenBoxCrossMoverView.kt
@@ -28,3 +28,32 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawOpenBoxCrossMover(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + paint.strokeWidth) * sc4, h / 2)
+    drawLine(0f, -size * 0.5f * sc1, 0f, size * 0.5f * sc1, paint)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        translate(0f, -size / 2)
+        rotate(deg * sc3)
+        drawLine(0f, 0f, size * sc2, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawOBCMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawOpenBoxCrossMover(scale, w, h, paint)
+}
