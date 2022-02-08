118edb6be67abdc6b17a4f4be99360b33f3cf131
Anwesh43 <anweshthecool0@gmail.com>
2022-02-09 00:50:38 +0530

    openboxcrossmoverview: adding view in activity

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.openboxcrossmoverview.OpenBoxCrossMoverView
+
 //import com.example.siderectrotlineview.SideRectRotLineView
 
 //import com.example.linesiderotbarview.LineSideRotView
@@ -461,6 +463,7 @@ class MainActivity : AppCompatActivity() {
         //UpRectWingsView.create(this)
         //LineSideRotView.create(this)
         //SideRectRotLineView.create(this)
+        OpenBoxCrossMoverView.create(this)
         fullScreen()
     }
 }
