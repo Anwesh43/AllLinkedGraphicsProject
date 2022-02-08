870a182d13f5ad6d80551907bb03d4a0a0448728
Anwesh43 <anweshthecool0@gmail.com>
2022-02-09 00:45:11 +0530

    openboxcrossmoverview: adding OBCMNode class

--- openboxcrossmoverview/src/main/java/com/example/openboxcrossmoverview/OpenBoxCrossMoverView.kt
+++ openboxcrossmoverview/src/main/java/com/example/openboxcrossmoverview/OpenBoxCrossMoverView.kt
@@ -120,4 +120,45 @@ class OpenBoxCrossMoverView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class OBCMNode(var i : Int, val state : State = State()) {
+
+        private var prev : OBCMNode? = null
+        private var next : OBCMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = OBCMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas: Canvas, paint : Paint) {
+            canvas.drawOBCMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : OBCMNode {
+            var curr : OBCMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
