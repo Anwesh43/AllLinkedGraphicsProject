dfce0e35735b97b922c43c152ae2950b16b2a3e2
Anwesh43 <anweshthecool0@gmail.com>
2022-02-11 03:55:38 +0530

    sidearmbarrotview: adding constants

---
+++ sidearmbarrotview/src/main/java/com/example/sidearmbarrotview/SideBarArmRotView.kt
@@ -0,0 +1,28 @@
+package com.example.sidearmbarrotview
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.View
+import android.view.MotionEvent
+
+val colors : Array<Int> = arrayOf(
+    "#01579B",
+    "#D50000",
+    "#DD2C00",
+    "#2962FF",
+    "#00838F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val delay : Long = 20
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val barWFactor : Float = 11.2f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
