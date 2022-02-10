301ddfcad73ec6bdcd047e4c195286269e0afd75
Anwesh43 <anweshthecool0@gmail.com>
2022-02-11 04:12:57 +0530

    sidearmbarrotview: adding Renderer class

--- sidearmbarrotview/src/main/java/com/example/sidearmbarrotview/SideBarArmRotView.kt
+++ sidearmbarrotview/src/main/java/com/example/sidearmbarrotview/SideBarArmRotView.kt
@@ -186,4 +186,27 @@ class SideBarArmRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SideBarArmRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val sbar : SideBarArmRot = SideBarArmRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            sbar.draw(canvas, paint)
+            animator.animate {
+                sbar.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            sbar.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
