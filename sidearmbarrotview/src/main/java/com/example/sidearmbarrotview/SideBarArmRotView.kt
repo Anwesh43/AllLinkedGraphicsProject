ec02a4aa3b90f64d5098f551e57ae48b4749d8bd
Anwesh43 <anweshthecool0@gmail.com>
2022-02-11 04:00:49 +0530

    sidearmbarrotview: adding Canvas extensio methods

--- sidearmbarrotview/src/main/java/com/example/sidearmbarrotview/SideBarArmRotView.kt
+++ sidearmbarrotview/src/main/java/com/example/sidearmbarrotview/SideBarArmRotView.kt
@@ -30,3 +30,32 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSideBarArmRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val barW : Float = Math.min(w, h) / barWFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2) * sc4)
+    rotate(deg * sc3)
+    drawLine(-size * 0.5f * sc1, 0f, size * 0.5f * sc1, 0f, paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawRect(RectF(-size / 2, -size * sc2, -size / 2 + barW, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSBARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSideBarArmRot(scale, w, h, paint)
+}
