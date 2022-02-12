6716968d6be1ad1950fb5827cab707e58f75102b
Anwesh43 <anweshthecool0@gmail.com>
2022-02-12 10:31:54 +0530

    krottview: adding constants and libraries

---
+++ ktorottview/src/main/java/com/example/ktorottview/KRotToTView.kt
@@ -0,0 +1,27 @@
+package com.example.ktorottview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#01579B",
+    "#D50000",
+    "#DD2C00",
+    "#2962FF",
+    "#00838F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val deg : Float = 45f
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
