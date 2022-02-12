dacd9167eb6554967ec7303220f2ad561a64bfb2
Anwesh43 <anweshthecool0@gmail.com>
2022-02-12 10:55:40 +0530

    krottview: adding KRotTView class

--- ktorottview/src/main/java/com/example/ktorottview/KRotToTView.kt
+++ ktorottview/src/main/java/com/example/ktorottview/KRotToTView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawKRTT(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawKRotT(scale, w, h, paint)
 }
+
+class KRotToTView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
