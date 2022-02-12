274c3f20ec0334bd307f6dbcb8aec6d0b22d82ae
Anwesh43 <anweshthecool0@gmail.com>
2022-02-12 11:10:29 +0530

    krottview: adding Renderer class

--- ktorottview/src/main/java/com/example/ktorottview/KRotToTView.kt
+++ ktorottview/src/main/java/com/example/ktorottview/KRotToTView.kt
@@ -185,4 +185,27 @@ class KRotTView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : KRotTView) {
+
+        private val animator : Animator = Animator(view)
+        private val krtt : KRotT = KRotT(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            krtt.draw(canvas, paint)
+            animator.animate {
+                krtt.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            krtt.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
