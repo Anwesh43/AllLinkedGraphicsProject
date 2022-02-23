733d72272e87bcc455e466862673051933af93ab
Anwesh43 <anweshthecool0@gmail.com>
2022-02-23 09:38:03 +0530

    diagboatlineview: adding Renderer class

--- diagboatlineview/src/main/java/com/example/diagboatlineview/DiagBoatLineView.kt
+++ diagboatlineview/src/main/java/com/example/diagboatlineview/DiagBoatLineView.kt
@@ -178,4 +178,27 @@ class DiagBoatLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : DiagBoatLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val dbl : DiagBoatLine = DiagBoatLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            dbl.draw(canvas, paint)
+            animator.animate {
+                dbl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            dbl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
