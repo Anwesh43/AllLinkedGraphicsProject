061c97dd9593ebc85e6c6571e1d51d9f4c11dba0
Anwesh43 <anweshthecool0@gmail.com>
2022-02-23 09:18:23 +0530

    diagboatlineview: adding Canvas extension methods

--- diagboatlineview/src/main/java/com/example/diagboatlineview/DiagBoatLineView.kt
+++ diagboatlineview/src/main/java/com/example/diagboatlineview/DiagBoatLineView.kt
@@ -28,3 +28,25 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawDiagBoatLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    drawLine(0f, 0f, 0f, -size * 0.5f * sc1, paint)
+    drawLine(size / 2, 0f, size / 2 - size * 0.5f * sc2, -size * 0.5f * sc1, paint)
+    restore()
+}
+
+fun Canvas.drawDBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawDiagBoatLine(scale, w, h, paint)
+}
