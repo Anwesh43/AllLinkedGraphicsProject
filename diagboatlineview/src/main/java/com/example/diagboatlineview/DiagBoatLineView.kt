0b6e493c5fb8f9d440837d737b9f7e12f948a783
Anwesh43 <anweshthecool0@gmail.com>
2022-02-23 09:22:01 +0530

    diagboatlineview : adding DiagBoatLienView class

--- diagboatlineview/src/main/java/com/example/diagboatlineview/DiagBoatLineView.kt
+++ diagboatlineview/src/main/java/com/example/diagboatlineview/DiagBoatLineView.kt
@@ -50,3 +50,19 @@ fun Canvas.drawDBLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawDiagBoatLine(scale, w, h, paint)
 }
+
+class DiagBoatLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
