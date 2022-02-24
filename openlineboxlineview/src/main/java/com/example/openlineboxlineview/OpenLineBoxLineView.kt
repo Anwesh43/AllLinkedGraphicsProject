0eff6ac9fb0a3220dc18ed0a8937002c87eaa72f
Anwesh43 <anweshthecool0@gmail.com>
2022-02-24 12:45:39 +0530

    openlineboxlineview : adding Canvas extension methods

--- openlineboxlineview/src/main/java/com/example/openlineboxlineview/OpenLineBoxLineView.kt
+++ openlineboxlineview/src/main/java/com/example/openlineboxlineview/OpenLineBoxLineView.kt
@@ -17,13 +17,55 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawOpenLineBoxLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val sc6 : Float = scale.divideScale(5, parts)
+    val size : Float = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, h / 2 - (h / 2 + paint.strokeWidth) * sc6)
+    rotate(deg * sc5)
+    if (sc1 > 0f) {
+        drawLine(0f, -size, 0f, -size + size * 0.5f * sc1, paint)
+    }
+    if (sc2 > 0f) {
+        drawLine(-size * 0.25f * sc2, -size / 2, size * 0.25f * sc2, -size / 2, paint)
+    }
+
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        if (sc3 > 0f) {
+            drawLine(-size * 0.25f, -size / 2, -size * 0.25f, -size / 2 + size * 0.5f * sc3, paint)
+        }
+        if (sc4 > 0f) {
+            drawLine(-size * 0.25f, 0f, -size * 0.25f * (1 + sc4), 0f, paint)
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawOLBLNode(i : Int, scale : Float, paint : Paint) {
+    val w  : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawOpenLineBoxLine(scale, w, h, paint)
+}
