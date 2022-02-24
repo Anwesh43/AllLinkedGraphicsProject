e7aa48e3799390d929823530399750bd5228fb6a
Anwesh43 <anweshthecool0@gmail.com>
2022-02-24 13:12:41 +0530

    openlineboxlineview : adding OLBLNode class

--- openlineboxlineview/src/main/java/com/example/openlineboxlineview/OpenLineBoxLineView.kt
+++ openlineboxlineview/src/main/java/com/example/openlineboxlineview/OpenLineBoxLineView.kt
@@ -132,4 +132,45 @@ class OpenLineBoxLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class OLBLNode(var i : Int, val state : State = State()) {
+
+        private var next : OLBLNode? = null
+        private var prev : OLBLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = OLBLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawOLBLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : OLBLNode {
+            var curr : OLBLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
