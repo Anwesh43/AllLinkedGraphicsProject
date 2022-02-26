a18bd0e6c4363902f53c49ed2c45e637e4ab0b30
Anwesh43 <anweshthecool0@gmail.com>
2022-02-26 16:59:18 +0530

    linerottosidenview: adding LineRotToSideNView class

--- linerottosidenview/src/main/java/com/example/linerottosidenview/LineRotToSideNView.kt
+++ linerottosidenview/src/main/java/com/example/linerottosidenview/LineRotToSideNView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLRTSNNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotToSideN(scale, w, h, paint)
 }
+
+class LineRotToSideNView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
