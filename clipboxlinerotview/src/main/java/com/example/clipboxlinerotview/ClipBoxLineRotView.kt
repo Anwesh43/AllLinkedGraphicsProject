5e7bdc67c82fd9eb52dd9000bddb98f1a8fc8a14
Anwesh43 <anweshthecool0@gmail.com>
2022-02-27 15:17:12 +0530

    clipboxlineview: adding constants and libraries

---
+++ clipboxlinerotview/src/main/java/com/example/clipboxlinerotview/ClipBoxLineRotView.kt
@@ -0,0 +1,27 @@
+package com.example.clipboxlinerotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#304FFE",
+    "#C51162",
+    "#F57F17",
+    "#00C853",
+    "#FFD600"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val deg : Float = 180f
+val rot : Float = 90f
