22004d4ff0976b996329c21a136a630c1b4cc671
Anwesh43 <anweshthecool0@gmail.com>
2022-02-27 19:00:56 +0530

    clipboxlineview: adding CBLRNode class

--- clipboxlinerotview/src/main/java/com/example/clipboxlinerotview/ClipBoxLineRotView.kt
+++ clipboxlinerotview/src/main/java/com/example/clipboxlinerotview/ClipBoxLineRotView.kt
@@ -118,4 +118,45 @@ class ClipBoxLineRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CBLRNode(var i : Int, val state : State = State()) {
+
+        private var prev : CBLRNode? = null
+        private var next : CBLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CBLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCBLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CBLRNode {
+            var curr : CBLRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
