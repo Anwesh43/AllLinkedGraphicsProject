bb8eab9a22f0b0b4d67a16f1b73b520d93cbdce8
Anwesh43 <anweshthecool0@gmail.com>
2022-02-27 19:03:22 +0530

    clipboxlinerotview: adding ClipBoxLineRot class

--- clipboxlinerotview/src/main/java/com/example/clipboxlinerotview/ClipBoxLineRotView.kt
+++ clipboxlinerotview/src/main/java/com/example/clipboxlinerotview/ClipBoxLineRotView.kt
@@ -159,4 +159,27 @@ class ClipBoxLineRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ClipBoxLineRot(var i : Int) {
+
+        private var curr : CBLRNode = CBLRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
