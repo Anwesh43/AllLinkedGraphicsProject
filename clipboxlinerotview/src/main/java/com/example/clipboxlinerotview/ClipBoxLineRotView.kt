1021587eea2f5240e50c2f7d09e7cf39fea35c44
Anwesh43 <anweshthecool0@gmail.com>
2022-02-27 19:10:29 +0530

    clipboxlinerotview: adding static create method

--- clipboxlinerotview/src/main/java/com/example/clipboxlinerotview/ClipBoxLineRotView.kt
+++ clipboxlinerotview/src/main/java/com/example/clipboxlinerotview/ClipBoxLineRotView.kt
@@ -223,4 +223,13 @@ class ClipBoxLineRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : ClipBoxLineRotView {
+            val view : ClipBoxLineRotView = ClipBoxLineRotView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
