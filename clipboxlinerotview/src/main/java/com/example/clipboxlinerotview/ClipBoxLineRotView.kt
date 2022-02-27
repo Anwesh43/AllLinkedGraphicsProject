32d9268303ad41c225edfd5716ad6b0d1925f4d2
Anwesh43 <anweshthecool0@gmail.com>
2022-02-27 18:52:00 +0530

    clipboxlineview: adding Canvas extension methods

--- clipboxlinerotview/src/main/java/com/example/clipboxlinerotview/ClipBoxLineRotView.kt
+++ clipboxlinerotview/src/main/java/com/example/clipboxlinerotview/ClipBoxLineRotView.kt
@@ -29,3 +29,29 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawClipBoxOpenLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        rotate(rot * j)
+        drawLine(-size * 0.5f * sc1, 0f, size * 0.5f * sc1, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawCBLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawClipBoxOpenLine(scale, w, h, paint)
+}
