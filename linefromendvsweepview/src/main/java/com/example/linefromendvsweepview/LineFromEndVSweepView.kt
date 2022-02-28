7570b93028f9d119cca2e86ef4c691456e81ced2
Anwesh43 <anweshthecool0@gmail.com>
2022-02-28 18:25:19 +0530

    linefromendvsweepview: adding Canvas extension methods

--- linefromendvsweepview/src/main/java/com/example/linefromendvsweepview/LineFromEndVSweepView.kt
+++ linefromendvsweepview/src/main/java/com/example/linefromendvsweepview/LineFromEndVSweepView.kt
@@ -29,3 +29,32 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineFromEndVSweep(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale( 1, parts)
+    val sc3 : Float = scale.divideScale( 2, parts)
+    val sc4 : Float = scale.divideScale( 3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h  / 2 + (h / 2 + size) * sc4)
+    rotate(rot * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        translate(0f, (h / 2 + paint.strokeWidth) * (1 - sc1))
+        rotate(deg * sc2)
+        drawLine(0f, 0f, size, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLFEVSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineFromEndVSweep(scale, w, h, paint)
+}
