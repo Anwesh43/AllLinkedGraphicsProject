4f44e97ea81e52b33a0b7d4042cb932396b2026d
Anwesh43 <anweshthecool0@gmail.com>
2022-03-01 08:24:30 +0530

    linearchookview: adding constants and libraries

---
+++ linearchookview/src/main/java/com/example/linearchookview/LineArcHookView.kt
@@ -0,0 +1,27 @@
+package com.example.linearchookview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#01579B",
+    "#D50000",
+    "#DD2C00",
+    "#2962FF",
+    "#00838F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 3.2f
+val delay : Long = 20
+val deg : Float = 180f
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
