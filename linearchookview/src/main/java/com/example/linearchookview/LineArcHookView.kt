7539b15649bd51746898d33fb7fc07be6ffd4bad
Anwesh43 <anweshthecool0@gmail.com>
2022-03-01 09:07:08 +0530

    linearchookview: adding fixes

--- linearchookview/src/main/java/com/example/linearchookview/LineArcHookView.kt
+++ linearchookview/src/main/java/com/example/linearchookview/LineArcHookView.kt
@@ -51,6 +51,7 @@ fun Canvas.drawLAHNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawLineArcHook(scale, w, h, paint)
 }
 
