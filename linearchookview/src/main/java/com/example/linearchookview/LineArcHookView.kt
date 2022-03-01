431752db28dcebacc173c1ad755129e685423297
Anwesh43 <anweshthecool0@gmail.com>
2022-03-01 08:30:31 +0530

    linearchookview: adding Canvas extension methods

--- linearchookview/src/main/java/com/example/linearchookview/LineArcHookView.kt
+++ linearchookview/src/main/java/com/example/linearchookview/LineArcHookView.kt
@@ -25,7 +25,31 @@ val delay : Long = 20
 val deg : Float = 180f
 val rot : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
+val strokeFactor : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineArcHook(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + paint.strokeWidth) * sc4)
+    rotate(rot * sc3)
+    drawLine(0f, 0f, size * 0.5f * sc1, 0f, paint)
+    drawArc(RectF(size / 2, -size / 4, size, size / 4), deg, deg * sc2, false, paint)
+    restore()
+}
+
+fun Canvas.drawLAHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcHook(scale, w, h, paint)
+}
