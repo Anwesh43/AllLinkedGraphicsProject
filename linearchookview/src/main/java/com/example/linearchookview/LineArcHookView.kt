e885f596e16df0cb51311de5bff66b55aad3a3b9
Anwesh43 <anweshthecool0@gmail.com>
2022-03-01 08:31:51 +0530

    linearchookview: adding LineArcHookView class

--- linearchookview/src/main/java/com/example/linearchookview/LineArcHookView.kt
+++ linearchookview/src/main/java/com/example/linearchookview/LineArcHookView.kt
@@ -53,3 +53,19 @@ fun Canvas.drawLAHNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineArcHook(scale, w, h, paint)
 }
+
+class LineArcHookView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
