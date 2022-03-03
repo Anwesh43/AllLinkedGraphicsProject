c06babfb1dd603f2de217e34ac8eb0d039e1d4ff
Anwesh43 <anweshthecool0@gmail.com>
2022-03-03 12:31:13 +0530

    Multipletimesrotarcview: adding Canvas extension methods

--- multipletimesrotarcview/src/main/java/com/example/multipletimesrotarcview/MultipleTimesRotArcView.kt
+++ multipletimesrotarcview/src/main/java/com/example/multipletimesrotarcview/MultipleTimesRotArcView.kt
@@ -9,15 +9,17 @@ import android.graphics.Canvas
 import android.graphics.RectF
 import android.content.Context
 
-val colors : Array<String> = arrayOf(
+val colors : Array<Int> = arrayOf(
     "#01579B",
     "#D50000",
     "#DD2C00",
     "#2962FF",
     "#00838F"
-)
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val delay : Long = 20
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -29,3 +31,53 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) *  n
+
+fun Canvas.drawMultipleTimesRotArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val fixedSize : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val sc6 : Float = scale.divideScale(5, parts)
+    val size : Float = fixedSize * (1 - sc6)
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sc4)
+    drawArc(
+        RectF(
+            -size / 2, -size / 2, size / 2, size / 2),
+        0f,
+        sweep * sc5,
+        true,
+        paint
+    )
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        for (k in 0..1) {
+            save()
+            rotate(deg * k * sc2)
+            drawLine(0f, 0f, size * sc1, 0f, paint)
+            restore()
+        }
+        drawArc(
+            RectF(-size / 2, -size / 2, size / 2, size / 2),
+            0f,
+            rot * sc3,
+            true,
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawMTRANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawMultipleTimesRotArc(scale, w, h, paint)
+}
