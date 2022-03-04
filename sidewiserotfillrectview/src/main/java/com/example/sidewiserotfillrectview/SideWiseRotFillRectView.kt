5b9d85181719420d3606a4ebb2bf4090f51c4284
Anwesh43 <anweshthecool0@gmail.com>
2022-03-04 08:59:33 +0530

    sidewiserotfillrectview: adding SideWiseRotFillRectView class

--- sidewiserotfillrectview/src/main/java/com/example/sidewiserotfillrectview/SideWiseRotFillRectView.kt
+++ sidewiserotfillrectview/src/main/java/com/example/sidewiserotfillrectview/SideWiseRotFillRectView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawSWRFRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSideWiseRotFillRect(scale, w, h, paint)
 }
+
+class SideWiseRotFillRectView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
