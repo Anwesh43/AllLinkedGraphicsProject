610a40a67d9f331c045dd4c04ec7e97826a08f35
Anwesh43 <anweshthecool0@gmail.com>
2022-03-04 08:58:46 +0530

    sidewiserotfillrectview: adding Canvas extension methods

--- sidewiserotfillrectview/src/main/java/com/example/sidewiserotfillrectview/SideWiseRotFillRectView.kt
+++ sidewiserotfillrectview/src/main/java/com/example/sidewiserotfillrectview/SideWiseRotFillRectView.kt
@@ -29,3 +29,36 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSideWiseRotFillRect(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + paint.strokeWidth) * sc4)
+    for (k in 0..1) {
+        save()
+        scale(1f - 2 * k, 1f)
+        translate(-size, 0f)
+        for (j in 0..1) {
+            save()
+            rotate(deg * (1f - 2 * j) * (1 + j) * sc2)
+            drawLine(0f, 0f, 0f, -size * sc1, paint)
+            restore()
+        }
+        restore()
+    }
+    drawRect(RectF(-size, 0f, size, (size + paint.strokeWidth / 2) * sc3), paint)
+    restore()
+}
+
+fun Canvas.drawSWRFRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSideWiseRotFillRect(scale, w, h, paint)
+}
