e122d4678e5013a14be7c0961621889da22a0822
Anwesh43 <anweshthecool0@gmail.com>
2022-03-05 10:58:52 +0530

    altsiderotrectupview: adding AltSieRotRetUpView class

--- altsiderotrectupview/src/main/java/com/example/altsiderotrectupview/AltSideRotRectUpView.kt
+++ altsiderotrectupview/src/main/java/com/example/altsiderotrectupview/AltSideRotRectUpView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawASRRUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawAltSideRotRectUp(scale, w, h, paint)
 }
+
+class AltSideRotRectUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
