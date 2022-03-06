6ef2287a785bb88d0a779c9200271f8e618e9aa4
Anwesh43 <anweshthecool0@gmail.com>
2022-03-06 12:52:25 +0530

    hammerbarhitleftview: adding HBHLNode class

--- hammerbarhitleftview/src/main/java/com/example/hammerbarhitleftview/HammerBarHitLeftView.kt
+++ hammerbarhitleftview/src/main/java/com/example/hammerbarhitleftview/HammerBarHitLeftView.kt
@@ -119,4 +119,45 @@ class HammerBarHitLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HBHLNode(var i : Int, val state : State = State()) {
+
+        private var prev : HBHLNode? = null
+        private var next : HBHLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HBHLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHBHLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HBHLNode {
+            var curr : HBHLNode? = prev
+            if (dir == 1) {
+                curr == next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
