326d631ca5e5aa7e2730cb816d94beb45367289e
Anwesh43 <anweshthecool0@gmail.com>
2022-03-06 12:41:37 +0530

    hammerbarhitleftview: adding Canvas extension methods

--- hammerbarhitleftview/src/main/java/com/example/hammerbarhitleftview/HammerBarHitLeftView.kt
+++ hammerbarhitleftview/src/main/java/com/example/hammerbarhitleftview/HammerBarHitLeftView.kt
@@ -31,3 +31,28 @@ val rot : Float = -90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawHammerBarHitLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val lSize : Float = Math.min(w, h) / lineWidthFactor
+    val barW : Float = Math.min(w, h) / barWFactor
+    val barH : Float = Math.min(w, h) / barHFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 - (w / 2) * sc4, h / 2)
+    rotate(rot * sc3)
+    drawRect(RectF(-lSize / 2, -size * sc1, lSize / 2, 0f), paint)
+    translate(0f, -size)
+    drawRect(RectF(-barW * 0.5f * sc2, -barH, barW * 0.5f * sc2, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawHBHLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawHammerBarHitLeft(scale, w, h, paint)
+}
