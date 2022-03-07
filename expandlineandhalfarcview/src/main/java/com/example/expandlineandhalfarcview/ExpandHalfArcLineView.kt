85d1984a57b6c953688c163b1d3a6c7160e9fd8b
Anwesh43 <anweshthecool0@gmail.com>
2022-03-08 01:26:20 +0530

    expandlineandhalfarcview: adding EHALNode class

--- expandlineandhalfarcview/src/main/java/com/example/expandlineandhalfarcview/ExpandHalfArcLineView.kt
+++ expandlineandhalfarcview/src/main/java/com/example/expandlineandhalfarcview/ExpandHalfArcLineView.kt
@@ -123,4 +123,45 @@ class ExpandHalfArcLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class EHALNode(var i : Int, val state : State = State()) {
+
+        private var prev : EHALNode? = null
+        private var next : EHALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = EHALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawEHALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : EHALNode {
+            var curr : EHALNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
