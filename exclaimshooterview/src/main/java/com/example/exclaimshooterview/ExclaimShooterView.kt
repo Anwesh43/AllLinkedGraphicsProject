6b93cdf1535cda9fb19c4ced2577f4eff8c4cee0
Anwesh43 <anweshthecool0@gmail.com>
2022-03-07 11:46:32 +0530

    exclaimshooterview: adding constants and libraries

---
+++ exclaimshooterview/src/main/java/com/example/exclaimshooterview/ExclaimShooterView.kt
@@ -0,0 +1,27 @@
+package com.example.exclaimshooterview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val rFactor : Float = 11.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#00C853",
+    "#AA00FF",
+    "#01579B",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
