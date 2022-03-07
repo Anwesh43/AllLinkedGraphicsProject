df4bef111cfac280df132ed0aec7184b1043f9e8
Anwesh43 <anweshthecool0@gmail.com>
2022-03-08 01:33:04 +0530

    expandlineandhalfarcview: adding Rendereer class

--- expandlineandhalfarcview/src/main/java/com/example/expandlineandhalfarcview/ExpandHalfArcLineView.kt
+++ expandlineandhalfarcview/src/main/java/com/example/expandlineandhalfarcview/ExpandHalfArcLineView.kt
@@ -187,4 +187,27 @@ class ExpandHalfArcLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ExpandHalfArcLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val ehal : ExpandHalfArcLine = ExpandHalfArcLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            ehal.draw(canvas, paint)
+            animator.animate {
+                ehal.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            ehal.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
