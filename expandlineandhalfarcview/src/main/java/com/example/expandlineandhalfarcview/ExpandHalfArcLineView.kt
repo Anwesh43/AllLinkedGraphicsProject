a55e403884d0f552f8c8d94e5dfab69310208739
Anwesh43 <anweshthecool0@gmail.com>
2022-03-08 01:18:23 +0530

    expandlineandhalfarcview: adding Canvas extension  methods

--- expandlineandhalfarcview/src/main/java/com/example/expandlineandhalfarcview/ExpandHalfArcLineView.kt
+++ expandlineandhalfarcview/src/main/java/com/example/expandlineandhalfarcview/ExpandHalfArcLineView.kt
@@ -31,3 +31,32 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawExpandHalfArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + paint.strokeWidth), h / 2)
+    rotate(-rot * sc3)
+    for (j in 0..1) {
+        save()
+        rotate(rot * sc2 * (1f - 2 * j))
+        drawLine(0f, 0f, 0f, size * sc1, paint)
+        restore()
+    }
+    drawArc(RectF(-r / 2, -r / 2, r / 2, r / 2), 0f, deg * sc2, true, paint)
+    restore()
+}
+
+fun Canvas.drawEHALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawExpandHalfArc(scale, w, h, paint)
+}
