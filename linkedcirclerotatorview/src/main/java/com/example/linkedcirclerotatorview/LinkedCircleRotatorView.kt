ce5ad5e1e863f4937007b574627ace010e839d12
Anwesh43 <anweshthecool0@gmail.com>
2022-03-10 01:18:23 +0530

    linkedcirclerotatorview: adding Canvas extension methods

--- linkedcirclerotatorview/src/main/java/com/example/linkedcirclerotatorview/LinkedCircleRotatorView.kt
+++ linkedcirclerotatorview/src/main/java/com/example/linkedcirclerotatorview/LinkedCircleRotatorView.kt
@@ -24,7 +24,40 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 60f
+val rFactor : Float = 11.9f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLinkedCircleRotator(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val r : Float = Math.min(w, h) / rFactor
+    save()
+    translate(w / 2, h / 2 - (h / 2 + size) * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        rotate(rot * sc3)
+        drawLine(0f, 0f, -size * sc1, 0f, paint)
+        save()
+        translate(-size, (-h / 2 - r) * (1 - sc2))
+        drawCircle(0f, 0f, r, paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLCRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLinkedCircleRotator(scale, w, h, paint)
+}
