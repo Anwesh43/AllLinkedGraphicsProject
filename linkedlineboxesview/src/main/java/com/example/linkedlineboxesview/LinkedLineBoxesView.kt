f5ad81abb01293c69a9e0e2166a32c3ccae67b93
Anwesh43 <anweshthecool0@gmail.com>
2022-03-09 09:58:47 +0530

    linkedlineboxesview: adding LinkedLineBoxesView class

--- linkedlineboxesview/src/main/java/com/example/linkedlineboxesview/LinkedLineBoxesView.kt
+++ linkedlineboxesview/src/main/java/com/example/linkedlineboxesview/LinkedLineBoxesView.kt
@@ -71,3 +71,19 @@ fun Canvas.drawLLBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLinkedLineBoxes(scale, w, h, paint)
 }
+
+class LinkedLineBoxesView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
