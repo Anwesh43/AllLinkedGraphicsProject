55d372e2df96a900473d7f300e3a63ff2aea22ff
Anwesh43 <anweshthecool0@gmail.com>
2022-03-13 13:31:14 +0530

    bothdirectionarrowsview: adding BothDirectionArrowsView

--- bothdirectionarrowsview/src/main/java/com/example/bothdirectionarrowsview/BothDirectionArrowsView.kt
+++ bothdirectionarrowsview/src/main/java/com/example/bothdirectionarrowsview/BothDirectionArrowsView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawBDANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBothDirectionArrows(scale, w, h, paint)
 }
+
+class BothDirectionArrowsView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
