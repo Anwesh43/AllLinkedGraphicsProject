60624cd8bdb61842c66b6f0a9961b6d496fbecb9
Anwesh43 <anweshthecool0@gmail.com>
2022-03-14 02:09:17 +0530

    linerotrectexpanderview: adding LineRotRectExpanderView class

--- linerotrectexpanderview/src/main/java/com/example/linerotrectexpanderview/LineRotRectExpanderView.kt
+++ linerotrectexpanderview/src/main/java/com/example/linerotrectexpanderview/LineRotRectExpanderView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLRRENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotExpander(scale, w, h, paint)
 }
+
+class LineRotRectExpanderView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
