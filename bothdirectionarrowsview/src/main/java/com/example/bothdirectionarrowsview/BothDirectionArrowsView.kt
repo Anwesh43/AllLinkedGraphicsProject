675ff2cd8d4836988912c07172d8e45f5b2b36a3
Anwesh43 <anweshthecool0@gmail.com>
2022-03-13 13:28:59 +0530

    bothdirectionarrowsview: adding Canvas extension methods

--- bothdirectionarrowsview/src/main/java/com/example/bothdirectionarrowsview/BothDirectionArrowsView.kt
+++ bothdirectionarrowsview/src/main/java/com/example/bothdirectionarrowsview/BothDirectionArrowsView.kt
@@ -8,7 +8,7 @@ import android.graphics.Canvas
 import android.content.Context
 import android.graphics.Color
 
-val color : Array<Int> = arrayOf(
+val colors : Array<Int> = arrayOf(
     "#1A237E",
     "#00C853",
     "#AA00FF",
@@ -28,3 +28,35 @@ val heightFactor : Float = 23.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBothDirectionArrows(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / strokeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val hSize : Float = Math.min(w, h) / heightFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(0f, (h / 2 + hSize) * sc3)
+        for (k in 0..1) {
+            save()
+            scale(1f - 2 * k, 1f)
+            drawLine(0f, -hSize * sc2, size * sc1, 0f, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBDANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBothDirectionArrows(scale, w, h, paint)
+}
