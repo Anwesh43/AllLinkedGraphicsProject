c39265bc3edf3f9e738961548b09ac3144144759
Anwesh43 <anweshthecool0@gmail.com>
2022-03-15 10:36:47 +0530

    squaresidelinerotview: adding SquareSideLineRotView class

--- squaresidelinerotview/src/main/java/com/example/squaresidelinerotview/SquareSideLineRotView.kt
+++ squaresidelinerotview/src/main/java/com/example/squaresidelinerotview/SquareSideLineRotView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawSSLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSquareSideLineRot(scale, w, h, paint)
 }
+
+class SquareSideLineRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN ->{
+
+            }
+        }
+        return true
+    }
+}
