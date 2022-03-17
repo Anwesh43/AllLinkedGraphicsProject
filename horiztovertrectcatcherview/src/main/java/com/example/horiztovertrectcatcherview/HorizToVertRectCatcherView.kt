45fc240ac74d52716ff9d274b509d137d3b52119
Anwesh43 <anweshthecool0@gmail.com>
2022-03-17 15:12:56 +0530

    horiztovertrectcatcherview: adding Rendererer class

--- horiztovertrectcatcherview/src/main/java/com/example/horiztovertrectcatcherview/HorizToVertRectCatcherView.kt
+++ horiztovertrectcatcherview/src/main/java/com/example/horiztovertrectcatcherview/HorizToVertRectCatcherView.kt
@@ -191,4 +191,27 @@ class HorizToVertRectCatcherView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : HorizToVertRectCatcherView) {
+
+        private val animator : Animator = Animator(view)
+        private val horizToVertRectCatcher : HorizToVertRectCatcher = HorizToVertRectCatcher(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            horizToVertRectCatcher.draw(canvas, paint)
+            animator.animate {
+                horizToVertRectCatcher.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            horizToVertRectCatcher.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
