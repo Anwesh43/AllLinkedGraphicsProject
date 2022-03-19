9bfc5d612c8d5047def1e0044f5d70b96f193b19
Anwesh43 <anweshthecool0@gmail.com>
2022-03-20 02:16:08 +0530

    quarterlinerotrectupview: adding QuarterLineRotRectUp class

--- quarterlinerotrectupview/src/main/java/com/example/quarterlinerotrectupview/QuarterLineRotRectUpView.kt
+++ quarterlinerotrectupview/src/main/java/com/example/quarterlinerotrectupview/QuarterLineRotRectUpView.kt
@@ -135,7 +135,7 @@ class QuarterLineRotRectUpView(ctx : Context) : View(ctx) {
             canvas.drawQLRRUNode(i, state.scale, paint)
         }
 
-        fun udpate(cb : (Float) -> Unit) {
+        fun update(cb : (Float) -> Unit) {
             state.update(cb)
         }
 
@@ -153,6 +153,29 @@ class QuarterLineRotRectUpView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class QuarterLineRotRectUp(var i : Int) {
+
+        private var curr : QLRRUNode = QLRRUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
