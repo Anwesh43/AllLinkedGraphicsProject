13888a90b025f4b06c8c16ce4de6d7122937ac7b
Anwesh43 <anweshthecool0@gmail.com>
2022-03-20 02:02:35 +0530

    quarterlinerotrectupview: adding QuarterLineRotRectUpView class

--- quarterlinerotrectupview/src/main/java/com/example/quarterlinerotrectupview/QuarterLineRotRectUpView.kt
+++ quarterlinerotrectupview/src/main/java/com/example/quarterlinerotrectupview/QuarterLineRotRectUpView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawQLRRUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawQuarterLineRotRectUp(scale, w, h, paint)
 }
+
+class QuarterLineRotRectUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
