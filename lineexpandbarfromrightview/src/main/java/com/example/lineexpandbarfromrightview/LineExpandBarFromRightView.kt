2375d81702af9c428b3e95d57c883ff3208a6644
Anwesh43 <anweshthecool0@gmail.com>
2022-03-23 01:09:20 +0530

    lineexpnadbarfromview: adding LineExpandBarFromRightView class

--- lineexpandbarfromrightview/src/main/java/com/example/lineexpandbarfromrightview/LineExpandBarFromRightView.kt
+++ lineexpandbarfromrightview/src/main/java/com/example/lineexpandbarfromrightview/LineExpandBarFromRightView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawLEBFRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineExpandBarFromRight(scale, w, h, paint)
 }
+
+class LineExpandBarFromRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
