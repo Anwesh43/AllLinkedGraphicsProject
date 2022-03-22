4b8a0be267160bbfee30455c9df7656b94ded103
Anwesh43 <anweshthecool0@gmail.com>
2022-03-23 01:17:36 +0530

    lineexpandbarfromrightview: adding LEBFRNode class

--- lineexpandbarfromrightview/src/main/java/com/example/lineexpandbarfromrightview/LineExpandBarFromRightView.kt
+++ lineexpandbarfromrightview/src/main/java/com/example/lineexpandbarfromrightview/LineExpandBarFromRightView.kt
@@ -117,4 +117,45 @@ class LineExpandBarFromRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LEBFRNode(var i : Int, val state : State = State()) {
+
+        private var next : LEBFRNode? = null
+        private var prev : LEBFRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LEBFRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLEBFRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LEBFRNode {
+            var curr : LEBFRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
