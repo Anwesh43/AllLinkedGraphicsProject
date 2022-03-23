fc95fc023b0427ff4a1a9701e04d74fe845f640d
Anwesh43 <anweshthecool0@gmail.com>
2022-03-24 01:26:03 +0530

    invertedvaseview: adding InvertedVaseView class

--- invertedvaseview/src/main/java/com/example/invertedvaseview/InvertedVaseView.kt
+++ invertedvaseview/src/main/java/com/example/invertedvaseview/InvertedVaseView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawIVNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawInvertedVase(scale, w, h, paint)
 }
+
+class InvertedVaseView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
