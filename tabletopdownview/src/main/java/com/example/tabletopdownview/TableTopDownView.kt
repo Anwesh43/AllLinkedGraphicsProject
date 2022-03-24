bdb6260873e3c1a8b209620f62786fea49828150
Anwesh43 <anweshthecool0@gmail.com>
2022-03-25 01:08:17 +0530

    tabletopdownview: adding libraries and constants

---
+++ tabletopdownview/src/main/java/com/example/tabletopdownview/TableTopDownView.kt
@@ -0,0 +1,27 @@
+package com.example.tabletopdownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#00C853",
+    "#AA00FF",
+    "#01579B",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val barHFactor : Float = 7.2f
