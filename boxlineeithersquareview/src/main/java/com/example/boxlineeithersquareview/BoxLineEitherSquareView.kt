71d0d626daada131611b14ec7c5a06a4d408d769
Anwesh43 <anweshthecool0@gmail.com>
2022-03-26 03:05:15 +0530

    boxlineeithersquareview: adding BoxLineEitherSquareView class

--- boxlineeithersquareview/src/main/java/com/example/boxlineeithersquareview/BoxLineEitherSquareView.kt
+++ boxlineeithersquareview/src/main/java/com/example/boxlineeithersquareview/BoxLineEitherSquareView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawBLESNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBoxLineEitherSquare(scale, w, h, paint)
 }
+
+class BoxLineEitherSquareView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
