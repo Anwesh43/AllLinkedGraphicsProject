46705caa0bb1ef922f5df28a41a8d89a05f174ca
Anwesh43 <anweshthecool0@gmail.com>
2022-03-26 02:55:54 +0530

    boxlineeithersquareview: adding constant s and libraries

---
+++ boxlineeithersquareview/src/main/java/com/example/boxlineeithersquareview/BoxLineEitherSquareView.kt
@@ -0,0 +1,28 @@
+package com.example.boxlineeithersquareview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#00C853",
+    "#AA00FF",
+    "#01579B",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val sqSizeFactor : Float = 13.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
