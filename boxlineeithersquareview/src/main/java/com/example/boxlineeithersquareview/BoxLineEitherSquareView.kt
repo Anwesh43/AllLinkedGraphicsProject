17b84ae5b2311b44d4d9b71ac04023a3fb7e5f8c
Anwesh43 <anweshthecool0@gmail.com>
2022-03-26 03:12:44 +0530

    boxlineeithersquareview: adding BLESNode class

--- boxlineeithersquareview/src/main/java/com/example/boxlineeithersquareview/BoxLineEitherSquareView.kt
+++ boxlineeithersquareview/src/main/java/com/example/boxlineeithersquareview/BoxLineEitherSquareView.kt
@@ -123,4 +123,45 @@ class BoxLineEitherSquareView(ctx : Context) : View(ctx) {
                 animated = false
         }
     }
+
+    data class BLESNode(var i : Int, val state : State = State()) {
+
+        private var prev : BLESNode? = null
+        private var next : BLESNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLESNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLESNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLESNode {
+            var curr : BLESNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
