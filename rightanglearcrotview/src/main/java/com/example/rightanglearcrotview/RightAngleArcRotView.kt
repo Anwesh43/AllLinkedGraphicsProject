c11927109f1a780dcf4073db5fcb68699a1b925a
Anwesh43 <anweshthecool0@gmail.com>
2022-04-01 02:03:00 +0530

    rightanglearcrotview: adding constants and libraries

---
+++ rightanglearcrotview/src/main/java/com/example/rightanglearcrotview/RightAngleArcRotView.kt
@@ -0,0 +1,27 @@
+package com.example.rightanglearcrotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#00C853",
+    "#AA00FF",
+    "#01579B",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val rot : Float = 90f
+val deg : Float = 45f
+val delay : Long = 20
+val sizeFactor : Float = 4.9f
+val strokeFactor : Float = 90f
