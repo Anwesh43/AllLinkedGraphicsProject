16e1f558f0a9a0a559f924cbe338619f4014a275
Anwesh43 <anweshthecool0@gmail.com>
2022-03-31 09:21:29 +0530

    horizcrossvertlineview: adding Canvas extension methods

--- horizcrossvertlineview/src/main/java/com/example/horizcrossvertlineview/HorizCrossVertLineView.kt
+++ horizcrossvertlineview/src/main/java/com/example/horizcrossvertlineview/HorizCrossVertLineView.kt
@@ -28,3 +28,29 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawHorizCrossVertLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size / 2) * sc4)
+    for (j in 0..1) {
+        save()
+        rotate((deg + deg * sc3) * (1f - 2 * j) * sc1)
+        drawLine(-size * 0.5f * sc1, 0f, size * 0.5f * sc1, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawHCVLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawHorizCrossVertLine(scale, w, h, paint)
+}
+
