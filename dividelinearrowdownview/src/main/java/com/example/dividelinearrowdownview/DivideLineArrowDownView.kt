ea2aeca9a72d5523fc85b9735f2f2edf308d601e
Anwesh43 <anweshthecool0@gmail.com>
2022-04-03 01:16:11 +0530

    dividelinearrowdownview: adding Renderer class

--- dividelinearrowdownview/src/main/java/com/example/dividelinearrowdownview/DivideLineArrowDownView.kt
+++ dividelinearrowdownview/src/main/java/com/example/dividelinearrowdownview/DivideLineArrowDownView.kt
@@ -189,4 +189,27 @@ class DivideLineArrowDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : DivideLineArrowDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val dlad : DivideLineArrowDown= DivideLineArrowDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            dlad.draw(canvas, paint)
+            animator.animate {
+                dlad.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            dlad.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
