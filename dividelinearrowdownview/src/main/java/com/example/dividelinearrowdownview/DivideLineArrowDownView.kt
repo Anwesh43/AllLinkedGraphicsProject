abca5d6abfffc5d464e191bca9858f9fee05e85a
Anwesh43 <anweshthecool0@gmail.com>
2022-04-03 01:06:19 +0530

    dividelinearrowdownview: adding DivideLineArrowDownView class

--- dividelinearrowdownview/src/main/java/com/example/dividelinearrowdownview/DivideLineArrowDownView.kt
+++ dividelinearrowdownview/src/main/java/com/example/dividelinearrowdownview/DivideLineArrowDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawDLADNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawDivideLineArrowDown(scale, w, h, paint)
 }
+
+class DivideLineArrowDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
