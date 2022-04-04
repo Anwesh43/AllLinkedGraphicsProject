5ff750edb07e8347c7d932b29c9e0f94a4bc3b1e
Anwesh43 <anweshthecool0@gmail.com>
2022-04-05 01:37:44 +0530

    linedividetriballshiftview: adding Renderer class

--- linedividetriballshiftview/src/main/java/com/example/linedividetriballshiftview/LineDivideTriBallShiftView.kt
+++ linedividetriballshiftview/src/main/java/com/example/linedividetriballshiftview/LineDivideTriBallShiftView.kt
@@ -190,4 +190,27 @@ class LineDivideTriBallShiftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineDivideTriBallShiftView) {
+
+        private val ldtbs : LineDivideTriBallShift = LineDivideTriBallShift(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val animator : Animator = Animator(view)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            ldtbs.draw(canvas, paint)
+            animator.animate {
+                ldtbs.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            ldtbs.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
