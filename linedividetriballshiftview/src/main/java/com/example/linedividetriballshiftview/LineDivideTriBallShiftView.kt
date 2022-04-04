d8dd42d0c59f930bc6a57fe697da8184ec065b88
Anwesh43 <anweshthecool0@gmail.com>
2022-04-05 01:32:37 +0530

    linedividetriballshiftview: adding LineDivideTriBalLShift class

--- linedividetriballshiftview/src/main/java/com/example/linedividetriballshiftview/LineDivideTriBallShiftView.kt
+++ linedividetriballshiftview/src/main/java/com/example/linedividetriballshiftview/LineDivideTriBallShiftView.kt
@@ -168,4 +168,26 @@ class LineDivideTriBallShiftView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class LineDivideTriBallShift(var i : Int) {
+
+        private var curr : LDTBSNode = LDTBSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
