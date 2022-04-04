fafdafa97a67e184b3fbdc25254bfd3a5fc1ed36
Anwesh43 <anweshthecool0@gmail.com>
2022-04-04 13:03:08 +0530

    tdividerlineview: adding static crete method

--- tdividerlineview/src/main/java/com/example/tdividerlineview/TDividerLineView.kt
+++ tdividerlineview/src/main/java/com/example/tdividerlineview/TDividerLineView.kt
@@ -208,4 +208,13 @@ class TDividerLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : TDividerLineView {
+            val view : TDividerLineView = TDividerLineView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
