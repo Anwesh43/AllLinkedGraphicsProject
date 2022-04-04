3ce7742d8b645467a6a1cd14cb42bebca7dc4402
Anwesh43 <anweshthecool0@gmail.com>
2022-04-04 12:59:52 +0530

    tdividerlineview: adding renderer in view

--- tdividerlineview/src/main/java/com/example/tdividerlineview/TDividerLineView.kt
+++ tdividerlineview/src/main/java/com/example/tdividerlineview/TDividerLineView.kt
@@ -59,14 +59,16 @@ fun Canvas.drawTDLNode(i : Int, scale : Float, paint : Paint) {
 
 class TDividerLineView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
