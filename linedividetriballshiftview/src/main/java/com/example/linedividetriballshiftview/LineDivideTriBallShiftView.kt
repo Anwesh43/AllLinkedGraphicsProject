d3db19887caf0cf151912a2c78ead6b36d7e89ae
Anwesh43 <anweshthecool0@gmail.com>
2022-04-05 02:01:40 +0530

    linedividetriballshiftview: adding static create method

--- linedividetriballshiftview/src/main/java/com/example/linedividetriballshiftview/LineDivideTriBallShiftView.kt
+++ linedividetriballshiftview/src/main/java/com/example/linedividetriballshiftview/LineDivideTriBallShiftView.kt
@@ -215,4 +215,13 @@ class LineDivideTriBallShiftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : LineDivideTriBallShiftView {
+            val view : LineDivideTriBallShiftView = LineDivideTriBallShiftView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
