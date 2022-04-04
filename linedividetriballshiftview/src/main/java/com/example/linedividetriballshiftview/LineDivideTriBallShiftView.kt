d37375762c3f51dc3dfd1641a2667eb53af1dd8f
Anwesh43 <anweshthecool0@gmail.com>
2022-04-05 00:27:33 +0530

    linedividetriballshiftview: adding Canvas extension methods

--- linedividetriballshiftview/src/main/java/com/example/linedividetriballshiftview/LineDivideTriBallShiftView.kt
+++ linedividetriballshiftview/src/main/java/com/example/linedividetriballshiftview/LineDivideTriBallShiftView.kt
@@ -20,13 +20,46 @@ val colors : Array<Int> = arrayOf(
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val sizeFactor : Float = 4.9f
-val ballRFactor : Float = 13.2f
+val ballRFactor : Float = 9.2f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val strokeFactor : Float = 90f
-val deg : Float = 45f
+val deg : Float = 22.5f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawLineDivideBallShift(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val r : Float = size / ballRFactor
+    save()
+    translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        rotate(deg * sc3)
+        drawLine(0f, 0f, -size * sc1, 0f, paint)
+        for (k in 0..2) {
+            save()
+            translate(-size + k * size * 0.5f, -r)
+            drawCircle(0f, 0f, r * sc2, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLDTBSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineDivideBallShift(scale, w, h, paint)
+}
