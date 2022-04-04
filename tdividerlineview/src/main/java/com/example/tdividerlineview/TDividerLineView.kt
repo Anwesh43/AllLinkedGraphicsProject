984a1bf7b8036b083b830918ffafb9a7548adbf6
Anwesh43 <anweshthecool0@gmail.com>
2022-04-04 12:41:43 +0530

    tdividerlineview: adding Canvas extension methods

--- tdividerlineview/src/main/java/com/example/tdividerlineview/TDividerLineView.kt
+++ tdividerlineview/src/main/java/com/example/tdividerlineview/TDividerLineView.kt
@@ -24,7 +24,35 @@ val sizeFactor : Float = 11.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 45f
+val tSizeFactor : Float = 11.2f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawTDividerLineView(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2 + (w / 2 + size), h / 2)
+    for (j in 0..1) {
+        save()
+        rotate(rot * (1f - 2 * j) * sc3)
+        drawLine(0f, 0f, -size * sc1, 0f, paint)
+        drawLine(0f, 0f, 0f, (size / tSizeFactor) * sc2, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawTDLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTDividerLineView(scale, w, h, paint)
+}
