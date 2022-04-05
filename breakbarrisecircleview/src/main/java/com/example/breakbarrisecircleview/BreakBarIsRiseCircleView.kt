f20725f39b9b05dd507377b15901bdf1169b2445
Anwesh43 <anweshthecool0@gmail.com>
2022-04-06 00:57:45 +0530

    breakbarrisecircleview: adding BBRCNdoe class

--- breakbarrisecircleview/src/main/java/com/example/breakbarrisecircleview/BreakBarIsRiseCircleView.kt
+++ breakbarrisecircleview/src/main/java/com/example/breakbarrisecircleview/BreakBarIsRiseCircleView.kt
@@ -140,4 +140,45 @@ class BreakBarIsRiseCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BBRCNode(var i : Int, val state : State = State()) {
+
+        private var prev : BBRCNode? = null
+        private var next : BBRCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BBRCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBBRCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BBRCNode? {
+            var curr : BBRCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
