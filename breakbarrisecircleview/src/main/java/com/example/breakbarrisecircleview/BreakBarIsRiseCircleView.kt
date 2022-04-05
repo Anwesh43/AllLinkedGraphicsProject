5541ea2c1fea0e6f051bfac23df1d8b4fcf87e72
Anwesh43 <anweshthecool0@gmail.com>
2022-04-06 00:53:05 +0530

    breakbarrisecircleview: adding BreakBarIseCircleView class

--- breakbarrisecircleview/src/main/java/com/example/breakbarrisecircleview/BreakBarIsRiseCircleView.kt
+++ breakbarrisecircleview/src/main/java/com/example/breakbarrisecircleview/BreakBarIsRiseCircleView.kt
@@ -77,3 +77,19 @@ fun Canvas.drawBBRCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBreakBarRiseCircle(scale, w, h, paint)
 }
+
+class BreakBarIsRiseCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
