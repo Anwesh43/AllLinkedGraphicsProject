b8ec749a74b2ecf8b76d79063c2b3531ff41beba
Anwesh43 <anweshthecool0@gmail.com>
2022-04-06 01:14:35 +0530

    breakbarisrisecircleview: adding BreakBarIsRiseCircleView in activity

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.breakbarrisecircleview.BreakBarIsRiseCircleView
+
 //import com.example.linedividetriballshiftview.LineDivideTriBallShiftView
 
 //import com.example.tdividerlineview.TDividerLineView
@@ -571,6 +573,7 @@ class MainActivity : AppCompatActivity() {
         //DivideLineArrowDownView.create(this)
         //TDividerLineView.create(this)
         //LineDivideTriBallShiftView.create(this)
+        BreakBarIsRiseCircleView.create(this)
         fullScreen()
     }
 }
