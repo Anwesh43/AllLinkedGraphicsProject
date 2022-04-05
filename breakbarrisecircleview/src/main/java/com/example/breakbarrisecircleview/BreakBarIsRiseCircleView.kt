7d751c52e1ac787d7dfacd570f2ca7c469804835
Anwesh43 <anweshthecool0@gmail.com>
2022-04-06 01:00:36 +0530

    breakbarrisecircleview: adding BreakBarRiseCircle class

--- breakbarrisecircleview/src/main/java/com/example/breakbarrisecircleview/BreakBarIsRiseCircleView.kt
+++ breakbarrisecircleview/src/main/java/com/example/breakbarrisecircleview/BreakBarIsRiseCircleView.kt
@@ -181,4 +181,27 @@ class BreakBarIsRiseCircleView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BreakBarRiseCircle(var i : Int) {
+
+        private var curr : BBRCNode = BBRCNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
