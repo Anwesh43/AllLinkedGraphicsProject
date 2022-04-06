8f2ba049baca16a8d04e3185f57bf15e989b08b5
Anwesh43 <anweshthecool0@gmail.com>
2022-04-07 02:43:53 +0530

    biinerotconccircleview: adding BiLineRotCircle class

--- bilinerotconccircleview/src/main/java/com/example/bilinerotconccircleview/BiLineRotConcCircleView.kt
+++ bilinerotconccircleview/src/main/java/com/example/bilinerotconccircleview/BiLineRotConcCircleView.kt
@@ -170,4 +170,27 @@ class BiLineRotConcCircleView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiLineRotConcCircle(var i : Int) {
+
+        private var curr : BLRCCNode = BLRCCNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
