026162cbc12a635b778906fbb615a23a12d6b7f0
Anwesh43 <anweshthecool0@gmail.com>
2022-04-08 01:23:15 +0530

    bisidebarview: adding Canvas extension methods

--- bisidebarview/src/main/java/com/example/bisidebarview/BiSideBarView.kt
+++ bisidebarview/src/main/java/com/example/bisidebarview/BiSideBarView.kt
@@ -30,3 +30,33 @@ val barWFactor : Float = 13.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBiSideBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sqSize : Float = size / barWFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2 + paint.strokeWidth) * sc4)
+    rotate(rot * sc3)
+    drawLine(0f,-size * 0.5f * sc1, 0f, size * 0.5f * sc1, paint)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        translate(0f, -size / 2)
+        drawRect(RectF(-size * sc2, 0f, 0f, sqSize), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawBiSideBar(scale, w, h, paint)
+}
