c7a48428eb06eaa83a396648292cb03cfae757e2
Anwesh43 <anweshthecool0@gmail.com>
2022-04-09 03:36:10 +0530

    barbreaktoxview: adding Renderer classs

--- barbreaktoxview/src/main/java/com/example/barbreaktoxview/BarBreakToXView.kt
+++ barbreaktoxview/src/main/java/com/example/barbreaktoxview/BarBreakToXView.kt
@@ -185,4 +185,27 @@ class BarBreakToXView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BarBreakToXView) {
+
+        private val animator : Animator = Animator(view)
+        private val bbtx : BarBreakToX = BarBreakToX(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bbtx.draw(canvas, paint)
+            animator.animate {
+                bbtx.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bbtx.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
