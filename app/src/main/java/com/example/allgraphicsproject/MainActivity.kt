92a9c2c637bd4163132870797bcce503397b210e
Anwesh43 <anweshthecool0@gmail.com>
2022-04-09 03:37:41 +0530

    barbreaktoxview: adding view in activity

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.barbreaktoxview.BarBreakToXView
+
 //import com.example.bisidebarview.BiSideBarView
 
 //import com.example.bilinerotconccircleview.BiLineRotConcCircleView
@@ -578,6 +580,7 @@ class MainActivity : AppCompatActivity() {
         //BreakBarIsRiseCircleView.create(this)
         //BiLineRotConcCircleView.create(this)
         //BiSideBarView.create(this)
+        BarBreakToXView.create(this)
         fullScreen()
     }
 }
