2f2fb802cb25870dbd233c919ded4adf6eb855fe
Anwesh43 <anweshthecool0@gmail.com>
2022-04-09 03:15:05 +0530

    barbreaktoxview: adding Canvas extension methods

--- barbreaktoxview/src/main/java/com/example/barbreaktoxview/BarBreakToXView.kt
+++ barbreaktoxview/src/main/java/com/example/barbreaktoxview/BarBreakToXView.kt
@@ -27,7 +27,34 @@ val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val deg : Float = 45f
 val rot : Float = 90f
+val barHFactor : Float = 5.9f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBarBreakToX(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val barH : Float = size / barHFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2) * sc4)
+    rotate(rot * sc3)
+    for (j in 0..1) {
+        save()
+        rotate(deg * sc2)
+        drawRect(RectF(0f, -barH / 2, size * sc1, barH / 2), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBBTXNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBarBreakToX(scale, w, h, paint)
+}
