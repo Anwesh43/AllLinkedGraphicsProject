757480917cdb7f2b7111d3a231505fda13abf5be
Anwesh43 <anweshthecool0@gmail.com>
2022-04-10 01:41:42 +0530

    blocklinearrocreatorview: adding BlockArrowLineCreator class

--- blocklinearrocreatorview/src/main/java/com/example/blocklinearrocreatorview/BlockLineArrowCreatorView.kt
+++ blocklinearrocreatorview/src/main/java/com/example/blocklinearrocreatorview/BlockLineArrowCreatorView.kt
@@ -167,4 +167,27 @@ class BlockLineArrowCreatorView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BlockLineArrowCreator(var i : Int) {
+
+        private var curr : BLACNode = BLACNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
