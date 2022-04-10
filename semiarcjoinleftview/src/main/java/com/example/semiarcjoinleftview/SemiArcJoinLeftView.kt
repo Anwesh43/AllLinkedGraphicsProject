d4a8945b7f37469e0892294d92937a2630856e33
Anwesh43 <anweshthecool0@gmail.com>
2022-04-11 00:40:23 +0530

    semiarcjoinleftview: adding renderer in view

--- semiarcjoinleftview/src/main/java/com/example/semiarcjoinleftview/SemiArcJoinLeftView.kt
+++ semiarcjoinleftview/src/main/java/com/example/semiarcjoinleftview/SemiArcJoinLeftView.kt
@@ -65,14 +65,16 @@ fun Canvas.drawSAJLNode(i : Int, scale : Float, paint : Paint) {
 
 class SemiArcJoinLeftView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -132,7 +134,7 @@ class SemiArcJoinLeftView(ctx : Context) : View(ctx) {
         private var next : SAJLNode? = null
 
         init {
-
+            addNeighbor()
         }
 
         fun addNeighbor() {
