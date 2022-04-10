7749b2d5222e4e06cd33790522f88cf723158188
Anwesh43 <anweshthecool0@gmail.com>
2022-04-11 00:17:03 +0530

    semiarcjoinleftview: adding constants and libraries

---
+++ semiarcjoinleftview/src/main/java/com/example/semiarcjoinleftview/SemiArcJoinLeftView.kt
@@ -0,0 +1,27 @@
+package com.example.semiarcjoinleftview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#00C853",
+    "#AA00FF",
+    "#01579B",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 360f
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.8f
+val rFactor : Float = 9.3f
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
