eede7f187056cf66aee244bae0c2d640aaff776c
Anwesh43 <anweshthecool0@gmail.com>
2022-04-11 00:30:26 +0530

    semiarcjoinleftview: adding SemiArcJoinLeft class

--- semiarcjoinleftview/src/main/java/com/example/semiarcjoinleftview/SemiArcJoinLeftView.kt
+++ semiarcjoinleftview/src/main/java/com/example/semiarcjoinleftview/SemiArcJoinLeftView.kt
@@ -150,7 +150,7 @@ class SemiArcJoinLeftView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
-        fun startUpdaitng(cb : () -> Unit) {
+        fun startUpdating(cb : () -> Unit) {
             state.startUpdating(cb)
         }
 
@@ -164,6 +164,29 @@ class SemiArcJoinLeftView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class SemiArcJoinLeft(var i : Int) {
+
+        private var curr : SAJLNode = SAJLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
