f962dca79af9b17193d263efd4bb9fa0ebf2876e
Anwesh43 <anweshthecool0@gmail.com>
2022-04-11 00:32:21 +0530

    semiarcjoinleftview: adding Renderer class

--- semiarcjoinleftview/src/main/java/com/example/semiarcjoinleftview/SemiArcJoinLeftView.kt
+++ semiarcjoinleftview/src/main/java/com/example/semiarcjoinleftview/SemiArcJoinLeftView.kt
@@ -189,4 +189,27 @@ class SemiArcJoinLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SemiArcJoinLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val sajl : SemiArcJoinLeft = SemiArcJoinLeft(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            sajl.draw(canvas, paint)
+            animator.animate {
+                sajl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            sajl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
