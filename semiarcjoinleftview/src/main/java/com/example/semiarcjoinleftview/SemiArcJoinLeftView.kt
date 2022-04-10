a97ac37e5151d9726d9c1e53a14c41f51fd9b76c
Anwesh43 <anweshthecool0@gmail.com>
2022-04-11 00:18:17 +0530

    semiarcjoinleftview: adding Int and Float extension methods

--- semiarcjoinleftview/src/main/java/com/example/semiarcjoinleftview/SemiArcJoinLeftView.kt
+++ semiarcjoinleftview/src/main/java/com/example/semiarcjoinleftview/SemiArcJoinLeftView.kt
@@ -19,9 +19,13 @@ val colors : Array<Int> = arrayOf(
 }.toTypedArray()
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val deg : Float = 360f
+val deg : Float = 180f
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 3.8f
 val rFactor : Float = 9.3f
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
