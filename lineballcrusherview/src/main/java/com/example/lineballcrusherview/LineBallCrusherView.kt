90264ef635f993bc350468ed4f2858f182a9e6a4
Anwesh43 <anweshthecool0@gmail.com>
2022-04-12 00:57:28 +0530

    lineballcrusherview: adding Canvas extension methods

--- lineballcrusherview/src/main/java/com/example/lineballcrusherview/LineBallCrusherView.kt
+++ lineballcrusherview/src/main/java/com/example/lineballcrusherview/LineBallCrusherView.kt
@@ -25,3 +25,36 @@ val colors : Array<Int> = arrayOf(
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineBalLCrusher(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = size / rFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2)
+    save()
+    translate((w / 2 + r) * sc4, 0f)
+    drawArc(RectF(-r, -r, r, r), 0f, 360f * sc1, true, paint)
+    restore()
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        rotate(deg * sc3)
+        drawLine(0f, 0f, 0f, size * (sc2 - sc5), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLBCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBalLCrusher(scale, w, h, paint)
+}
