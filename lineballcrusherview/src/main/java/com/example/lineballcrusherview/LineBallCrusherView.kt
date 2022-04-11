d84ac6547c4a9ee3436a9679ccefc5c72a84e337
Anwesh43 <anweshthecool0@gmail.com>
2022-04-12 01:07:34 +0530

    lineballcrusherview: adding Renderer classs

--- lineballcrusherview/src/main/java/com/example/lineballcrusherview/LineBallCrusherView.kt
+++ lineballcrusherview/src/main/java/com/example/lineballcrusherview/LineBallCrusherView.kt
@@ -17,10 +17,15 @@ val rFactor : Float = 5.8f
 val delay : Long = 20
 val deg : Float = 90f
 val colors : Array<Int> = arrayOf(
-    ""
+    "#1A237E",
+    "#00C853",
+    "#AA00FF",
+    "#01579B",
+    "#BF360C"
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -59,7 +64,7 @@ fun Canvas.drawLBCNode(i : Int, scale : Float, paint : Paint) {
     drawLineBallCrusher(scale, w, h, paint)
 }
 
-class LineBalLCrusherView(ctx : Context) : View(ctx) {
+class LineBallCrusherView(ctx : Context) : View(ctx) {
 
     override fun onDraw(canvas : Canvas) {
 
@@ -183,6 +188,29 @@ class LineBalLCrusherView(ctx : Context) : View(ctx) {
 
         fun startUpdating(cb : () -> Unit) {
             curr.startUpdating(cb)
+        }
+    }
+
+    data class Renderer(var view : LineBallCrusherView) {
+
+        private val lbc : LineBallCrusher = LineBallCrusher(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lbc.draw(canvas, paint)
+            animator.animate {
+                lbc.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lbc.startUpdating {
+                animator.start()
+            }
         }
     }
 }
