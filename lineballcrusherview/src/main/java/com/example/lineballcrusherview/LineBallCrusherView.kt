7506e51b08a6d427a9e492d2fe9fba82bab637c8
Anwesh43 <anweshthecool0@gmail.com>
2022-04-12 01:03:02 +0530

    lineballcrusherview: adding LBCNode clalsss

--- lineballcrusherview/src/main/java/com/example/lineballcrusherview/LineBallCrusherView.kt
+++ lineballcrusherview/src/main/java/com/example/lineballcrusherview/LineBallCrusherView.kt
@@ -121,4 +121,45 @@ class LineBalLCrusherView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBCNode(var i : Int, val state : State = State()) {
+
+        private var prev : LBCNode? = null
+        private var next : LBCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBCNode {
+            var curr : LBCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
