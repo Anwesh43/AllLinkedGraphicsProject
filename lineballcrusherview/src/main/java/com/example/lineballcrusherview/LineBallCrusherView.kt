718ea4544c25f396edf6928dea00f8ee10818877
Anwesh43 <anweshthecool0@gmail.com>
2022-04-12 01:09:15 +0530

    lineballcrusherview: adding LineBallCrusherView in activity

--- lineballcrusherview/src/main/java/com/example/lineballcrusherview/LineBallCrusherView.kt
+++ lineballcrusherview/src/main/java/com/example/lineballcrusherview/LineBallCrusherView.kt
@@ -215,4 +215,13 @@ class LineBallCrusherView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineBallCrusherView {
+            val view : LineBallCrusherView = LineBallCrusherView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
