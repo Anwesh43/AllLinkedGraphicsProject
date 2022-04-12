0f345a3dbb780d450418876487bf60af96380a5f
Anwesh43 <anweshthecool0@gmail.com>
2022-04-13 00:36:44 +0530

    multiarcsteptoendview: adding Canvas extension methods

--- multiarcstarttoendview/src/main/java/com/example/multiarcstarttoendview/MultiArcStepToEndView.kt
+++ multiarcstarttoendview/src/main/java/com/example/multiarcstarttoendview/MultiArcStepToEndView.kt
@@ -28,3 +28,25 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i: Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawMultiArcStepToEnd(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..(parts - 1)) {
+        val scj : Float = scale.divideScale(j, parts)
+        val scj1 : Float = scj.divideScale(0, 2)
+        val scj2 : Float = scj.divideScale(1, 2)
+        save()
+        drawArc(RectF(-size, -size, size, size), deg * j + deg * scj2, deg * (scj1 - scj2), true, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawMASTENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawMultiArcStepToEnd(scale, w, h, paint)
+}
