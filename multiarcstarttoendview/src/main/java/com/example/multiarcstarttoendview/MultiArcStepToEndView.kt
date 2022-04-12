cbabbfaebe28ef15d9a6c9b169ac1da3934ab253
Anwesh43 <anweshthecool0@gmail.com>
2022-04-13 00:29:13 +0530

    multiarcsteptoendview: adding constants and libraries

---
+++ multiarcstarttoendview/src/main/java/com/example/multiarcstarttoendview/MultiArcStepToEndView.kt
@@ -0,0 +1,27 @@
+package com.example.multiarcstarttoendview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#00C853",
+    "#AA00FF",
+    "#01579B",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val deg : Float = 360f / parts
+val backColor : Int = Color.parseColor("#BDBDBD")
+
