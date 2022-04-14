ef2ec0debebee60dc9861abcb9782dd9f30f80b9
Anwesh43 <anweshthecool0@gmail.com>
2022-04-15 02:36:34 +0530

    linejoindownsquareview: adding LJSDNode class

--- linejoindropsquareview/src/main/java/com/example/linejoindropsquareview/LineJoinSquareDownView.kt
+++ linejoindropsquareview/src/main/java/com/example/linejoindropsquareview/LineJoinSquareDownView.kt
@@ -120,5 +120,46 @@ class LineJoinSquareDownView(ctx : Context) : View(ctx) {
                 }
             }
         }
+
+        data class LJSDNode(var i : Int, val state : State = State()) {
+
+            private var prev : LJSDNode? = null
+            private var next : LJSDNode? = null
+
+            init {
+                addNeighbor()
+            }
+
+            fun addNeighbor() {
+                if (i < colors.size - 1) {
+                    next = LJSDNode(i + 1)
+                    next?.prev = this
+                }
+            }
+
+            fun draw(canvas : Canvas, paint : Paint) {
+                canvas.drawLJSDNode(i, state.scale, paint)
+            }
+
+            fun update(cb : (Float) -> Unit) {
+                state.update(cb)
+            }
+
+            fun startUpdating(cb : () -> Unit) {
+                state.startUpdating(cb)
+            }
+
+            fun getNext(dir : Int, cb : () -> Unit) : LJSDNode {
+                var curr : LJSDNode? = prev
+                if (dir == 1) {
+                    curr = next
+                }
+                if (curr != null) {
+                    return curr
+                }
+                cb()
+                return this
+            }
+        }
     }
 }
