c52674d02d706f24a18751388283d45df4a37a1c
Anwesh43 <anweshthecool0@gmail.com>
2022-04-16 01:38:57 +0530

    arcgotooppositelineview: adding AGTOLNode class

--- arcgotooppositelineview/src/main/java/com/example/arcgotooppositelineview/ArcGoToOppositeLineView.kt
+++ arcgotooppositelineview/src/main/java/com/example/arcgotooppositelineview/ArcGoToOppositeLineView.kt
@@ -124,4 +124,44 @@ class ArcGoToOppositeLineView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class AGTOLNode(var i : Int, val state : State = State()) {
+
+        private var next : AGTOLNode? = null
+        private var prev : AGTOLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AGTOLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAGTOLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AGTOLNode {
+            var curr : AGTOLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
