26dfa3e71f79c74a1d3ef377524877b998445c11
Anwesh43 <anweshthecool0@gmail.com>
2022-04-16 01:47:51 +0530

    arcgotooppositelineview: adding Renderer classs

--- arcgotooppositelineview/src/main/java/com/example/arcgotooppositelineview/ArcGoToOppositeLineView.kt
+++ arcgotooppositelineview/src/main/java/com/example/arcgotooppositelineview/ArcGoToOppositeLineView.kt
@@ -187,4 +187,27 @@ class ArcGoToOppositeLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcGoToOppositeLineView) {
+
+        private val agtol : ArcGoToOppositeLine = ArcGoToOppositeLine(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            agtol.draw(canvas, paint)
+            animator.animate {
+                agtol.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            agtol.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
