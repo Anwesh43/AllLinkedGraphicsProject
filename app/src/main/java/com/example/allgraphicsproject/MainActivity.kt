a3e5beab2d27898b5bf5549c2fba2ec66d4cefed
Anwesh43 <anweshthecool0@gmail.com>
2022-04-16 01:19:49 +0530

    adding arcgotooppositelineview setup

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linejoindropsquareview.LineJoinSquareDownView
+//import com.example.linejoindropsquareview.LineJoinSquareDownView
 
 //import com.example.lineballcrusherview.LineBallCrusherView
 
@@ -593,7 +593,7 @@ class MainActivity : AppCompatActivity() {
         //SemiArcJoinLeftView.create(this)
         //LineBallCrusherView.create(this)
         //MultiArcStepToEndView.create(this)
-        LineJoinSquareDownView.create(this)
+        //LineJoinSquareDownView.create(this)
         fullScreen()
     }
 }
