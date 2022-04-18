f631bf151f31ad918a0aae0aeaed4b8e48a664c9
Anwesh43 <anweshthecool0@gmail.com>
2022-04-18 11:40:10 +0530

    rightanflepointerview: adding Renderer class

--- rightanglepointerview/src/main/java/com/example/rightanglepointerview/RgithAnglePointerView.kt
+++ rightanglepointerview/src/main/java/com/example/rightanglepointerview/RgithAnglePointerView.kt
@@ -196,4 +196,27 @@ class RightAnglePointerView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : RightAnglePointerView) {
+
+        private val animator : Animator = Animator(view)
+        private val rap : RightAnglePointer = RightAnglePointer(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            rap.draw(canvas, paint)
+            animator.animate {
+                rap.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            rap.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
