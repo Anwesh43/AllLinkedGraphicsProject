709667ff098de2a5edf6723ff5d2b559420cd5a9
Anwesh43 <anweshthecool0@gmail.com>
2022-04-18 11:21:47 +0530

    rightanflepointerview: adding RAPNode class

--- rightanglepointerview/src/main/java/com/example/rightanglepointerview/RgithAnglePointerView.kt
+++ rightanglepointerview/src/main/java/com/example/rightanglepointerview/RgithAnglePointerView.kt
@@ -132,4 +132,45 @@ class RightAnglePointerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RAPNode(var i : Int, val state : State = State()) {
+
+        private var next : RAPNode? = null
+        private var prev : RAPNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RAPNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRAPNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RAPNode {
+            var curr : RAPNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
