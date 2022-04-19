422273129c5f29a2b16a805f9201b89be70f0a84
Anwesh43 <anweshthecool0@gmail.com>
2022-04-20 02:36:13 +0530

    shiftstackbarview: adding constants and libraries

---
+++ shiftstackbarview/src/main/java/com/example/shiftstackbarview/ShiftStackBarView.kt
@@ -0,0 +1,26 @@
+package com.example.shiftstackbarview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#00C853",
+    "#AA00FF",
+    "#01579B",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val deg : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
