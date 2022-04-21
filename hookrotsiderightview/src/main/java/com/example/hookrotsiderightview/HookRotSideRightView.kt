5049a8bff5ddd7a315c474be872264921e31cc87
Anwesh43 <anweshthecool0@gmail.com>
2022-04-22 01:18:57 +0530

    hookrotsiderightview: addign constants and libraries

---
+++ hookrotsiderightview/src/main/java/com/example/hookrotsiderightview/HookRotSideRightView.kt
@@ -0,0 +1,27 @@
+package com.example.hookrotsiderightview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#00C853",
+    "#AA00FF",
+    "#01579B",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.05f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+
