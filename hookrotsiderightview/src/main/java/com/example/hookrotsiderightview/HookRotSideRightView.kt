ac24520fb78e5591bde875124443455112a5a048
Anwesh43 <anweshthecool0@gmail.com>
2022-04-22 01:25:48 +0530

    hookrotsiderightview: addign Canvas extension methods

--- hookrotsiderightview/src/main/java/com/example/hookrotsiderightview/HookRotSideRightView.kt
+++ hookrotsiderightview/src/main/java/com/example/hookrotsiderightview/HookRotSideRightView.kt
@@ -28,3 +28,34 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int): Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawHookSideRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2 + (w / 2) * sc5, h / 2)
+    rotate(rot * sc4)
+    if (sc1 > 0f) {
+        drawLine(0f, 0f, 0f, -size * sc1, paint)
+    }
+    if (sc2 > 0f) {
+        drawLine(0f, -size, size * 0.5f * sc2, -size, paint)
+    }
+    if (sc3 > 0f) {
+        drawLine(size / 2, -size, size / 2, -size + size * 0.5f * sc3, paint)
+    }
+    restore()
+}
+
+fun Canvas.drawHRSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    drawHookSideRight(scale, w, h, paint)
+}
