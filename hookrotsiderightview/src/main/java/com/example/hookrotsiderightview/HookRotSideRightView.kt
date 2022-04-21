87cd1b4ff32ec71c74bc58c00e559464cf4c0e5a
Anwesh43 <anweshthecool0@gmail.com>
2022-04-22 01:32:46 +0530

    hookrotsiderightview: addign HRSRNode class

--- hookrotsiderightview/src/main/java/com/example/hookrotsiderightview/HookRotSideRightView.kt
+++ hookrotsiderightview/src/main/java/com/example/hookrotsiderightview/HookRotSideRightView.kt
@@ -122,4 +122,45 @@ class HookRotSideRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HRSRNode(var i : Int, val state : State = State()) {
+
+        private var prev : HRSRNode? = null
+        private var next : HRSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HRSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHRSRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HRSRNode {
+            var curr : HRSRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
