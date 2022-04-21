bf6c829ce0f7993787f510d4c33002d2d19adf22
Anwesh43 <anweshthecool0@gmail.com>
2022-04-22 01:26:58 +0530

    hookrotsiderightview: adding HookRotSideRightView class

--- hookrotsiderightview/src/main/java/com/example/hookrotsiderightview/HookRotSideRightView.kt
+++ hookrotsiderightview/src/main/java/com/example/hookrotsiderightview/HookRotSideRightView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawHRSRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawHookSideRight(scale, w, h, paint)
 }
+
+class HookRotSideRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
