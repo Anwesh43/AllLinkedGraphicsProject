f84220491cbe9fd1ae2d71121b3803d91ff4b9ee
Anwesh43 <anweshthecool0@gmail.com>
2022-04-23 02:42:01 +0530

    connectblocklineview: adding ConnectBlockLineView class

--- connectblocklineview/src/main/java/com/example/connectblocklineview/ConnectBlockLineView.kt
+++ connectblocklineview/src/main/java/com/example/connectblocklineview/ConnectBlockLineView.kt
@@ -76,3 +76,18 @@ fun Canvas.drawCBLNode(i : Int, scale : Float, paint : Paint) {
     drawConnectBlockLine(scale, w, h, paint)
 }
 
+class ConnectBlockLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
