5c0b8f5f02e575d9ff6b513da73e123582f7a2cb
Anwesh43 <anweshthecool0@gmail.com>
2022-04-23 03:09:41 +0530

    connectblocklineview: adding static create method

--- connectblocklineview/src/main/java/com/example/connectblocklineview/ConnectBlockLineView.kt
+++ connectblocklineview/src/main/java/com/example/connectblocklineview/ConnectBlockLineView.kt
@@ -227,4 +227,13 @@ class ConnectBlockLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : ConnectBlockLineView {
+            val view : ConnectBlockLineView = ConnectBlockLineView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
