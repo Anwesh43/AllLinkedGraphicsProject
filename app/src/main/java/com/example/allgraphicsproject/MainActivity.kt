6d39667347017ed17c81b77a989efc3735c8ed01
Anwesh43 <anweshthecool0@gmail.com>
2022-04-23 02:29:05 +0530

    connectblocklineview: adding setup

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.hookrotsiderightview.HookRotSideRightView
+//import com.example.hookrotsiderightview.HookRotSideRightView
 
 //import com.example.shiftstackbarview.ShiftStackBarView
 
@@ -608,7 +608,7 @@ class MainActivity : AppCompatActivity() {
         //RightAnglePointerView.create(this)
         //DotEitherExpanderView.create(this)
         //ShiftStackBarView.create(this)
-        HookRotSideRightView.create(this)
+        //HookRotSideRightView.create(this)
         fullScreen()
     }
 }
