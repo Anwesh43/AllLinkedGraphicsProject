8883899ec6615c71f87a954d1a0d3934fab4007b
Anwesh43 <anweshthecool0@gmail.com>
2022-04-27 01:18:33 +0530

    linesplitboxview: adding LSBNode class

--- linesplitboxview/src/main/java/com/example/linesplitboxview/LineSplitBoxView.kt
+++ linesplitboxview/src/main/java/com/example/linesplitboxview/LineSplitBoxView.kt
@@ -123,4 +123,45 @@ class LineSplitBoxView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSBNode(var i : Int, val state : State = State()) {
+
+        private var next : LSBNode? = null
+        private var prev : LSBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSBNode(i + 1)
+                next?.prev =  this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSBNode {
+            var curr : LSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
