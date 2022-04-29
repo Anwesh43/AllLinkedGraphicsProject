05b1e22a52b364bb77389ba23cb1a6fd09bc061e
Anwesh43 <anweshthecool0@gmail.com>
2022-04-30 04:03:45 +0530

    linebisiderotview: adding Canvas extension methods

--- linebisiderotview/src/main/java/com/example/linebisiderotview/LineBiSideRotView.kt
+++ linebisiderotview/src/main/java/com/example/linebisiderotview/LineBiSideRotView.kt
@@ -31,4 +31,41 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawLineBiSideRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val lSize : Float = Math.min(w, h) / lSizeFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc5)
+    rotate(deg * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        save()
+        translate(-size / 2, 0f)
+        for (k in 0..1) {
+            save()
+            rotate(rot * sc3 * (1f - 2 * j))
+            drawLine(0f, 0f, 0f, lSize * 0.5f * (1f - 2 * k) * sc2, paint)
+            restore()
+        }
+        restore()
+        restore()
+    }
+    drawLine(-size * 0.5f * sc1, 0f, size * 0.5f * sc1, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawLBSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBiSideRot(scale, w, h, paint)
+}
 
