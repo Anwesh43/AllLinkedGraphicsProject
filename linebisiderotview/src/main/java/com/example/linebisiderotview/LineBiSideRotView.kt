72ce07b90b95b07203e46c322d7518340d7273da
Anwesh43 <anweshthecool0@gmail.com>
2022-04-30 04:04:55 +0530

    linebisiderotview: adding LineBiSideRotView class

--- linebisiderotview/src/main/java/com/example/linebisiderotview/LineBiSideRotView.kt
+++ linebisiderotview/src/main/java/com/example/linebisiderotview/LineBiSideRotView.kt
@@ -69,3 +69,19 @@ fun Canvas.drawLBSRNode(i : Int, scale : Float, paint : Paint) {
     drawLineBiSideRot(scale, w, h, paint)
 }
 
+class LineBiSideRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
+
