1aceb82d6e4675fd975aa37a69231bf3209d3fa7
Anwesh43 <anweshthecool0@gmail.com>
2022-05-01 16:47:00 +0530

    squarepointerlineview: adding Canvas extension methods

--- squarepointerlineview/src/main/java/com/example/squarepointerlineview/SquarePointerLineView.kt
+++ squarepointerlineview/src/main/java/com/example/squarepointerlineview/SquarePointerLineView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 5
+val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -30,3 +30,34 @@ val sqSizeFactor : Float = 11.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSquarePointerLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sqSize : Float = Math.min(w, h) / sqSizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    save()
+    rotate(rot * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j,1f)
+        drawLine(0f, 0f, size * sc1, -size * sc1, paint)
+        restore()
+    }
+    restore()
+    drawRect(RectF(-sqSize * sc2, -sqSize * sc2, sqSize * sc2, sqSize * sc2), paint)
+    restore()
+}
+
+fun Canvas.drawSPLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSquarePointerLine(scale, w, h, paint)
+}
