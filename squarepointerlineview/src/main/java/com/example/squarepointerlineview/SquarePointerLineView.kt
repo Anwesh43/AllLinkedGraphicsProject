2f3e5052516d85073637eb7381dd34be8e360cf7
Anwesh43 <anweshthecool0@gmail.com>
2022-05-01 16:48:09 +0530

    squarepointerlineview: adding SquarePointerLineView class

--- squarepointerlineview/src/main/java/com/example/squarepointerlineview/SquarePointerLineView.kt
+++ squarepointerlineview/src/main/java/com/example/squarepointerlineview/SquarePointerLineView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawSPLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSquarePointerLine(scale, w, h, paint)
 }
+
+class SquarePointerLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
