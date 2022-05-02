74295ed36293bbf3c7724d6874ce284da25a822c
Anwesh43 <anweshthecool0@gmail.com>
2022-05-03 01:10:45 +0530

    downrotarrowview: adding DownRotArrowView class

--- downrotarrowview/src/main/java/com/example/downrotarrowview/DownRotArrowView.kt
+++ downrotarrowview/src/main/java/com/example/downrotarrowview/DownRotArrowView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawDRANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawDownRotArrow(scale, w, h, paint)
 }
+
+class DownRotArrowView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
