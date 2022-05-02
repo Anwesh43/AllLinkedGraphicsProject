059af53e908029bf90426f65dedd406272174371
Anwesh43 <anweshthecool0@gmail.com>
2022-05-03 01:15:21 +0530

    downrotarrowview: adding DRANode class

--- downrotarrowview/src/main/java/com/example/downrotarrowview/DownRotArrowView.kt
+++ downrotarrowview/src/main/java/com/example/downrotarrowview/DownRotArrowView.kt
@@ -117,4 +117,45 @@ class DownRotArrowView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DRANode(var i : Int, val state : State = State()) {
+
+        private var prev : DRANode? = null
+        private var next : DRANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DRANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDRANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DRANode {
+            var curr : DRANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
