562cd3b4eb3cfa0a4f514a04fab29b96825aad3a
Anwesh43 <anweshthecool0@gmail.com>
2022-05-06 12:04:47 +0530

    perpanglesquareview: adding setup

--- app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
+++ app/src/main/java/com/example/allgraphicsproject/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.allgraphicsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.downrotarrowview.DownRotArrowView
+//import com.example.downrotarrowview.DownRotArrowView
 
 //import com.example.squarepointerlineview.SquarePointerLineView
 
@@ -626,7 +626,7 @@ class MainActivity : AppCompatActivity() {
         //LineSplitBoxView.create(this)
         //LineBiSideRotView.create(this)
         //SquarePointerLineView.create(this)
-        DownRotArrowView.create(this)
+       // DownRotArrowView.create(this)
         fullScreen()
     }
 }
