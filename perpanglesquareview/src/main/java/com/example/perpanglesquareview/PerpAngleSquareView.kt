9c55831012a177ac8963653e94969d98f2258e5e
Anwesh43 <anweshthecool0@gmail.com>
2022-05-06 12:16:21 +0530

    perpanglesquareview: adding PerpAngleSquareViw class

--- perpanglesquareview/src/main/java/com/example/perpanglesquareview/PerpAngleSquareView.kt
+++ perpanglesquareview/src/main/java/com/example/perpanglesquareview/PerpAngleSquareView.kt
@@ -60,3 +60,20 @@ fun Canvas.drawPASNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawPerpAngleSquare(scale, w, h, paint)
 }
+
+
+class PerpAngleSquareView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
