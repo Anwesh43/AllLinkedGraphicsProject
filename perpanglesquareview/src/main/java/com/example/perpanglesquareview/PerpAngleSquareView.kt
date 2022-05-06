9834188722cf0979b053fcf405137297d76e34f1
Anwesh43 <anweshthecool0@gmail.com>
2022-05-06 12:27:51 +0530

    perpanglesquareview: adding PASNode class

--- perpanglesquareview/src/main/java/com/example/perpanglesquareview/PerpAngleSquareView.kt
+++ perpanglesquareview/src/main/java/com/example/perpanglesquareview/PerpAngleSquareView.kt
@@ -124,4 +124,44 @@ class PerpAngleSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PASNode(var i : Int, val state : State = State()) {
+        private var prev : PASNode? = null
+        private var next : PASNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PASNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPASNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PASNode {
+            var curr : PASNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
