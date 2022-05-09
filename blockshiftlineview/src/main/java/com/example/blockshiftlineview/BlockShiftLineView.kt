788482cafa371f96fb4a40267043913d55570508
Anwesh43 <anweshthecool0@gmail.com>
2022-05-09 09:46:17 +0530

    blockshiftlineview: adding BlockLineShiftView class

--- blockshiftlineview/src/main/java/com/example/blockshiftlineview/BlockShiftLineView.kt
+++ blockshiftlineview/src/main/java/com/example/blockshiftlineview/BlockShiftLineView.kt
@@ -58,3 +58,18 @@ fun Canvas.drawBSLNode(i : Int, scale : Float, paint : Paint) {
     drawBlockShiftLine(scale, w, h, paint)
 }
 
+class BlockShiftLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
