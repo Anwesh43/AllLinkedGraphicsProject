8ae714b8681ea79f9e484826b9a1f72753ff6627
Anwesh43 <anweshthecool0@gmail.com>
2022-05-11 01:31:46 +0530

    barbelowlineview: adding BarBelowLine classs

--- barbelowlineview/src/main/java/com/example/barbelowlineview/BarBelowLineView.kt
+++ barbelowlineview/src/main/java/com/example/barbelowlineview/BarBelowLineView.kt
@@ -163,4 +163,27 @@ class BarBelowLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BarBelowLine(var i : Int, val state : State = State()) {
+
+        private var curr : BBLNode = BBLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
