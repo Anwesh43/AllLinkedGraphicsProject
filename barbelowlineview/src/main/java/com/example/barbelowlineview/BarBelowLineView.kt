f77c6ff883ddc64cccc4e4726466749105c5f2ae
Anwesh43 <anweshthecool0@gmail.com>
2022-05-11 01:21:43 +0530

    barbelowlineview: adding Canvas extension methods

--- barbelowlineview/src/main/java/com/example/barbelowlineview/BarBelowLineView.kt
+++ barbelowlineview/src/main/java/com/example/barbelowlineview/BarBelowLineView.kt
@@ -29,3 +29,33 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBarBelowLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc5 : Float = scale.divideScale(4, parts)
+    val sc6 : Float = scale.divideScale(5, parts)
+    val barSize : Float = size / 4
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc6)
+    rotate(rot * sc5)
+    for (j in 0..1) {
+        val sc1 : Float = scale.divideScale(j * 2, parts)
+        val sc2 : Float = scale.divideScale(j * 2 + 1, parts)
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-size / 2, 0f)
+        drawRect(RectF(0f, -barSize * sc1, barSize, 0f), paint)
+        drawLine(barSize / 2 - (barSize / 2) * sc2, -size / 2, barSize / 2 + (barSize / 2) * sc2, -size / 2, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarBelowLine(scale, w, h, paint)
+}
