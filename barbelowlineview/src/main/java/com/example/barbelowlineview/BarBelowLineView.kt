b1d406f430ab9c8d09282c193e5a332199ea35f5
Anwesh43 <anweshthecool0@gmail.com>
2022-05-11 01:22:48 +0530

    barbelowlineview: adding BarBelowLineView classs

--- barbelowlineview/src/main/java/com/example/barbelowlineview/BarBelowLineView.kt
+++ barbelowlineview/src/main/java/com/example/barbelowlineview/BarBelowLineView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawBBLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBarBelowLine(scale, w, h, paint)
 }
+
+class BarBelowLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
