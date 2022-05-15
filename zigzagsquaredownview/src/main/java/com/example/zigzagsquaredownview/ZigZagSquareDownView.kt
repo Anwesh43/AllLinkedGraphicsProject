83bf8ec4c62ab79d98e1678df2432a99519e99b9
Anwesh43 <anweshthecool0@gmail.com>
2022-05-15 10:37:21 +0530

    zigzagsquaredownview: adding ZigZagSquareDownView class

--- zigzagsquaredownview/src/main/java/com/example/zigzagsquaredownview/ZigZagSquareDownView.kt
+++ zigzagsquaredownview/src/main/java/com/example/zigzagsquaredownview/ZigZagSquareDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawZZSDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawZigZagSquareDown(scale, w, h, paint)
 }
+
+class ZigZagSquareDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
