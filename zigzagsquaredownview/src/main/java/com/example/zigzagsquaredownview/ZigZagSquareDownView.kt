b6c86b43103e2d535338e06fa37544d42d5bf547
Anwesh43 <anweshthecool0@gmail.com>
2022-05-15 10:29:01 +0530

    zigzagsquaredownview: adding Canvas extension methods

--- zigzagsquaredownview/src/main/java/com/example/zigzagsquaredownview/ZigZagSquareDownView.kt
+++ zigzagsquaredownview/src/main/java/com/example/zigzagsquaredownview/ZigZagSquareDownView.kt
@@ -30,3 +30,35 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawZigZagSquareDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val barSize : Float = (Math.min(w, h) / sqSizeFactor)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size / 2) * sc4)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        drawLine(0f, 0f, size * sc2, 0f, paint)
+        save()
+        translate(size * sc2, 0f)
+        drawRect(RectF(0f, -barSize * sc1, barSize, 0f), paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawZZSDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawZigZagSquareDown(scale, w, h, paint)
+}
