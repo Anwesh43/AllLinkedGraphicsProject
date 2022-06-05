12197d27e3e036a53c4cec5b82a3fad2c5b53b37
Anwesh43 <anweshthecool0@gmail.com>
2022-06-05 12:10:07 +0530

    lineupcirclerotrightview adding classes

---
+++ lineupcirclerotrightview/src/main/java/com/example/lineupcirclerotrightview/LineUpCircleRightView.kt
@@ -0,0 +1,229 @@
+package com.example.lineupcirclerotrightview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#00C853",
+    "#AA00FF",
+    "#01579B",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineUpCircleRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+    rotate(rot * sc3)
+    save()
+    translate( (w / 2 + size / 2) * (sc1 - 1), 0f)
+    drawLine(-size / 2, 0f, size / 2, 0f, paint)
+    restore()
+    save()
+    translate(0f, -0.75f * size)
+    drawArc(
+        RectF(-size * 0.5f, -size * 0.5f, size * 0.5f, size * 0.5f),
+        0f,
+        360f * sc2,
+        false,
+        paint
+    )
+    restore()
+    restore()
+}
+
+fun Canvas.drawLUCRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineUpCircleRight(scale, w, h, paint)
+}
+
+class LineUpCircleRotRightView(ctx : Context) : View(ctx) {
+
+    private val renderer : Renderer = Renderer(this)
+
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                renderer.handleTap()
+            }
+        }
+        return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scGap * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
+
+    data class LUCRNode(var i : Int, val state : State = State()) {
+
+        private var prev : LUCRNode? = null
+        private var next : LUCRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LUCRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLUCRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LUCRNode {
+            var curr : LUCRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
+
+    data class LineUpCircleRight(var i : Int) {
+
+        private var curr : LUCRNode = LUCRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
+
+    data class Renderer(var view : LineUpCircleRotRightView) {
+
+        private val animator : Animator = Animator(view)
+        private val lucr : LineUpCircleRight = LineUpCircleRight(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lucr.draw(canvas, paint)
+            animator.animate {
+                lucr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lucr.startUpdating {
+                animator.start()
+            }
+        }
+    }
+
+    companion object {
+
+        fun create(activity : Activity) : LineUpCircleRotRightView {
+            val view : LineUpCircleRotRightView = LineUpCircleRotRightView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
+}
