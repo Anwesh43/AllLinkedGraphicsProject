9920984139db9b2914c6cc27cd808f9aa13a26d3
Anwesh43 <anweshthecool0@gmail.com>
2022-06-17 10:17:32 +0530

    rightlinebarupview: adding Canvas extension merthods

--- rightlinebarupview/src/main/java/com/example/rightlinebarupview/RightLineBarUpView.kt
+++ rightlinebarupview/src/main/java/com/example/rightlinebarupview/RightLineBarUpView.kt
@@ -30,3 +30,29 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawRightLineBarUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val barSize : Float = Math.min(w, h) /
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        rotate(-rot * sc2)
+        drawLine(0f, 0f, size * sc1, 0f, paint)
+        restore()
+    }
+    drawRect(RectF(size - barSize, -size * sc3, size, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawRLBUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawRightLineBarUp(scale, w, h, paint)
+}
