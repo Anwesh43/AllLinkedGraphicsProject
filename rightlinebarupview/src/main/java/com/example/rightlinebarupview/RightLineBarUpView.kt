cd4691257e7a026d5a39e06e5b1de4b3765808bb
Anwesh43 <anweshthecool0@gmail.com>
2022-06-17 10:18:37 +0530

    rightlinebarupview: adding RightLineBarUpView class

--- rightlinebarupview/src/main/java/com/example/rightlinebarupview/RightLineBarUpView.kt
+++ rightlinebarupview/src/main/java/com/example/rightlinebarupview/RightLineBarUpView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawRLBUNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawRightLineBarUp(scale, w, h, paint)
 }
+
+class RightLineBarUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
